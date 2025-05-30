f0c3ab9176a0bbaec4a3a41e86d2952e7eedbad7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-05-30 12:07:46 -0400

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.py
@@ -0,0 +1,20 @@
+# Last updated: 5/30/2025, 12:07:45 PM
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        if self.k > len(self.heap) or self.heap[0] < val:
+            heapq.heappush(self.heap, val)
+            if len(self.heap) > self.k:
+                heapq.heappop(self.heap)
+        return self.heap[0]
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
