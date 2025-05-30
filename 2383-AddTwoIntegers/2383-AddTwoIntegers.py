4230050af2c0af7001bd3d149a7a635e38989004
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-05-30 12:07:19 -0400

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.py
@@ -0,0 +1,9 @@
+# Last updated: 5/30/2025, 12:07:19 PM
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1 + num2
