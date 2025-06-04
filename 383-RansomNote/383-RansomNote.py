58eea432f9aaed20755b45d02e03b215a76998f8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-04 10:17:19 -0400

    File updated at 6/4/2025, 10:17:19 AM

--- 383-RansomNote/383-RansomNote.py
+++ 383-RansomNote/383-RansomNote.py
@@ -1,9 +1,12 @@
-# Last updated: 6/4/2025, 10:16:03 AM
+# Last updated: 6/4/2025, 10:17:19 AM
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+
+        if len(magazine) < len(ransomNote):
+            return False
+
         frequencies = Counter(magazine)
         
-
         for c in ransomNote:
             if frequencies[c] > 0:
                 frequencies[c] -= 1
