a23ca9ef6df50a96583edb2f0980e55b540c9eb9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-04 10:16:04 -0400

    File updated at 6/4/2025, 10:16:03 AM

--- 383-RansomNote/383-RansomNote.py
+++ 383-RansomNote/383-RansomNote.py
@@ -1,9 +1,7 @@
-# Last updated: 6/4/2025, 10:13:38 AM
+# Last updated: 6/4/2025, 10:16:03 AM
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        frequencies = defaultdict(int)
-        for char in magazine:
-            frequencies[char] = frequencies.get(char, 0) + 1
+        frequencies = Counter(magazine)
         
 
         for c in ransomNote:
