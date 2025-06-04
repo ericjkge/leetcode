d3743d6ccac186d63193d079f22b0c2fcb079f17
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-04 09:39:50 -0400

    File updated at 6/4/2025, 9:39:50 AM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,4 +1,4 @@
-# Last updated: 6/4/2025, 9:39:07 AM
+# Last updated: 6/4/2025, 9:39:50 AM
 """
 # Definition for a Node.
 class Node:
@@ -15,13 +15,13 @@ class Solution:
 
         def dfs(n):
             if n in mapping:
-                return mapping[n]
+                return mapping[n] # Return copy if it already exists
             
             copy = Node(n.val)
 
             mapping[n] = copy
             for nei in n.neighbors:
                 copy.neighbors.append(dfs(nei))
-            return copy
+            return copy # Return newly created copy
 
         return dfs(node) if node else None
