ead9e456e0347bc7a9f9d0ced35abd8f7f334ffd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-05 14:35:38 -0400

    File updated at 6/5/2025, 2:35:38 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,18 +1,19 @@
-# Last updated: 6/3/2025, 2:29:00 PM
+# Last updated: 6/5/2025, 2:35:38 PM
 class Solution(object):
     def twoSum(self, nums, target):
-        values = {}
-        for i in range(len(nums)):
-            values[nums[i]] = i
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in values and values[complement] != i:
-                return [i, values.get(complement)]
-        return []
-
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
+
+        mapping = defaultdict()
+        for i in range(len(nums)):
+            mapping[nums[i]] = i
+        
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in mapping and mapping[complement] != i:
+                return i, mapping[complement]
+        return -1
         
