ad4cd1cc1b233229d4cef6089d470f02f69e0b87
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-06 16:16:37 -0400

    File updated at 6/6/2025, 4:16:37 PM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 6/6/2025, 3:43:58 PM
+# Last updated: 6/6/2025, 4:16:37 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,16 +7,31 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 
-        nodes = []
-        for l in lists:
-            while l:
-                nodes.append(l.val)
-                l = l.next
+        def mergeLists(l1, l2):
+            dummy = curr = ListNode()
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+            
+            curr.next = l1 if l1 else l2
+            return dummy.next
         
-        head = curr = ListNode()
-        for node in sorted(nodes):
-            curr.next = ListNode(node)
-            curr = curr.next
-
-        return head.next
+        
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(mergeLists(l1, l2))
+            lists = mergedLists
+            
+        return lists[0]
 
