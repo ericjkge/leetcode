c14f0e559b4c030e8a1eb7a55c9afc187111da06
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-06 14:40:51 -0400

    File updated at 6/6/2025, 2:40:51 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,4 +1,4 @@
-# Last updated: 6/6/2025, 12:44:39 PM
+# Last updated: 6/6/2025, 2:40:51 PM
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans = []
@@ -6,7 +6,7 @@ class Solution:
         for i, v in enumerate(nums):
             if i != 0 and nums[i] == nums[i-1]:
                 continue
-            
+
             l, r = i + 1, len(nums) - 1
             while l < r:
                 if nums[l] + nums[r] > -v:
@@ -16,7 +16,7 @@ class Solution:
                 else:
                     ans.append([v, nums[l], nums[r]])
                     l += 1
-                    while nums[l] == nums[l - 1] and l < r: 
+                    while l < r and nums[l] == nums[l - 1]:
                         l += 1
-                        
+            
         return ans
