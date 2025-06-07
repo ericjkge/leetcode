10f8ed1cafc73ff7709c19c5616ad5e479e8a301
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-07 12:21:12 -0400

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -0,0 +1,25 @@
+# Last updated: 6/7/2025, 12:21:12 PM
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        hashmap = defaultdict(int)
+        for letter in s:
+            hashmap[letter] += 1
+        
+        max_odd = 0
+        ans = 0
+        for letter in hashmap:
+            if hashmap[letter] % 2 == 1:
+                if hashmap[letter] <= max_odd:
+                    ans += hashmap[letter] - 1
+                else:
+                    if max_odd != 0:
+                        ans += max_odd - 1
+                    max_odd = hashmap[letter]
+            else:
+                ans += hashmap[letter]
+        
+        print(ans)
+        print(max_odd)
+        return ans + max_odd
+        
+        
