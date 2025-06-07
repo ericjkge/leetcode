37b5bae27b56f01106847a828db0bdb6a1a5151c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-07 11:12:21 -0400

    File updated at 6/7/2025, 11:12:21 AM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,4 +1,4 @@
-# Last updated: 6/7/2025, 11:11:47 AM
+# Last updated: 6/7/2025, 11:12:21 AM
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         queue = deque()
@@ -23,43 +23,3 @@ class Solution:
             time += 1
 
         return time if fresh == 0 else -1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # queue = deque()
-        # time, fresh = 0, 0
-        # rows = len(grid)
-        # cols = len(grid[0])
-        # directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-        # for i in range(rows):
-        #     for j in range(cols):
-        #         if grid[i][j] == 1:
-        #             fresh += 1
-        #         if grid[i][j] == 2:
-        #             queue.append([i, j])
-        
-        # while queue and fresh > 0:
-        #     for i in range(len(queue)):
-        #         r, c = queue.popleft()
-        #         for dr, dc in directions:
-        #             row = r + dr
-        #             col = c + dc
-        #             if (0 <= row < rows and 0 <= col < cols and grid[row][col] == 1):
-        #                 grid[row][col] = 2
-        #                 queue.append([row, col])
-        #                 fresh -= 1
-        #     time += 1
-        
-        # return time if fresh == 0 else -1
