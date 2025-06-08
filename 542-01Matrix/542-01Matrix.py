e372e1ec23a0b4812821dad17fb8aea2d2fba200
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-08 19:23:58 -0400

    Create 542-01Matrix

---
+++ 542-01Matrix/542-01Matrix.py
@@ -0,0 +1,27 @@
+# Last updated: 6/8/2025, 7:23:58 PM
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        rows = len(mat)
+        cols = len(mat[0])
+        dist = [[float("inf")] * cols for _ in range(rows)]
+        queue = deque()
+
+        for row in range(len(mat)):
+            for col in range(len(mat[0])):
+                if mat[row][col] == 0:
+                    dist[row][col] = 0
+                    queue.append((row, col))
+        
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and dist[nr][nc] > dist[r][c] + 1:
+                    dist[nr][nc] = dist[r][c] + 1
+                    queue.append((nr, nc))
+        
+        return dist
+
+                
