c7e1a5f666516af23cc73a7c9708658c9f33132b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-08 21:16:43 -0400

    Create 23-MergeKSortedLists

---
+++ 23-MergeKSortedLists/23-MergeKSortedLists.py
@@ -0,0 +1,37 @@
+# Last updated: 6/8/2025, 9:16:43 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+
+        def mergeLists(l1, l2):
+            dummy = curr = ListNode()
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+            
+            curr.next = l1 if l1 else l2
+            return dummy.next
+        
+        
+        if not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) > 1:
+            mergedLists = []
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i + 1] if i + 1 < len(lists) else None
+                mergedLists.append(mergeLists(l1, l2))
+            lists = mergedLists
+            
+        return lists[0]
+
