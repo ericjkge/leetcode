3dd0d8a869af2ffcefd5155746b4b1922ad97dc6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-09 23:45:10 -0400

    File updated at 6/9/2025, 11:45:10 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,24 +1,21 @@
-# Last updated: 6/8/2025, 6:53:31 PM
+# Last updated: 6/9/2025, 11:45:10 PM
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def expand(i, j):
-            while i >= 0 and j < len(s) and s[i] == s[j]:
+            while 0 <= i and j < len(s) and s[i] == s[j]:
                 i -= 1
                 j += 1
             return j - i - 1
-
+        
         ans = [0, 0]
-
         for i in range(len(s)):
-            odd_substring = expand(i, i)
-            if odd_substring > ans[1] - ans[0] + 1:
-                dist = odd_substring // 2
+            odd_length = expand(i, i)
+            if odd_length > ans[1] - ans[0] + 1:
+                dist = odd_length // 2
                 ans = [i - dist, i + dist]
-            even_substring = expand(i, i + 1)
-            if even_substring > ans[1] - ans[0] + 1:
-                dist = even_substring // 2 - 1
+            even_length = expand(i, i + 1)
+            if even_length > ans[1] - ans[0] + 1:
+                dist = even_length // 2 - 1
                 ans = [i - dist, i + 1 + dist]
         
-        i, j = ans
-
-        return s[i:j + 1]
+        return s[ans[0]:ans[1] + 1]
