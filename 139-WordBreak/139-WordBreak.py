fb9ad9df74b3283773d0059797bb6185b3ad3f74
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-09 23:27:48 -0400

    File updated at 6/9/2025, 11:27:47 PM

--- 139-WordBreak/139-WordBreak.py
+++ 139-WordBreak/139-WordBreak.py
@@ -1,15 +1,15 @@
-# Last updated: 6/6/2025, 10:02:12 AM
+# Last updated: 6/9/2025, 11:27:47 PM
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False] * (len(s) + 1)
         dp[len(s)] = True
 
-        for i in range(len(s) - 1, -1, -1):
+        for i in range(len(s), -1, -1):
             for word in wordDict:
-                if i + len(word) <= len(dp) and s[i:i+len(word)] == word:
+                if i + len(word) < len(dp) and s[i:i + len(word)] == word:
+                    print(s[i:i + len(word)])
                     dp[i] = dp[i + len(word)]
                 if dp[i] == True:
-                    break 
+                    break
         
         return dp[0]
-
