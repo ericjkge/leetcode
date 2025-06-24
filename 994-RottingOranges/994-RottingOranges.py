c174faa539e034bd7d7bd4067a3921acdd23c418
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-24 17:05:23 -0400

    File updated at 6/24/2025, 5:05:22 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,25 +1,27 @@
-# Last updated: 6/7/2025, 11:12:21 AM
+# Last updated: 6/24/2025, 5:05:22 PM
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         queue = deque()
         time = fresh = 0
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
                     fresh += 1
-                elif grid[i][j] == 2:
-                    queue.append([i, j])
-        
+                elif grid[row][col] == 2:
+                    queue.append([row, col])
+                
         while queue and fresh > 0:
-            for i in range(len(queue)): # Epoch
+            for _ in range(len(queue)):
                 x, y = queue.popleft()
                 for dx, dy in directions:
                     nx, ny = x + dx, y + dy
                     if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == 1:
                         grid[nx][ny] = 2
-                        queue.append([nx, ny])
                         fresh -= 1
+                        queue.append([nx, ny])
+                    else:
+                        continue
             time += 1
 
         return time if fresh == 0 else -1
