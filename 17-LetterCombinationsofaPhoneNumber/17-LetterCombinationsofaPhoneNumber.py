9d066e8b86e477f81d2ebbc6289144656836ba35
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-24 17:54:27 -0400

    File updated at 6/24/2025, 5:54:27 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
@@ -1,35 +1,32 @@
-# Last updated: 6/5/2025, 5:47:12 PM
+# Last updated: 6/24/2025, 5:54:27 PM
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         if digits == "":
             return []
         
-        mapping = {"2":"abc", 
-                    "3":"def",
-                    "4":"ghi",
-                    "5":"jkl",
-                    "6":"mno",
-                    "7":"pqrs",
-                    "8":"tuv",
-                    "9":"wxyz"}
-        
-        results = []
+        mapping = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
 
+        ans = []
         def backtrack(index, path):
             if len(path) == len(digits):
-                results.append("".join(path))
+                ans.append("".join(path[:]))
                 return
-
+            
             possible_letters = mapping[digits[index]]
 
             for letter in possible_letters:
                 path.append(letter)
                 backtrack(index + 1, path)
                 path.pop()
-        
+
         backtrack(0, [])
-        return results
-
-
-
-            
+        return ans
