92b316f5d304f9d5b5c4d48735c975b74c004ad8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-24 18:29:51 -0400

    File updated at 6/24/2025, 6:29:51 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,8 +1,9 @@
-# Last updated: 6/24/2025, 6:29:44 PM
+# Last updated: 6/24/2025, 6:29:51 PM
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
+
         threshold = len(nums) // 2
         mapping = defaultdict()
 
