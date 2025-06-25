4e04449fbf9da50c5b666bde2d94bfca6e90b960
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-24 22:28:20 -0400

    File updated at 6/24/2025, 10:28:20 PM

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
@@ -1,21 +1,30 @@
-# Last updated: 6/24/2025, 10:28:05 PM
+# Last updated: 6/24/2025, 10:28:20 PM
+# Two heaps
 class MedianFinder:
 
     def __init__(self):
-        self.low = []
-        self.high = []
-
+        self.small = [] # the smaller half of the list, max heap
+        self.large = [] # the larger half of the list
+        
+    # Alltogether, the add operation is O(logn), The findMedian operation is O(1).
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.low, -num)
-        heapq.heappush(self.high, -heapq.heappop(self.low))
-        if len(self.low) < len(self.high):
-            heapq.heappush(self.low, -heapq.heappop(self.high))
+        # 1, 2, 3
+        # 1, 2
+        if len(self.small) == len(self.large):
+            heappush(self.large, -heappushpop(self.small, -num)) # large [1], [2, 3]
+            # print("---large----")
+            # print(self.large)
+        else:
+            heappush(self.small, -heappushpop(self.large, num)) # small [-1]
+            # print("---samll----")
+            # print(self.small)
+        
 
     def findMedian(self) -> float:
-        if len(self.low) == len(self.high):
-            return (-self.low[0] + self.high[0]) / 2
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2.0
         else:
-            return -self.low[0]
+            return float(self.large[0])
 
 
 # Your MedianFinder object will be instantiated and called as such:
