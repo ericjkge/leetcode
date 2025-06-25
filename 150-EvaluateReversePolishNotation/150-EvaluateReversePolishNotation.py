357fa8f3241fcd71a6c764c4030b07fc06ab37f4
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-25 15:52:48 -0400

    File updated at 6/25/2025, 3:52:48 PM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
@@ -1,20 +1,19 @@
-# Last updated: 6/6/2025, 3:13:43 PM
+# Last updated: 6/25/2025, 3:52:48 PM
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         operations = {
-            "+" : lambda a, b: a + b, 
-            "-" : lambda a, b: a - b,
-            "*" : lambda a, b: a * b,
-            "/" : lambda a, b: int(a / b),
+            "+": lambda a, b: a + b,
+            "-": lambda a, b: a - b,
+            "*": lambda a, b: a * b,
+            "/": lambda a, b: int(a / b)
         }
-
+        
         stack = []
         for token in tokens:
-            if token.lstrip("-").isdigit():
+            if token not in operations.keys():
                 stack.append(int(token))
             else:
                 b = stack.pop()
                 a = stack.pop()
                 stack.append(operations[token](a, b))
-
-        return stack.pop()
+        return stack[0]
