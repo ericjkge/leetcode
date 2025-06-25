d36cf77c68e1a68acb3c1bd5507b14486633d2d0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-25 18:57:31 -0400

    File updated at 6/25/2025, 6:57:30 PM

--- 278-FirstBadVersion/278-FirstBadVersion.py
+++ 278-FirstBadVersion/278-FirstBadVersion.py
@@ -1,4 +1,4 @@
-# Last updated: 6/7/2025, 11:27:55 AM
+# Last updated: 6/25/2025, 6:57:30 PM
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
@@ -6,11 +6,10 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         left = 1
         right = n
-        while left <= right:
+        while left < right:
             mid = (left + right) // 2
-            if isBadVersion(mid):
-                right = mid - 1
-            else:
+            if not isBadVersion(mid):
                 left = mid + 1
-        
-        return left
+            else:
+                right = mid
+        return right
