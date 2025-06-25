efcd7d4c14d434395d23807e855d31bbebd3d50b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-25 11:09:08 -0400

    File updated at 6/25/2025, 11:09:07 AM

--- 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.py
+++ 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 6/25/2025, 11:08:19 AM
+# Last updated: 6/25/2025, 11:09:07 AM
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -26,7 +26,7 @@ class Codec:
                 queue.append(node.right)
             else:
                 ans += "null,"
-        return ans.rstrip(",null,")
+        return ans
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
