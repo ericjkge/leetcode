1e25ceb0bf7ac034f7f405c20fb1c0b74216e172
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-28 15:44:49 -0400

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.py
@@ -0,0 +1,21 @@
+# Last updated: 6/28/2025, 3:44:49 PM
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        i = len(a) - 1
+        j = len(b) - 1
+        carry = 0
+        print(type(a[i]))
+        ans = []
+        while i >= 0 or j >= 0 or carry:
+            total = carry
+            if i >= 0:
+                total += int(a[i])
+                i -= 1
+            if j >= 0:
+                total += int(b[j])
+                j -= 1
+            
+            ans.append(str(total % 2))
+            carry = total // 2
+
+        return "".join(reversed(ans))
