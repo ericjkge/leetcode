72ccc65cb924fc18d743afa70f4efbeaf98f5428
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-28 16:30:59 -0400

    File updated at 6/28/2025, 4:30:58 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,9 +1,15 @@
-# Last updated: 5/30/2025, 2:51:10 PM
+# Last updated: 6/28/2025, 4:30:58 PM
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        a, b = 1, 2
-        for _ in range(3, n+1):
-            a, b = b, a+b
-        return b
+        memo = {}
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            
+            if i == 0 or i == 1:
+                return 1
+             
+            memo[i] = dp(i - 1) + dp(i - 2)
+            return memo[i]
+        
+        return dp(n)
