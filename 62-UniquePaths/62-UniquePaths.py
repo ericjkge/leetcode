1d33be8bb7d49c2377ae816e77f795cb3dfc539b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-28 15:06:29 -0400

    File updated at 6/28/2025, 3:06:29 PM

--- 62-UniquePaths/62-UniquePaths.py
+++ 62-UniquePaths/62-UniquePaths.py
@@ -1,10 +1,14 @@
-# Last updated: 6/28/2025, 3:02:18 PM
+# Last updated: 6/28/2025, 3:06:29 PM
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
+        memo = {}
         def dp(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+
             if i == 0 or j == 0:
                 return 1
-            return dp(i - 1, j) + dp(i, j - 1)
+            memo[(i, j)] = dp(i - 1, j) + dp(i, j - 1)
+            return memo[(i, j)]
 
         return dp(m - 1, n - 1)
