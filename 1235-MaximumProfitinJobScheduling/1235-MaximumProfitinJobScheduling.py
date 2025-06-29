3b0121d5e8dcd79b58b69fef1a31de97aed0f004
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-29 17:34:07 -0400

    File updated at 6/29/2025, 5:34:07 PM

--- 1235-MaximumProfitinJobScheduling/1235-MaximumProfitinJobScheduling.py
+++ 1235-MaximumProfitinJobScheduling/1235-MaximumProfitinJobScheduling.py
@@ -1,18 +1,17 @@
-# Last updated: 6/29/2025, 5:30:33 PM
+# Last updated: 6/29/2025, 5:34:07 PM
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
         intervals = sorted(zip(startTime, endTime, profit))
-        cache = {}
-
-        def dp(i): # max profit from job i to end
+        memo = {}
+        def dp(i):
             if i == len(intervals):
                 return 0
-
-            if i in cache:
-                return cache[i]
+            
+            if i in memo:
+                return memo[i]
 
             j = bisect.bisect(intervals, (intervals[i][1], -1, -1))
-            cache[i] = max(dp(i + 1), dp(j) + intervals[i][2])
-            return cache[i]
-
+            memo[i] = max(dp(i + 1), dp(j) + intervals[i][2])
+            return memo[i]
+        
         return dp(0)
