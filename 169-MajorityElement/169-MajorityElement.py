4fe4b9933b64bc2c206d1bbbca32ab7660ebe88f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-28 20:27:43 -0400

    File updated at 6/28/2025, 8:27:43 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,4 +1,4 @@
-# Last updated: 6/28/2025, 8:27:03 PM
+# Last updated: 6/28/2025, 8:27:43 PM
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         curr = nums[0]
@@ -6,8 +6,7 @@ class Solution:
         for i in range(1, len(nums)):
             if counter == 0:
                 curr = nums[i]
-                counter += 1        
-            elif curr == nums[i]:
+            if curr == nums[i]:
                 counter += 1
             else:
                 counter -= 1
