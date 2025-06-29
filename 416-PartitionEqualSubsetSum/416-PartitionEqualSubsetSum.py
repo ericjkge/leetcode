6c6596ee8e2dd94041449faa73e28157328d11e3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-29 15:56:59 -0400

    File updated at 6/29/2025, 3:56:59 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
@@ -1,4 +1,4 @@
-# Last updated: 6/10/2025, 12:31:02 AM
+# Last updated: 6/29/2025, 3:56:59 PM
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
@@ -8,9 +8,8 @@ class Solution:
         target = total // 2
         dp = [False] * (target + 1)
         dp[0] = True
-
         for num in nums:
-            for i in range(target, num - 1, -1):
-                dp[i] = dp[i] or dp[i - num]
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
         
         return dp[target]
