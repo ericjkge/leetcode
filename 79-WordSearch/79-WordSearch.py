d144236809d4b991578594f50f5b84422561c0ad
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-30 00:28:20 -0400

    File updated at 6/30/2025, 12:28:20 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,4 +1,4 @@
-# Last updated: 6/30/2025, 12:13:56 AM
+# Last updated: 6/30/2025, 12:28:20 AM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
@@ -10,18 +10,24 @@ class Solution:
             if i == len(word):
                 return True
             
-            if (r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != word[i] or (r, c) in path):
+            if (r < 0 or r >= rows or c < 0 or c >= cols or (r, c) in path or board[r][c] != word[i]):
                 return False
-            
+        
             path.add((r, c))
-            for dx, dy in directions:
-                if backtrack(r + dx, c + dy, i + 1):
+            for dr, dc in directions:
+                if backtrack(r + dr, c + dc, i + 1):
                     return True
             path.remove((r, c))
+
             return False
 
         for row in range(rows):
             for col in range(cols):
                 if backtrack(row, col, 0):
                     return True
+        
         return False
+
+
+# Time: O(n * m * 3^w)
+# Space: O(w)
