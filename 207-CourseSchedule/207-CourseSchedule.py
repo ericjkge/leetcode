33623aa79f84d3bf915dc6e5dac186384ab0e17e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-30 13:24:55 -0400

    File updated at 6/30/2025, 1:24:54 PM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,22 +1,22 @@
-# Last updated: 6/30/2025, 1:21:52 PM
+# Last updated: 6/30/2025, 1:24:54 PM
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = defaultdict(list)
-        in_degree = [0] * numCourses
+        in_degrees = [0] * numCourses
 
         for crs, pre in prerequisites:
             graph[pre].append(crs)
-            in_degree[crs] += 1
+            in_degrees[crs] += 1
         
-        queue = deque(i for i in range(numCourses) if in_degree[i] == 0)
+        queue = deque(i for i in range(numCourses) if in_degrees[i] == 0)
         taken = 0
 
         while queue:
             pre = queue.popleft()
             taken += 1
             for crs in graph[pre]:
-                in_degree[crs] -= 1
-                if in_degree[crs] == 0:
+                in_degrees[crs] -= 1
+                if in_degrees[crs] == 0:
                     queue.append(crs)
         
         return taken == numCourses
