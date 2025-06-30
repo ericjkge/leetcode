9194397db9c7cf6e004bd52403a62621e51b7c3b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-30 01:16:30 -0400

    Create 438-FindAllAnagramsinaString

---
+++ 438-FindAllAnagramsinaString/438-FindAllAnagramsinaString.py
@@ -0,0 +1,19 @@
+# Last updated: 6/30/2025, 1:16:30 AM
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        window = Counter()
+        p_count = Counter(p)
+        ans = []
+        n = len(p)
+
+        left = 0
+        for right in range(len(s)):
+            window[s[right]] += 1
+            if right >= n:
+                window[s[left]] -= 1
+                if window[s[left]] == 0:
+                    del window[s[left]]
+                left += 1
+            if window == p_count:
+                ans.append(left)
+        return ans
