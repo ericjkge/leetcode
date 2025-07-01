73d690902167d95ce3604f48cc0905a4b502254f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-30 21:43:06 -0400

    File updated at 6/30/2025, 9:43:06 PM

--- 721-AccountsMerge/721-AccountsMerge.py
+++ 721-AccountsMerge/721-AccountsMerge.py
@@ -1,4 +1,4 @@
-# Last updated: 6/30/2025, 9:25:41 PM
+# Last updated: 6/30/2025, 9:43:06 PM
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         graph = defaultdict(list)
@@ -9,24 +9,24 @@ class Solution:
             emails = account[1:]
             for email in emails:
                 hashmap[email] = name
-            for i in range(1, len(emails)):
+            for i in range(len(emails)): # Creates self loop on 0
                 graph[emails[0]].append(emails[i])
                 graph[emails[i]].append(emails[0])
-
-        seen = set()
-        ans = []
-
+        
         def dfs(email, component):
             seen.add(email)
             component.append(email)
             for neighbor in graph[email]:
-                if  neighbor not in seen:
+                if neighbor not in seen:
                     dfs(neighbor, component)
-
-        for email in hashmap:
+        
+        seen = set()
+        ans = []
+        for email in graph:
+            component = []
             if email not in seen:
-                component = []
                 dfs(email, component)
                 ans.append([hashmap[email]] + sorted(component))
         
         return ans
+                
