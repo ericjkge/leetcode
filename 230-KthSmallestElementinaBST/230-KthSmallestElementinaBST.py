8e52b089662235db9dff9e6e49cd6d7740192d4e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-01 13:48:10 -0400

    Create 230-KthSmallestElementinaBST

---
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.py
@@ -0,0 +1,20 @@
+# Last updated: 7/1/2025, 1:48:09 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        def dfs(node):
+            if node.left:
+                dfs(node.left)
+            ans.append(node.val)
+            if node.right:
+                dfs(node.right)
+        
+        ans = []
+        dfs(root)
+        return ans[k - 1]
+        
