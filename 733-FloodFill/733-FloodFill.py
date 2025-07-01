822815c08ce4a40ed593b872fbdc8173d2d157dd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-06-30 23:49:58 -0400

    File updated at 6/30/2025, 11:49:58 PM

--- 733-FloodFill/733-FloodFill.py
+++ 733-FloodFill/733-FloodFill.py
@@ -1,19 +1,19 @@
-# Last updated: 6/30/2025, 11:42:58 PM
+# Last updated: 6/30/2025, 11:49:58 PM
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows, cols = len(image), len(image[0])
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        stack = [(sr, sc)]
         original = image[sr][sc]
-        image[sr][sc] = color
-        seen = set((sr, sc))
+        if original == color:
+            return image
 
+        stack = [(sr, sc)]
         while stack:
             x, y = stack.pop()
+            image[x][y] = color
             for dx, dy in directions:
                 nx, ny = x + dx, y + dy
-                if 0 <= nx < len(image) and 0 <= ny < len(image[0]) and image[nx][ny] == original and (nx, ny) not in seen:
-                    seen.add((nx, ny))
+                if 0 <= nx < len(image) and 0 <= ny < len(image[0]) and image[nx][ny] == original:
                     stack.append((nx, ny))
-                    image[nx][ny] = color
         
         return image
