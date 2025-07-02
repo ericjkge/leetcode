a5add2b8f6feda6281a41ccfd3c01f8a955cce85
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-02 01:11:53 -0400

    File updated at 7/2/2025, 1:11:53 AM

--- 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.py
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.py
@@ -1,28 +1,28 @@
-# Last updated: 7/2/2025, 1:04:58 AM
+# Last updated: 7/2/2025, 1:11:53 AM
 class MyQueue:
 
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
-
+ 
     def push(self, x: int) -> None:
         self.stack1.append(x)
 
     def pop(self) -> int:
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
-        popped = self.stack2.pop()
-        while self.stack2:
-            self.stack1.append(self.stack2.pop())
-        return popped
+        self.move()
+        return self.stack2.pop()
 
     def peek(self) -> int:
-        return self.stack1[0]
+        self.move()
+        return self.stack2[-1]        
 
     def empty(self) -> bool:
-        return len(self.stack1) == 0
-        
+        return not self.stack1 and not self.stack2
 
+    def move(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
 
 # Your MyQueue object will be instantiated and called as such:
 # obj = MyQueue()
