246c5b4c0660d87269cb6711356588655d17dfb2
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-02 17:29:46 -0400

    File updated at 7/2/2025, 5:29:45 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,4 +1,4 @@
-# Last updated: 7/2/2025, 5:23:01 PM
+# Last updated: 7/2/2025, 5:29:45 PM
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         ans = [1] * len(nums)
@@ -7,10 +7,13 @@ class Solution:
         for i in range(len(nums)):
             ans[i] = prefix
             prefix *= nums[i]
-
+        
         suffix = 1
         for i in range(len(nums) - 1, -1, -1):
             ans[i] = ans[i] * suffix
             suffix *= nums[i]
         
         return ans
+
+# Time: O(n)
+# Space: O(1) (ignore ans)
