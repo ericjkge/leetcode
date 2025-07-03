1765a58e032ac5996f9ca360c9f29619625f92ce
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-03 17:34:23 -0400

    File updated at 7/3/2025, 5:34:23 PM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,24 +1,22 @@
-# Last updated: 6/29/2025, 10:35:38 PM
+# Last updated: 7/3/2025, 5:34:23 PM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-
-        s_count = Counter()
-        t_count = Counter(t)
+        
+        t_counter = Counter(t)
+        s_counter = Counter()
+        left = 0
         min_length = float("inf")
         ans = ""
 
-        left = 0
         for right in range(len(s)):
-            s_count[s[right]] += 1
-            while all(t_count[c] <= s_count[c] for c in t_count):
+            s_counter[s[right]] += 1
+            while all(t_counter[c] <= s_counter[c] for c in t_counter):
                 if right - left + 1 < min_length:
                     ans = s[left:right + 1]
                     min_length = right - left + 1
-                s_count[s[left]] -= 1
+                s_counter[s[left]] -= 1
                 left += 1
-
+                
         return ans
-
-        
