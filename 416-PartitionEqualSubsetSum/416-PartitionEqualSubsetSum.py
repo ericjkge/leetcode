4317eba68a80a2a58468a08e66365ee067d0afdc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-03 16:57:09 -0400

    File updated at 7/3/2025, 4:57:08 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
@@ -1,4 +1,4 @@
-# Last updated: 7/3/2025, 4:53:26 PM
+# Last updated: 7/3/2025, 4:57:08 PM
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         total = sum(nums)
@@ -6,13 +6,19 @@ class Solution:
             return False
         
         target = total // 2
-        @cache
+
+        memo = {}
         def dfs(i, curr):
             if curr == target:
                 return True
             if curr > target or i == len(nums):
                 return False
+            
+            if (i, curr) in memo:
+                return memo[(i, curr)]
 
-            return dfs(i + 1, curr + nums[i]) or dfs(i + 1, curr)
+            memo[(i, curr)] = dfs(i + 1, curr + nums[i]) or dfs(i + 1, curr) 
+
+            return memo[(i, curr)]
 
         return dfs(0, 0) 
