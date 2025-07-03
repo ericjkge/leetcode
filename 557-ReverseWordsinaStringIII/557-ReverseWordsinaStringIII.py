c227f94ab2c0739dec87667c91049399ac22ee61
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-03 18:13:15 -0400

    File updated at 7/3/2025, 6:13:15 PM

--- 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.py
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.py
@@ -1,8 +1,16 @@
-# Last updated: 7/3/2025, 5:53:56 PM
+# Last updated: 7/3/2025, 6:13:15 PM
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split(" ")
-        ans = []
-        for word in words:
-            ans.append(word[::-1])
-        return " ".join(ans)
+        chars = list(s)
+        start = 0
+
+        for end in range(len(s) + 1):
+            if end == len(s) or chars[end] == " ":
+                l, r = start, end - 1
+                while l < r:
+                    chars[l], chars[r] = chars[r], chars[l]
+                    l += 1
+                    r -= 1
+                start = end + 1
+
+        return "".join(chars)
