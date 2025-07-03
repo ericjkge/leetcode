e6f04d025572251f273f8d4406ca2ac67964d0b1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-03 16:15:55 -0400

    File updated at 7/3/2025, 4:15:55 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 7/3/2025, 4:15:42 PM
+# Last updated: 7/3/2025, 4:15:55 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
