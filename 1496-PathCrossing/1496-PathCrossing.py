07cab45bd49204f7518abeb4732f0b0b760336be
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 10:21:54 -0400

    Create 1496-PathCrossing

---
+++ 1496-PathCrossing/1496-PathCrossing.py
@@ -0,0 +1,24 @@
+# Last updated: 7/4/2025, 10:21:54 AM
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        pos = (0, 0)
+        seen = set()
+        seen.add((0, 0))
+        directions = {
+            "N":(0, 1),
+            "S":(0, -1),
+            "E":(1, 0),
+            "W":(-1, 0)
+        }
+
+        for p in path:
+            x, y = pos
+            dx, dy = directions[p]
+            nx, ny = x + dx, y + dy
+            pos = (nx, ny)
+            if (nx, ny) in seen:
+                return True
+            seen.add((nx, ny))
+
+        
+        return False
