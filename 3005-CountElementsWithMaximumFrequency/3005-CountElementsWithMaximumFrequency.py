2024986f1b115dd37a09baff55e2457982dc9f05
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 10:51:02 -0400

    File updated at 7/4/2025, 10:51:02 AM

--- 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
+++ 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.py
@@ -1,10 +1,6 @@
-# Last updated: 7/4/2025, 10:50:42 AM
+# Last updated: 7/4/2025, 10:51:02 AM
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
         count = Counter(nums)
         max_freq = max(count.values())
-        total = 0
-        for key in count:
-            if count[key] == max_freq:
-                total += count[key]
-        return total
+        return sum(count[key] for key in count if count[key] == max_freq)
