31a6357add44ae262af2a09b5665d3510e9b8ca3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 11:04:53 -0400

    File updated at 7/4/2025, 11:04:53 AM

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.py
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.py
@@ -1,4 +1,4 @@
-# Last updated: 7/4/2025, 11:04:47 AM
+# Last updated: 7/4/2025, 11:04:53 AM
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
         counter = Counter(arr)
