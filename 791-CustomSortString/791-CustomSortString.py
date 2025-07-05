a3da04357388e5f49f9c32c4d2c1e1f35f7dce4c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 22:41:52 -0400

    File updated at 7/4/2025, 10:41:51 PM

--- 791-CustomSortString/791-CustomSortString.py
+++ 791-CustomSortString/791-CustomSortString.py
@@ -1,4 +1,4 @@
-# Last updated: 7/4/2025, 10:41:46 PM
+# Last updated: 7/4/2025, 10:41:51 PM
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
         priority = {char:index for index, char in enumerate(order)} # index, char
