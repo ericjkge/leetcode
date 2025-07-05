fb67bd0b7efee5fdb1760b1c2676c4701e613479
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 22:41:46 -0400

    Create 791-CustomSortString

---
+++ 791-CustomSortString/791-CustomSortString.py
@@ -0,0 +1,13 @@
+# Last updated: 7/4/2025, 10:41:46 PM
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        priority = {char:index for index, char in enumerate(order)} # index, char
+        string = list(s)
+        ans = []
+
+        for char in priority:
+            while char in string:
+                ans.append(char)
+                string.remove(char)
+        
+        return "".join(ans + string)
