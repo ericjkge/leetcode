3f05256ad34dfa3cf4b20f0791bdf71a187a0612
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 23:04:13 -0400

    Create 1657-DetermineifTwoStringsAreClose

---
+++ 1657-DetermineifTwoStringsAreClose/1657-DetermineifTwoStringsAreClose.py
@@ -0,0 +1,5 @@
+# Last updated: 7/4/2025, 11:04:13 PM
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+
+        return set(word1) == set(word2) and sorted(Counter(word1).values()) == sorted(Counter(word2).values())
