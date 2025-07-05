295938f36eeb3975b4b1bea7a5a3ae7e5695c09d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-04 23:34:18 -0400

    File updated at 7/4/2025, 11:34:18 PM

--- 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.py
+++ 232-ImplementQueueusingStacks/232-ImplementQueueusingStacks.py
@@ -1,10 +1,10 @@
-# Last updated: 7/2/2025, 1:11:53 AM
+# Last updated: 7/4/2025, 11:34:18 PM
 class MyQueue:
 
     def __init__(self):
         self.stack1 = []
         self.stack2 = []
- 
+
     def push(self, x: int) -> None:
         self.stack1.append(x)
 
@@ -14,11 +14,11 @@ class MyQueue:
 
     def peek(self) -> int:
         self.move()
-        return self.stack2[-1]        
+        return self.stack2[-1]
 
     def empty(self) -> bool:
         return not self.stack1 and not self.stack2
-
+    
     def move(self):
         if not self.stack2:
             while self.stack1:
