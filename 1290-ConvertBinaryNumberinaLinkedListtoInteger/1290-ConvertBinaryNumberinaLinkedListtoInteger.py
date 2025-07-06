efa1e071f595d92fc3322254db9fd620d2bdf6d5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-05 21:30:16 -0400

    File updated at 7/5/2025, 9:30:16 PM

--- 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
+++ 1290-ConvertBinaryNumberinaLinkedListtoInteger/1290-ConvertBinaryNumberinaLinkedListtoInteger.py
@@ -1,4 +1,4 @@
-# Last updated: 7/5/2025, 9:24:45 PM
+# Last updated: 7/5/2025, 9:30:16 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,15 +6,10 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
-        binary = []
-        node = head
-        while node:
-            binary.append(node.val)
-            node = node.next
-        
-        total = power = 0
-        for i in range(len(binary) - 1, -1, -1):
-            total += 2 ** power if binary[i] == 1 else 0
-            power += 1
-        
-        return total
+        num = head.val
+        while head.next:
+            num = num * 2 + head.next.val
+            head = head.next
+
+        return num
+
