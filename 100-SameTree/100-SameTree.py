fd46bd6329eadec2c2a307c454a4196139e248e1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-07 15:22:03 -0400

    Create 100-SameTree

---
+++ 100-SameTree/100-SameTree.py
@@ -0,0 +1,28 @@
+# Last updated: 7/7/2025, 3:22:03 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        stack1 = []
+        stack2 = []
+
+        def dfs(node, stack):
+            if not node:
+                stack.append("1000000")
+                return
+            dfs(node.left, stack)
+            dfs(node.right, stack)
+            stack.append(node.val)
+            
+        dfs(p, stack1)
+        print(stack1)
+        dfs(q, stack2)
+        print(stack2)
+        return stack1 == stack2
+            
+
+        
