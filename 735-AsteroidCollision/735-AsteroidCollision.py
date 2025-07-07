547a5b4eab6e6c75bdf59156c8cd23776e2bc837
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-06 23:23:18 -0400

    File updated at 7/6/2025, 11:23:17 PM

--- 735-AsteroidCollision/735-AsteroidCollision.py
+++ 735-AsteroidCollision/735-AsteroidCollision.py
@@ -1,18 +1,17 @@
-# Last updated: 7/6/2025, 11:21:34 PM
+# Last updated: 7/6/2025, 11:23:17 PM
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
         for a in asteroids:
             flag = True
             while stack and stack[-1] > 0 and a < 0:
-                if abs(stack[-1]) < abs(a):
+                if stack[-1] < abs(a):
                     stack.pop()
                     continue
-                elif abs(stack[-1]) == abs(a):
+                elif stack[-1] == abs(a):
                     stack.pop()
                 flag = False
                 break
             if flag:
                 stack.append(a)
-        
         return stack
