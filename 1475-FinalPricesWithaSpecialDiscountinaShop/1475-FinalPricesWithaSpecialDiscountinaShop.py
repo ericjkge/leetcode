c5d1a1226a835df1ac1a5d61a9a5aedf5ba00bde
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-07 10:23:48 -0400

    File updated at 7/7/2025, 10:23:48 AM

--- 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.py
+++ 1475-FinalPricesWithaSpecialDiscountinaShop/1475-FinalPricesWithaSpecialDiscountinaShop.py
@@ -1,18 +1,10 @@
-# Last updated: 7/7/2025, 10:20:14 AM
+# Last updated: 7/7/2025, 10:23:48 AM
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         # remove next smallest value from current
-        left = 0
-        ans = []
-        while left < len(prices):
-            flag = True
-            for right in range(left + 1, len(prices)):
-                if prices[right] <= prices[left]:
-                    ans.append(prices[left] - prices[right])
-                    flag = False
+        for i in range(len(prices)):
+            for j in range(i + 1, len(prices)):
+                if prices[j] <= prices[i]:
+                    prices[i] = prices[i] - prices[j]
                     break
-            if flag:
-                ans.append(prices[left])
-            left += 1
-        return ans
-        # [8 12 18 20 23]
+        return prices
