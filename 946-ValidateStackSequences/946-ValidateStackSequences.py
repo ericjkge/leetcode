9621ff7e992e64f20bba029885cc2d2016fe60c5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-06 22:16:52 -0400

    File updated at 7/6/2025, 10:16:52 PM

--- 946-ValidateStackSequences/946-ValidateStackSequences.py
+++ 946-ValidateStackSequences/946-ValidateStackSequences.py
@@ -1,4 +1,4 @@
-# Last updated: 7/6/2025, 10:14:03 PM
+# Last updated: 7/6/2025, 10:16:52 PM
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
         stack = []
@@ -7,16 +7,10 @@ class Solution:
         for p in pushed:
             if p != popped[counter]:
                 stack.append(p)
-
             else:
                 counter += 1
                 while stack and stack[-1] == popped[counter]:
                     stack.pop()
                     counter += 1
 
-        while stack:
-            if stack.pop() == popped[counter]:
-                counter += 1
-            else:
-                return False
-        return True
+        return not stack
