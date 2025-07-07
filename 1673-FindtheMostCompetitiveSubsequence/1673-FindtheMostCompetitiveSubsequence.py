248103aa40be74f326d28de8a5619aea1587635d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-07 12:13:22 -0400

    File updated at 7/7/2025, 12:13:22 PM

--- 1673-FindtheMostCompetitiveSubsequence/1673-FindtheMostCompetitiveSubsequence.py
+++ 1673-FindtheMostCompetitiveSubsequence/1673-FindtheMostCompetitiveSubsequence.py
@@ -1,4 +1,4 @@
-# Last updated: 7/7/2025, 12:13:18 PM
+# Last updated: 7/7/2025, 12:13:22 PM
 class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
         stack = []
