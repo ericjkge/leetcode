b59d62c17938172892fd52a006c5bf0cecdec880
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-08 10:58:49 -0400

    File updated at 7/8/2025, 10:58:49 AM

--- 1512-NumberofGoodPairs/1512-NumberofGoodPairs.py
+++ 1512-NumberofGoodPairs/1512-NumberofGoodPairs.py
@@ -1,9 +1,8 @@
-# Last updated: 7/4/2025, 2:33:20 PM
+# Last updated: 7/8/2025, 10:58:49 AM
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         counter = Counter(nums)
         ans = 0
         for _, freq in counter.items():
             ans += freq * (freq - 1) // 2
-
         return ans
