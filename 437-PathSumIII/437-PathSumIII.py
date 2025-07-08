5635d13224f14d6142bc3febbd330e8723548fe8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-08 13:29:09 -0400

    File updated at 7/8/2025, 1:29:09 PM

--- 437-PathSumIII/437-PathSumIII.py
+++ 437-PathSumIII/437-PathSumIII.py
@@ -1,4 +1,4 @@
-# Last updated: 7/8/2025, 12:53:54 PM
+# Last updated: 7/8/2025, 1:29:09 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,31 +7,22 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        if not root:
-            return 0
-            
-        counter = 0
-
         def dfs(node, total):
             nonlocal counter
             if not node:
                 return
-            
+
             total += node.val
-            if total == targetSum:
-                counter += 1
+            counter += prefix[total - targetSum]
+            prefix[total] += 1
+                        
             dfs(node.left, total)
             dfs(node.right, total)
 
-        stack = []
-        stack.append(root)
-
-        while stack:
-            node = stack.pop()
-            dfs(node, 0)
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-
+            prefix[total] -= 1
+        
+        counter = 0
+        prefix = defaultdict(int)
+        prefix[0] = 1 # this is to account for when total == targetSum right?
+        dfs(root, 0)
         return counter
