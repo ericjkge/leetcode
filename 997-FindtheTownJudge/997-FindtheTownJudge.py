a8fc290a4ecf35513824144c99d0cf22e1130619
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-09 01:03:39 -0400

    File updated at 7/9/2025, 1:03:39 AM

--- 997-FindtheTownJudge/997-FindtheTownJudge.py
+++ 997-FindtheTownJudge/997-FindtheTownJudge.py
@@ -1,16 +1,18 @@
-# Last updated: 7/9/2025, 12:56:56 AM
+# Last updated: 7/9/2025, 1:03:39 AM
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        ans = -1
-        trusts = defaultdict(list)
-        trusted_by = defaultdict(list)
-        for a, b in trust:
-            trusts[a].append(b)
-            trusted_by[b].append(a)
+        if len(trust) < n - 1:
+            return -1
         
+        indegree = [0] * n
+        outdegree = [0] * n
 
+        for a, b in trust:
+            indegree[b - 1] += 1
+            outdegree[a - 1] += 1
+        
         for i in range(1, n + 1):
-            if not trusts[i] and len(trusted_by[i]) == n - 1:
-                ans = i
-
-        return ans
+            if indegree[i - 1] == n - 1 and outdegree[i - 1] == 0:
+                return i
+        
+        return -1
