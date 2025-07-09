b35a9b55ad29b0d76283a8e06d55a20b27332f84
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-09 11:34:50 -0400

    File updated at 7/9/2025, 11:34:50 AM

--- 450-DeleteNodeinaBST/450-DeleteNodeinaBST.py
+++ 450-DeleteNodeinaBST/450-DeleteNodeinaBST.py
@@ -1,31 +1,33 @@
-# Last updated: 7/9/2025, 11:27:42 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Last updated: 7/9/2025, 11:34:50 AM
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
             return None
 
-        if root.val > key:
+        if key < root.val:
             root.left = self.deleteNode(root.left, key)
-        elif root.val < key:
+        elif key > root.val:
             root.right = self.deleteNode(root.right, key)
         else:
-            # Handle two cases where only right or left subtree
+            # Node with only one child or no child
             if not root.left:
                 return root.right
-            elif not root.right:
+            if not root.right:
                 return root.left
-            
-            # Find the min from right subtree
-            node = root.right
-            while node.left:
-                node = node.left
-            root.val = node.val
-            root.right = self.deleteNode(root.right, node.val)
-    
+
+            # Find inorder successor (leftmost in right subtree)
+            successor = root.right
+            parent = root
+            while successor.left:
+                parent = successor
+                successor = successor.left
+
+            # Replace root with successor node
+            if parent != root:
+                parent.left = successor.right
+                successor.right = root.right
+            successor.left = root.left
+
+            return successor
+
         return root
