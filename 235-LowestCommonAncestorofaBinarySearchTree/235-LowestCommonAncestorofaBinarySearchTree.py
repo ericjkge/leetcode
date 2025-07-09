327a6de2a47994483aa422063699f6dbab26975e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-08 23:37:39 -0400

    File updated at 7/8/2025, 11:37:38 PM

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 7/8/2025, 11:34:10 PM
+# Last updated: 7/8/2025, 11:37:38 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
@@ -8,12 +8,9 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
-
-        if root.val == p.val or root.val == q.val or p.val < root.val < q.val or q.val < root.val < p.val:
-            return root
-        elif root.val > p.val and root.val > q.val:
+        if root.val > p.val and root.val > q.val:
             return self.lowestCommonAncestor(root.left, p, q)
-        else:
+        elif root.val < p.val and root.val < q.val:
             return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
