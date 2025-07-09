0d1b39b29c90433b79a6dd0a58662d91a6625607
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-09 14:09:13 -0400

    File updated at 7/9/2025, 2:09:12 PM

--- 463-IslandPerimeter/463-IslandPerimeter.py
+++ 463-IslandPerimeter/463-IslandPerimeter.py
@@ -1,25 +1,14 @@
-# Last updated: 7/9/2025, 1:23:44 PM
+# Last updated: 7/9/2025, 2:09:12 PM
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        stack = []
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         perimeter = 0
-        seen = set()
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    stack.append((i, j))
-                    seen.add((i, j))
-                    break
-        
-        while stack:
-            r, c = stack.pop()
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid[0]) or grid[nr][nc] == 0:
-                    perimeter += 1
-                elif (nr, nc) not in seen:
-                    seen.add((nr, nc))
-                    stack.append((nr, nc))
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    for dr, dc in directions:
+                        nr, nc = r + dr, c + dc
+                        if nr < 0 or nr >= len(grid) or nc < 0 or nc >= len(grid[0]) or grid[nr][nc] == 0:
+                            perimeter += 1
         
         return perimeter
