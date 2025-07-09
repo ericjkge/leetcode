7de0c85268055e1581a74a95bca59e7cbe78a4f8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-09 12:34:07 -0400

    Create 1615-MaximalNetworkRank

---
+++ 1615-MaximalNetworkRank/1615-MaximalNetworkRank.py
@@ -0,0 +1,18 @@
+# Last updated: 7/9/2025, 12:34:07 PM
+class Solution:
+    def maximalNetworkRank(self, n: int, roads: List[List[int]]) -> int:
+        graph = defaultdict(list)
+        for a, b in roads:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                rank = len(graph[i]) + len(graph[j])
+                if j in graph[i]:
+                    rank -= 1
+                ans = max(ans, rank)
+
+        print(graph)
+        return ans
