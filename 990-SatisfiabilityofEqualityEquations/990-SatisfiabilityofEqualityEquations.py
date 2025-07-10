f98be5e0a28cb0f83d1ca75f9615e51e808b895c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-09 20:57:35 -0400

    File updated at 7/9/2025, 8:57:35 PM

--- 990-SatisfiabilityofEqualityEquations/990-SatisfiabilityofEqualityEquations.py
+++ 990-SatisfiabilityofEqualityEquations/990-SatisfiabilityofEqualityEquations.py
@@ -1,37 +1,37 @@
-# Last updated: 7/9/2025, 5:32:36 PM
-from collections import defaultdict
-
+# Last updated: 7/9/2025, 8:57:35 PM
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        graph = defaultdict(list)
-        for eq in equations:
-            if eq[1:3] == '==':
-                a, b = eq[0], eq[3]
-                graph[a].append(b)
-                graph[b].append(a)
-
-        # DFS to assign component ids
-        comp = {}
-        curr_id = 0
+        graph = defaultdict(set)
+        for a, sign, _, b in equations:
+            if sign == "=":
+                graph[a].add(b)
+                graph[b].add(a)
+        
+        seen = set()
+        components = defaultdict(int)
+        counter = 0
 
         def dfs(node):
-            comp[node] = curr_id
-            for nei in graph[node]:
-                if nei not in comp:
-                    dfs(nei)
-
-        for var in graph:
-            if var not in comp:
-                dfs(var)
-                curr_id += 1
-
-        # Check "!=" constraints
-        for eq in equations:
-            if eq[1:3] == '!=':
-                a, b = eq[0], eq[3]
-                if a == b:
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    components[neighbor] = counter
+                    seen.add(neighbor)
+                    dfs(neighbor)
+        
+        for a, _, _, b in equations:
+            if a not in seen:
+                components[a] = counter
+                seen.add(a)
+                dfs(a)
+            if b not in seen:
+                counter += 1
+                components[b] = counter
+                seen.add(b)
+                dfs(b)
+            counter += 1
+        
+        for a, sign, _, b in equations:
+            if sign == "!":
+                if components[a] == components[b]:
                     return False
-                if a in comp and b in comp and comp[a] == comp[b]:
-                    return False
-
         return True
