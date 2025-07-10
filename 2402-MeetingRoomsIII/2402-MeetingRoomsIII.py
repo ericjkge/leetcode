f7763e3015973007801453add4b64c8e3af4a52b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-10 17:01:58 -0400

    Create 2402-MeetingRoomsIII

---
+++ 2402-MeetingRoomsIII/2402-MeetingRoomsIII.py
@@ -0,0 +1,28 @@
+# Last updated: 7/10/2025, 5:01:58 PM
+class Solution:
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        counter = [0] * n
+        free_rooms = [i for i in range(n)]
+        heapq.heapify(free_rooms)
+        used_rooms = []
+
+        for start, end in sorted(meetings):
+            while used_rooms and used_rooms[0][0] <= start:
+                heapq.heappush(free_rooms, heapq.heappop(used_rooms)[1])
+            if free_rooms:
+                room = heapq.heappop(free_rooms)
+                counter[room] += 1
+                heapq.heappush(used_rooms, (end, room))
+            else:
+                duration = end - start
+                start = used_rooms[0][0]
+                end = start + duration
+                room = heapq.heappop(used_rooms)[1]
+                counter[room] += 1
+                heapq.heappush(used_rooms, (end, room))
+        
+        max_counter = max(counter)
+        for i in range(len(counter)):
+            if counter[i] == max_counter:
+                return i
+
