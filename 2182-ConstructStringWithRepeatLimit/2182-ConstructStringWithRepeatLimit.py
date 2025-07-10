230e94f1d003d596e5ab9bcb43105270316a963b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-10 00:10:47 -0400

    Create 2182-ConstructStringWithRepeatLimit

---
+++ 2182-ConstructStringWithRepeatLimit/2182-ConstructStringWithRepeatLimit.py
@@ -0,0 +1,28 @@
+# Last updated: 7/10/2025, 12:10:46 AM
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        counter = Counter(s)
+        heap = [(-ord(letter), letter) for letter in counter]
+        heapq.heapify(heap)
+        ans = []
+
+        while heap:
+            _, letter1 = heapq.heappop(heap)
+            use = min(repeatLimit, counter[letter1])
+
+            ans.extend([letter1 * use])
+            counter[letter1] -= use
+
+            if counter[letter1] > 0:
+                if not heap:
+                    break
+
+                _, letter2 = heapq.heappop(heap)
+                ans.append(letter2)
+                counter[letter2] -= 1
+                
+                if counter[letter2] > 0:
+                    heapq.heappush(heap, (-ord(letter2), letter2))
+                heapq.heappush(heap, (-ord(letter1), letter1))
+
+        return "".join(ans)
