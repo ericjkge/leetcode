ac13ac2c3fb0494aba21242a2b537ee7e19250cd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-12 15:58:33 -0400

    File updated at 7/12/2025, 3:58:33 PM

--- 455-AssignCookies/455-AssignCookies.py
+++ 455-AssignCookies/455-AssignCookies.py
@@ -1,15 +1,14 @@
-# Last updated: 7/12/2025, 3:57:05 PM
+# Last updated: 7/12/2025, 3:58:33 PM
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         g.sort() # remove if unnecessary
         s.sort()
-        g_index = s_index = ans = 0
+        g_index = s_index = 0
         while s_index < len(s) and g_index < len(g):
             if s[s_index] >= g[g_index]:
-                ans += 1
                 s_index += 1
                 g_index += 1
             else:
                 s_index += 1
 
-        return ans
+        return g_index
