14aca8baad269a256b1fc9054de9a7cef4e78eb7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-17 16:45:29 +0800

    File updated at 7/17/2025, 4:45:29 PM

--- 1005-MaximizeSumOfArrayAfterKNegations/1005-MaximizeSumOfArrayAfterKNegations.py
+++ 1005-MaximizeSumOfArrayAfterKNegations/1005-MaximizeSumOfArrayAfterKNegations.py
@@ -1,23 +1,17 @@
-# Last updated: 7/17/2025, 4:43:08 PM
+# Last updated: 7/17/2025, 4:45:29 PM
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
         nums.sort()
-        counter = 0
         for i in range(len(nums)):
-            if nums[i] < 0:
+            if k > 0 and nums[i] < 0:
                 nums[i] = -nums[i]
-                counter +=1
-            if counter == k:
-                break
+                k -= 1
         
-        if counter < k:
-            min_val = min(nums)
-            if (k - counter) % 2 == 1:
-                pos = nums.index(min_val)
-                nums[pos] = -nums[pos]
-        
+        nums.sort()
+        if k % 2 == 1:
+            nums[0] = -nums[0]
+                    
         return sum(nums)
             
-        # if negative, flip most negative to positive
-        # if zero, do remainder on zero
-        # else, do remainder on smallest
+# Time: O(nlogn)
+# Space: O(1)
