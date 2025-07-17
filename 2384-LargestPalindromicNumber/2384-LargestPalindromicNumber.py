88dfdabfeb3ab7a41ac343cdb9ff58ffb28cd54c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-17 21:50:25 +0800

    File updated at 7/17/2025, 9:50:25 PM

--- 2384-LargestPalindromicNumber/2384-LargestPalindromicNumber.py
+++ 2384-LargestPalindromicNumber/2384-LargestPalindromicNumber.py
@@ -1,4 +1,4 @@
-# Last updated: 7/17/2025, 9:37:23 PM
+# Last updated: 7/17/2025, 9:50:25 PM
 class Solution:
     def largestPalindromic(self, num: str) -> str:
         counter = Counter(num)
@@ -7,7 +7,6 @@ class Solution:
 
         for num in range(9, -1, -1):
             num_char = str(num)
-
             if num_char in counter:
                 freq = counter[num_char]
 
@@ -16,12 +15,11 @@ class Solution:
                         continue
                     else:
                         first_half.append(num_char * (freq // 2))
-
+                
                 if freq % 2 and not center:
                     center = num_char
         
-        if not center and not first_half:
+        if not first_half and not center:
             return "0"
-
+        
         return "".join(first_half + [center] + first_half[::-1])
-                    
