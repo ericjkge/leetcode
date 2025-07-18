4f7d0bbe86a765ae4e9e5df18ca8b88e9a2bee20
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-18 22:31:43 +0800

    Create 1855-MaximumDistanceBetweenaPairofValues

---
+++ 1855-MaximumDistanceBetweenaPairofValues/1855-MaximumDistanceBetweenaPairofValues.py
@@ -0,0 +1,15 @@
+# Last updated: 7/18/2025, 10:31:43 PM
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        ans = 0
+        for i in range(len(nums1)):
+            left, right = i, len(nums2)
+            while left < right:
+                mid = left + (right - left) // 2
+                if nums1[i] <= nums2[mid]:
+                    left = mid + 1
+                else:
+                    right = mid
+            ans = max(ans, left - i - 1)
+        
+        return ans
