ce88205f80dd76c91f7d2e9ddf4bbec2125792a1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-18 14:33:48 +0800

    File updated at 7/18/2025, 2:33:47 PM

--- 1024-VideoStitching/1024-VideoStitching.py
+++ 1024-VideoStitching/1024-VideoStitching.py
@@ -1,4 +1,4 @@
-# Last updated: 7/18/2025, 2:32:52 PM
+# Last updated: 7/18/2025, 2:33:47 PM
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
         counter = 0
@@ -7,14 +7,12 @@ class Solution:
         curr_end = next_end = 0
 
         for start, end in clips:
-            if start <= curr_end:
-                next_end = max(next_end, end)
-            else:
+            if start > curr_end:
                 curr_end = next_end
                 counter += 1
                 if start > curr_end:
                     return -1
-                next_end = max(next_end, end)
+            next_end = max(next_end, end)
             if next_end >= time:
                 counter += 1
                 break
