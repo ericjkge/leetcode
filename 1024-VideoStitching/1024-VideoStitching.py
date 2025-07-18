6ac8d654f7dc6b5d21ae5d5c0d548632921f2983
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-18 14:29:44 +0800

    Create 1024-VideoStitching

---
+++ 1024-VideoStitching/1024-VideoStitching.py
@@ -0,0 +1,35 @@
+# Last updated: 7/18/2025, 2:29:43 PM
+class Solution:
+    def videoStitching(self, clips: List[List[int]], time: int) -> int:
+        counter = 1
+        clips.sort()
+        print(clips)
+
+        curr_end = zero_counter = 0
+
+        while zero_counter < len(clips) and clips[zero_counter][0] == 0:
+            curr_end = max(curr_end, clips[zero_counter][1])
+            zero_counter += 1
+
+        next_end = curr_end
+
+        if curr_end >= time: # Edge case for initial full clip
+            return counter
+
+        for start, end in clips:
+            print(counter, start, end)
+            if start <= curr_end:
+                next_end = max(next_end, end)
+            else:
+                curr_end = next_end
+                counter += 1
+                if start > curr_end:
+                    return -1
+                next_end = max(next_end, end)
+            if next_end >= time:
+                counter += 1
+                break
+        
+        if next_end >= time:
+            return counter
+        return -1
