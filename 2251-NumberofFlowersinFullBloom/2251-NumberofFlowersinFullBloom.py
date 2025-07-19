06164edec0c1d0c3cf58d01e138f3f975b7046be
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-19 21:14:41 +0800

    Create 2251-NumberofFlowersinFullBloom

---
+++ 2251-NumberofFlowersinFullBloom/2251-NumberofFlowersinFullBloom.py
@@ -0,0 +1,33 @@
+# Last updated: 7/19/2025, 9:14:40 PM
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        startTimes = []
+        endTimes = []
+        for start, end in flowers:
+            startTimes.append(start)
+            endTimes.append(end + 1)
+
+        startTimes.sort()
+        endTimes.sort()
+
+        print(startTimes, endTimes)
+
+        ans = []
+
+        def binarySearch(arr, target): # goes out by 1
+            left, right = 0, len(arr)
+            while left < right:
+                mid = left + (right - left) // 2
+                if arr[mid] <= target:
+                    left = mid + 1
+                else:
+                    right = mid
+            return left
+
+        for person in people:
+            ans.append(binarySearch(startTimes, person) - binarySearch(endTimes, person))
+
+        return ans
+
+        
+        
