a40e649773c81466cb58d290990d1e0c96468eb1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-19 23:48:17 +0800

    File updated at 7/19/2025, 11:48:16 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,17 +1,17 @@
-# Last updated: 6/25/2025, 12:16:44 PM
+# Last updated: 7/19/2025, 11:48:16 PM
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def backtrack(index, path):
-            if index == len(nums):
+        def backtrack(path):
+            if len(path) == len(nums):
                 ans.append(path[:])
                 return
             
             for num in nums:
                 if num not in path:
                     path.append(num)
-                    backtrack(index + 1, path)
+                    backtrack(path)
                     path.pop()
 
-        backtrack(0, [])
+        backtrack([])
         return ans
