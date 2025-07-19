4b28af4eef7f2705210070175ad6c7b6679aa96a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-19 14:12:38 +0800

    File updated at 7/19/2025, 2:12:37 PM

--- 2250-CountNumberofRectanglesContainingEachPoint/2250-CountNumberofRectanglesContainingEachPoint.py
+++ 2250-CountNumberofRectanglesContainingEachPoint/2250-CountNumberofRectanglesContainingEachPoint.py
@@ -1,4 +1,4 @@
-# Last updated: 7/19/2025, 1:57:59 PM
+# Last updated: 7/19/2025, 2:12:37 PM
 class Solution:
     def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
         def binarySearch(arr, target):
@@ -10,20 +10,20 @@ class Solution:
                 else:
                     left = mid + 1
             return left
-
-        ans = []
+        
         hashmap = defaultdict(list)
         for l, h in rectangles:
             hashmap[h].append(l)
-
+        
         for lengths in hashmap.values():
             lengths.sort()
         
+        ans = []
         for x, y in points:
             count = 0
             for height in range(y, 101):
                 lengths = hashmap[height]
-                count += len(lengths) - binarySearch(lengths, x)
+                count += len(lengths) - binarySearch(lengths, x) 
             ans.append(count)
-
-        return ans 
+        
+        return ans
