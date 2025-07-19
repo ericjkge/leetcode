fc52d3987d0dfd4e8d6ed3a73f39a4fb7167b6c3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-19 13:58:00 +0800

    File updated at 7/19/2025, 1:57:59 PM

--- 2250-CountNumberofRectanglesContainingEachPoint/2250-CountNumberofRectanglesContainingEachPoint.py
+++ 2250-CountNumberofRectanglesContainingEachPoint/2250-CountNumberofRectanglesContainingEachPoint.py
@@ -1,36 +1,7 @@
-# Last updated: 7/18/2025, 11:52:46 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Last updated: 7/19/2025, 1:57:59 PM
 class Solution:
-    def closestNodes(self, root: Optional[TreeNode], queries: List[int]) -> List[List[int]]:
-        arr = []
-
-        def dfs(node):
-            if not node:
-                return
-
-            dfs(node.left)
-            arr.append(node.val)
-            dfs(node.right)
-        
-        dfs(root)
-        ans = [[-1, -1] for _ in range(len(queries))]
-        
-        def binarySearchMin(arr, target):
-            left, right = 0, len(arr)
-            while left < right:
-                mid = left + (right - left) // 2
-                if arr[mid] <= target:
-                    left = mid + 1
-                else:
-                    right = mid
-            return left - 1 if left > 0 else -1
-
-        def binarySearchMax(arr, target):
+    def countRectangles(self, rectangles: List[List[int]], points: List[List[int]]) -> List[int]:
+        def binarySearch(arr, target):
             left, right = 0, len(arr)
             while left < right:
                 mid = left + (right - left) // 2
@@ -38,12 +9,21 @@ class Solution:
                     right = mid
                 else:
                     left = mid + 1
-            return left if left < len(arr) else -1
+            return left
 
-        for i in range(len(queries)):
-            mini = binarySearchMin(arr, queries[i])
-            maxi = binarySearchMax(arr, queries[i])
-            ans[i][0] = arr[mini] if mini != -1 else -1
-            ans[i][1] = arr[maxi] if maxi != -1 else -1
+        ans = []
+        hashmap = defaultdict(list)
+        for l, h in rectangles:
+            hashmap[h].append(l)
 
-        return ans
+        for lengths in hashmap.values():
+            lengths.sort()
+        
+        for x, y in points:
+            count = 0
+            for height in range(y, 101):
+                lengths = hashmap[height]
+                count += len(lengths) - binarySearch(lengths, x)
+            ans.append(count)
+
+        return ans 
