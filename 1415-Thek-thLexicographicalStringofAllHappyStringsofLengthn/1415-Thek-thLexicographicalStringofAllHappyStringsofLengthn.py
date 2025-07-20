7e98175cf6983b98a81277338ebce71d60896d2d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-20 22:27:16 +0800

    Create 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn

---
+++ 1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn/1415-Thek-thLexicographicalStringofAllHappyStringsofLengthn.py
@@ -0,0 +1,23 @@
+# Last updated: 7/20/2025, 10:27:16 PM
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        ans = []
+        
+        def backtrack(path):
+            if len(path) == n:
+                ans.append(path[:])
+                return
+            
+            for letter in ["a", "b", "c"]:
+                if not path or path[-1] != letter:
+                    path.append(letter)
+                    backtrack(path)
+                    path.pop()
+            
+        backtrack([])
+
+        if len(ans) < k:
+            return ""
+
+        ans.sort()
+        return "".join(ans[k - 1])
