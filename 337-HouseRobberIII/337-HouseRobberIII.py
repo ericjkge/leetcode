0c4e44625fc8038e895b7acccb8fed2286c8a42e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-22 16:33:58 +0800

    Create 337-HouseRobberIII

---
+++ 337-HouseRobberIII/337-HouseRobberIII.py
@@ -0,0 +1,22 @@
+# Last updated: 7/22/2025, 4:33:57 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rob(self, root: Optional[TreeNode]) -> int:
+        def dp(node):
+            if not node:
+                return (0, 0)
+            
+            left = dp(node.left)
+            right = dp(node.right)
+
+            rob = node.val + left[1] + right[1] 
+            skip = max(left) + max(right)
+
+            return (rob, skip)
+
+        return max(dp(root))
