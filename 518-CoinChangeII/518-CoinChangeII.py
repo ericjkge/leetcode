7a9931e3ad21ce9c1d382579e9cf2ad80c258385
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-22 14:45:51 +0800

    File updated at 7/22/2025, 2:45:51 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,18 +1,17 @@
-# Last updated: 7/22/2025, 2:12:45 PM
+# Last updated: 7/22/2025, 2:45:51 PM
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        coins.sort(reverse=True)
-        n = len(coins)
-        @cache
-        def dp(i, j): # No. of ways to make up i using coins[j:]
-            if i == 0:
-                return 1
-            
-            total = 0
-            for index in range(j, n):
-                if coins[index] <= i:
-                    total += dp(i - coins[index], index)
-            
-            return total
+        dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
         
-        return dp(amount, 0)
+        for r in range(len(coins) + 1):
+            dp[r][0] = 1
+        
+        for r in range(1, len(coins) + 1):
+            for c in range(1, amount + 1):
+                target = c - coins[r - 1]
+                if target < 0:
+                    dp[r][c] = dp[r - 1][c]
+                else:
+                    dp[r][c] = dp[r - 1][c] + dp[r][target]
+        
+        return dp[-1][-1]
