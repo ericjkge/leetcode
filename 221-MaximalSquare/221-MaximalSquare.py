78ff4141f5d326dbf6840ee7a99cd80a096cce0a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-23 23:50:53 +0800

    File updated at 7/23/2025, 11:50:52 PM

--- 221-MaximalSquare/221-MaximalSquare.py
+++ 221-MaximalSquare/221-MaximalSquare.py
@@ -1,26 +1,26 @@
-# Last updated: 7/23/2025, 11:15:08 PM
+# Last updated: 7/23/2025, 11:50:52 PM
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        # dynamic programming: bottom up
-        # recursive: top down
-        ROWS, COLS = len(matrix), len(matrix[0])
-        cache = {}  # map each (r, c) -> maxLength of square
+        rows = len(matrix) - 1
+        cols = len(matrix[0]) - 1
+        memo = {}
 
-        # T: O(m*n) M: O(m*n)
-        def helper(r, c):
-            if r >= ROWS or c >= COLS:
+        def dp(i, j): # Side length of max square with top left at (i, j)
+            if i > rows or j > cols:
                 return 0
+            
+            if (i, j) in memo:
+                return memo[(i, j)]
 
-            if (r, c) not in cache:
-                down = helper(r + 1, c)
-                right = helper(r, c + 1)
-                diag = helper(r + 1, c + 1)
+            memo[(i, j)] = 0
+            down = dp(i + 1, j)
+            right = dp(i, j + 1)
+            diag = dp(i + 1, j + 1)
 
-                cache[(r, c)] = 0
-                if matrix[r][c] == "1":
-                    cache[(r, c)] = 1 + min(down, right, diag)
-
-            return cache[(r, c)]
-
-        helper(0, 0)
-        return max(cache.values()) ** 2
+            if matrix[i][j] == "1":
+                memo[(i, j)] = 1 + min(down, right, diag)
+            
+            return memo[(i, j)]
+        
+        dp(0, 0)
+        return max(memo.values()) ** 2
