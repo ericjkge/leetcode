221c47aab276b35e3692263c9f736a9c7005c1a4
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-26 10:20:25 +0800

    Create 513-FindBottomLeftTreeValue

---
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.py
@@ -0,0 +1,22 @@
+# Last updated: 7/26/2025, 10:20:24 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        queue = deque()
+        queue.append(root)
+        left = None
+        while queue:
+            left = queue[0].val
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        
+        return left
