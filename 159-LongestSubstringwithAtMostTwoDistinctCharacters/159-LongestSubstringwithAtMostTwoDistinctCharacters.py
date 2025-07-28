d2cbc7f51d44decbe90bbebf25888aa63d0ff10b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-28 18:12:48 +0800

    File updated at 7/28/2025, 6:12:47 PM

--- 159-LongestSubstringwithAtMostTwoDistinctCharacters/159-LongestSubstringwithAtMostTwoDistinctCharacters.py
+++ 159-LongestSubstringwithAtMostTwoDistinctCharacters/159-LongestSubstringwithAtMostTwoDistinctCharacters.py
@@ -1,4 +1,4 @@
-# Last updated: 7/28/2025, 6:12:43 PM
+# Last updated: 7/28/2025, 6:12:47 PM
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
         hashmap = {}
