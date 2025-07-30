05df040c477fe19527df9e1b7d7703cd447cc296
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-30 17:16:38 +0800

    File updated at 7/30/2025, 5:16:37 PM

--- 146-LRUCache/146-LRUCache.py
+++ 146-LRUCache/146-LRUCache.py
@@ -1,49 +1,46 @@
-# Last updated: 7/2/2025, 11:34:55 PM
-class Node:
-    def __init__(self, key, value):
+# Last updated: 7/30/2025, 5:16:37 PM
+class ListNode:
+    def __init__(self, key=None, val=None):
         self.key = key
-        self.val = value
-        self.left = self.right = None
+        self.val = val
+        self.next = self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
         self.cache = {}
-        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.capacity = capacity
+        self.left, self.right = ListNode(), ListNode()
         self.left.next, self.right.prev = self.right, self.left
-
+        
     def add(self, node):
         nxt = self.left.next
-        self.left.next = node
-        nxt.prev = node
-        node.prev, node.next = self.left, nxt
-
+        self.left.next = nxt.prev = node
+        node.next, node.prev = nxt, self.left
+    
     def remove(self, node):
-        prev = node.prev
-        nxt = node.next
+        prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
-
+        
     def get(self, key: int) -> int:
         if key in self.cache:
             self.remove(self.cache[key])
             self.add(self.cache[key])
             return self.cache[key].val
         return -1
-
+        
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.add(self.cache[key])
-
-        if len(self.cache) > self.capacity:
-            lru = self.right.prev
-            self.remove(lru)
-            del self.cache[lru.key]
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            self.add(self.cache[key])
+            self.cache[key].val = value
+        else:
+            node = ListNode(key, value)
+            self.add(node)
+            self.cache[key] = node
+            
+            if len(self.cache) > self.capacity:
+                lru = self.right.prev
+                self.remove(lru)
+                if lru.key in self.cache:
+                    del self.cache[lru.key]
