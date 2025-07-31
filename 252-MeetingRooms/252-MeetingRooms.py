a42588c346c4034286033b0b7fa31bd09a86c4fe
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-07-31 23:22:31 +0800

    Create 252-MeetingRooms

---
+++ 252-MeetingRooms/252-MeetingRooms.py
@@ -0,0 +1,11 @@
+# Last updated: 7/31/2025, 11:22:30 PM
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+        
+        for i in range(len(intervals)):
+            start, end = intervals[i]
+            if i > 0 and start < intervals[i - 1][1]:
+                return False
+        
+        return True
