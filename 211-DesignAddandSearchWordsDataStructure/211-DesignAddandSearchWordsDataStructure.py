99c2e9a9d98bbdc68d1c29e07342285b87c9e760
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-03 23:45:31 +0800

    File updated at 8/3/2025, 11:45:30 PM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
@@ -1,4 +1,4 @@
-# Last updated: 8/3/2025, 11:37:39 PM
+# Last updated: 8/3/2025, 11:45:30 PM
 class TrieNode():
     
     def __init__(self):
@@ -18,22 +18,21 @@ class WordDictionary:
             node = node.children[w]
         node.is_end = True
     
-    def dfs(self, node, string):
-        for i in range(len(string)):
-            if string[i] == ".":
+    def dfs(self, node, word, start):
+        for i in range(start, len(word)):
+            if word[i] == ".":
                 for child in node.children.values():
-                    if self.dfs(child, string[i + 1:]):
+                    if self.dfs(child, word, i + 1):
                         return True
-            if string[i] not in node.children:
                 return False
-            node = node.children[string[i]]
+            if word[i] not in node.children:
+                return False
+            node = node.children[word[i]]
         return node.is_end
         
     def search(self, word: str) -> bool:
-        node = self.root
-        return self.dfs(node, word)
+        return self.dfs(self.root, word, 0)
     
-
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
