db9348b2085e26dfcfd073d7b5a6792d8e43aaba
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-04 21:16:18 +0800

    Create 490-TheMaze

---
+++ 490-TheMaze/490-TheMaze.py
@@ -0,0 +1,26 @@
+# Last updated: 8/4/2025, 9:16:18 PM
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        rows, cols = len(maze), len(maze[0])
+        seen = set()
+        
+        def roll(r, c, dr, dc):
+            while 0 <= r + dr < rows and 0 <= c + dc < cols and maze[r + dr][c + dc] == 0:
+                r = r + dr
+                c = c + dc
+            return r, c
+        
+        def dfs(r, c):
+            if [r, c] == destination:
+                return True
+            for dr, dc in directions:
+                nr, nc = roll(r, c, dr, dc)
+                if (nr, nc) not in seen:
+                    seen.add((nr, nc))
+                    if dfs(nr, nc):
+                        return True
+            return False
+        
+        seen.add((start[0], start[1]))
+        return dfs(start[0], start[1])
