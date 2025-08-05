11e7cd4d66af3eb26e857de977f7b3344138cab1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-05 23:51:25 +0800

    File updated at 8/5/2025, 11:51:24 PM

--- 302-SmallestRectangleEnclosingBlackPixels/302-SmallestRectangleEnclosingBlackPixels.py
+++ 302-SmallestRectangleEnclosingBlackPixels/302-SmallestRectangleEnclosingBlackPixels.py
@@ -1,28 +1,64 @@
-# Last updated: 8/5/2025, 11:02:32 PM
+# Last updated: 8/5/2025, 11:51:24 PM
 class Solution:
     def minArea(self, image: List[List[str]], x: int, y: int) -> int:
         rows, cols = len(image), len(image[0])
-        self.left, self.right = cols - 1, 0
-        self.top, self.bottom = rows - 1, 0
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        seen = set()
+
+        def hasPixelCol(col):
+            return any(image[r][col] == "1" for r in range(rows))
+
+        def hasPixelRow(row):
+            return any(image[row][c] == "1" for c in range(cols))
+
+        # Find left
+        lo, hi = 0, y
+        while lo + 1 < hi:
+            mid = (lo + hi) // 2
+            if hasPixelCol(mid):
+                hi = mid
+            else:
+                lo = mid
+        if hasPixelCol(lo):
+            left = lo
+        else:
+            left = hi
+
+        # Find right
+        lo, hi = y, cols - 1
+        while lo + 1 < hi:
+            mid = (lo + hi) // 2
+            if hasPixelCol(mid):
+                lo = mid
+            else:
+                hi = mid
+        if hasPixelCol(hi):
+            right = hi
+        else:
+            right = lo
         
-        def dfs(r, c):
-            if c > self.right:
-                self.right = c
-            if c < self.left:
-                self.left = c
-            if r > self.bottom:
-                self.bottom = r
-            if r < self.top:
-                self.top = r
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and image[nr][nc] == "1":
-                    seen.add((nr, nc))
-                    dfs(nr, nc)
+        # Find top
+        lo, hi = 0, x
+        while lo + 1 < hi:
+            mid = (lo + hi) // 2
+            if hasPixelRow(mid):
+                hi = mid
+            else:
+                lo = mid
+        if hasPixelRow(lo):
+            top = lo
+        else:
+            top = hi
         
-        seen.add((x, y))
-        dfs(x, y)
-        
-        return (self.right - self.left + 1) * (self.bottom - self.top + 1)
+        # Find bottom
+        lo, hi = x, rows - 1
+        while lo + 1 < hi:
+            mid = (lo + hi) // 2
+            if hasPixelRow(mid):
+                lo = mid
+            else:
+                hi = mid
+        if hasPixelRow(hi):
+            bottom = hi
+        else:
+            bottom = lo
+
+        return (right - left + 1) * (bottom - top + 1)
