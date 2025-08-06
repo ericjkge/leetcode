a1d7b5ccd2281a8271470d4cd413814461c6d3da
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-06 19:29:14 +0800

    File updated at 8/6/2025, 7:29:14 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,15 +1,14 @@
-# Last updated: 8/6/2025, 7:26:49 PM
+# Last updated: 8/6/2025, 7:29:14 PM
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        prefix = [0, nums[0]]
-        for i in range(1, len(nums)):
-            prefix.append(prefix[-1] + nums[i])
+        prefix = 0
+        hashmap = {0: 1}
+        ans = 0
 
-        counter = 0
-        seen = {0: 1}
-        for p in prefix[1:]:
-            if p - k in seen:
-                counter += seen[p - k]
-            seen[p] = seen.get(p, 0) + 1
-        
-        return counter
+        for num in nums:
+            prefix += num
+            if prefix - k in hashmap:
+                ans += hashmap[prefix - k]
+            hashmap[prefix] = hashmap.get(prefix, 0) + 1
+
+        return ans
