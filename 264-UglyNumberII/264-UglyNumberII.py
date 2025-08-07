a747142527063abfb43853b2667cd8c4a64e1757
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-07 23:30:45 +0800

    Create 264-UglyNumberII

---
+++ 264-UglyNumberII/264-UglyNumberII.py
@@ -0,0 +1,15 @@
+# Last updated: 8/7/2025, 11:30:45 PM
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        heap = [1]
+        seen = {1}
+        factors = [2, 3, 5]
+        
+        for _ in range(n):
+            ans = heapq.heappop(heap)
+            for f in factors:
+                if ans * f not in seen:
+                    seen.add(ans * f)
+                    heapq.heappush(heap, ans * f)
+            
+        return ans
