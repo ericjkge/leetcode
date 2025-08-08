7928451dad99abe85cf687c0767c617454388f15
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-08 19:56:41 +0800

    Create 857-MinimumCosttoHireKWorkers

---
+++ 857-MinimumCosttoHireKWorkers/857-MinimumCosttoHireKWorkers.py
@@ -0,0 +1,21 @@
+# Last updated: 8/8/2025, 7:56:40 PM
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        workers = sorted([(w / q, q) for w, q in zip(wage, quality)])
+        
+        ans = float("inf")
+        max_heap = []
+        total_q = 0
+        
+        for ratio, q in workers:
+            heapq.heappush(max_heap, -q)
+            total_q += q
+            
+            if len(max_heap) > k:
+                total_q += heapq.heappop(max_heap)
+            
+            if len(max_heap) == k:
+                ans = min(ans, total_q * ratio)
+                print(total_q, ratio)
+            
+        return ans
