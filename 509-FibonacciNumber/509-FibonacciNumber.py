39ebb1007632b8e4781313d1bea46140e71fced4
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-08 23:02:30 +0800

    File updated at 8/8/2025, 11:02:30 PM

--- 509-FibonacciNumber/509-FibonacciNumber.py
+++ 509-FibonacciNumber/509-FibonacciNumber.py
@@ -1,11 +1,15 @@
-# Last updated: 8/8/2025, 11:01:53 PM
+# Last updated: 8/8/2025, 11:02:30 PM
 class Solution:
     def fib(self, n: int) -> int:
-        @cache
+        memo = {}
         def dp(i):
             if i == 0 or i == 1:
                 return i
             
-            return dp(i - 1) + dp(i - 2)
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = dp(i - 1) + dp(i - 2)
+            return memo[i]
 
         return dp(n)
