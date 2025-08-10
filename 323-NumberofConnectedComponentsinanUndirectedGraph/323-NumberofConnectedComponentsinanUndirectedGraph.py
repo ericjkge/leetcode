be972144c37787d70942c8a2d6e54e3bc3c9b8e7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-10 23:48:42 +0800

    File updated at 8/10/2025, 11:48:42 PM

--- 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
+++ 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
@@ -1,24 +1,30 @@
-# Last updated: 8/4/2025, 6:41:48 PM
+# Last updated: 8/10/2025, 11:48:42 PM
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        graph = {i: [] for i in range(n)}
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
+        class UnionFind:
+            def __init__(self, size):
+                self.root = [i for i in range(size)]
+                self.rank = [1] * size
+            
+            def find(self, x):
+                if x == self.root[x]:
+                    return x
+                self.root[x] = self.find(self.root[x])
+                return self.root[x]
+            
+            def union(self, a, b):
+                ra, rb = self.find(a), self.find(b)
+                if ra != rb:
+                    if self.rank[ra] > self.rank[rb]:
+                        self.root[rb] = ra
+                    elif self.rank[ra] < self.rank[rb]:
+                        self.root[ra] = rb
+                    else:
+                        self.root[rb] = ra
+                        self.rank[ra] += 1
         
-        ans = 0
-        seen = set()
-        
-        def dfs(node):
-            for neighbor in graph[node]:
-                if neighbor not in seen:
-                    seen.add(neighbor)
-                    dfs(neighbor)
-        
-        for i in range(n): 
-            if i not in seen:
-                ans += 1
-                seen.add(i)
-                dfs(i)
-        
-        return ans
+        uf = UnionFind(n)
+        for start, end in edges:
+            uf.union(start, end)
+        root = set([uf.find(i) for i in range(n)])
+        return len(root)
