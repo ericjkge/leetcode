666862e6fe73bd7d853249d61893659d7a50a48d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-11 15:49:34 +0800

    Create 261-GraphValidTree

---
+++ 261-GraphValidTree/261-GraphValidTree.py
@@ -0,0 +1,23 @@
+# Last updated: 8/11/2025, 3:49:34 PM
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+        
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        
+        # Condition 1: E = V - 1
+        if len(edges) != n - 1:
+            return False
+        
+        # Condition 2: Connected (any two conditions imply the third)
+        seen = set()
+        def dfs(node):
+            seen.add(node)
+            for nei in graph[node]:
+                if nei not in seen:
+                    dfs(nei)
+        
+        dfs(0)
+        return True if len(seen) == n else False
