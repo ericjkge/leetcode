b3276c68770e8f55ea094b7de8dc83504c81676f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-12 22:21:57 +0800

    File updated at 8/12/2025, 10:21:56 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,17 +1,15 @@
-# Last updated: 7/22/2025, 2:45:51 PM
+# Last updated: 8/12/2025, 10:21:56 PM
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp = [[0] * (amount + 1) for _ in range(len(coins) + 1)]
-        
-        for r in range(len(coins) + 1):
-            dp[r][0] = 1
-        
-        for r in range(1, len(coins) + 1):
-            for c in range(1, amount + 1):
-                target = c - coins[r - 1]
-                if target < 0:
-                    dp[r][c] = dp[r - 1][c]
-                else:
-                    dp[r][c] = dp[r - 1][c] + dp[r][target]
-        
-        return dp[-1][-1]
+        coins.sort(reverse=True)
+        @cache
+        def dp(amount, index):
+            if amount < 0 or index == len(coins):
+                return 0
+
+            if amount == 0:
+                return 1
+            
+            return dp(amount - coins[index], index) + dp(amount, index + 1)
+
+        return dp(amount, 0)
