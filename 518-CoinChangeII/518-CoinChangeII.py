17f72e2d6d2f5793e34f43dc3a7056fd563a9362
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-12 22:22:02 +0800

    File updated at 8/12/2025, 10:22:02 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,7 +1,6 @@
-# Last updated: 8/12/2025, 10:21:56 PM
+# Last updated: 8/12/2025, 10:22:02 PM
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        coins.sort(reverse=True)
         @cache
         def dp(amount, index):
             if amount < 0 or index == len(coins):
