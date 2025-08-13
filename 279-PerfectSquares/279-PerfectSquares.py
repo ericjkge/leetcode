0c389b475c81283d772a30bc42ef0d2c9f0924e6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-14 00:14:12 +0800

    Create 279-PerfectSquares

---
+++ 279-PerfectSquares/279-PerfectSquares.py
@@ -0,0 +1,17 @@
+# Last updated: 8/14/2025, 12:14:11 AM
+class Solution:
+    def numSquares(self, n: int) -> int:
+        squares = [i**2 for i in range(1, int(n ** 0.5) + 1)]
+
+        @cache
+        def dp(num):
+            if num == 0:
+                return 0
+            
+            root = int(num ** 0.5)
+            if root * root == num:
+                return 1
+
+            return 1 + min(dp(num - square) for square in squares if square <= num)
+
+        return dp(n)
