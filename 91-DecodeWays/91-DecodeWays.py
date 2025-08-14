83683c9e2625abf1f5c7b6559322204c404ae73a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-14 15:53:52 +0800

    File updated at 8/14/2025, 3:53:51 PM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,16 +1,17 @@
-# Last updated: 8/14/2025, 3:46:25 PM
+# Last updated: 8/14/2025, 3:53:51 PM
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
         if n == 0:
             return 0
+        
         dp = [0] * (n + 1)
         dp[0] = 1
         dp[1] = 1 if s[0] != "0" else 0
 
         for i in range(2, n + 1):
             one = s[i - 1]
-            two = s[i - 2: i]
+            two = s[i - 2:i]
             if one != "0":
                 dp[i] += dp[i - 1]
             if two[0] != "0" and 10 <= int(two) <= 26:
