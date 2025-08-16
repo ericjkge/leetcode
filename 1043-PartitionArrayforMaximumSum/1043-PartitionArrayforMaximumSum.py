b3e90de1e187ff501367b79c1d6daa0bff086f36
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-16 23:27:20 +0800

    File updated at 8/16/2025, 11:27:19 PM

--- 1043-PartitionArrayforMaximumSum/1043-PartitionArrayforMaximumSum.py
+++ 1043-PartitionArrayforMaximumSum/1043-PartitionArrayforMaximumSum.py
@@ -1,16 +1,13 @@
-# Last updated: 8/16/2025, 11:17:19 PM
+# Last updated: 8/16/2025, 11:27:19 PM
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
-        @cache
-        def dp(i):
-            if i == n:
-                return 0
-            best = 0
-            curr = 0
-            for j in range(1, min(k, n - i) + 1):
-                curr = max(curr, arr[i + j - 1])
-                best = max(best, curr * j + dp(i + j))
-            return best
+
+        dp = [0] * (n + 1)
+        for i in range(1, n + 1):
+            curMax = 0
+            for j in range(1, min(k, i) + 1):
+                curMax = max(curMax, arr[i - j])
+                dp[i] = max(dp[i], curMax * j + dp[i - j])
         
-        return dp(0)
+        return dp[n]
