61dba004a004fc59546c875fee00427bbc101227
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-16 21:00:09 +0800

    Create 647-PalindromicSubstrings

---
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -0,0 +1,19 @@
+# Last updated: 8/16/2025, 9:00:08 PM
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+
+        @cache
+        def dp(i, j):
+            if i >= j:
+                return True
+            if s[i] != s[j]:
+                return False
+            return dp(i + 1, j - 1) # Check smaller palindromic substring
+
+        ans = 0
+        for i in range(n):
+            for j in range(i, n):
+                ans += dp(i, j)
+
+        return ans
