04ae57bfc87b31e9804f2b033ba0285c9e5181dd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-19 00:51:48 +0800

    File updated at 8/19/2025, 12:51:48 AM

--- 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.py
+++ 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.py
@@ -1,18 +1,18 @@
-# Last updated: 8/19/2025, 12:48:15 AM
+# Last updated: 8/19/2025, 12:51:48 AM
 class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         MOD = 10**9 + 7
 
         @cache
         def dp(i, j): # num ways to reach sum j with i dice
-            if i == 0 and j == target:
-                return 1
-            if i == 0 or j == target:
+            if i == 0:
+                return 1 if j == 0 else 0
+            if j < 0:
                 return 0
 
             ways = 0
             for num in range(1, k + 1):
-                ways += dp(i - 1, j + num)
+                ways += dp(i - 1, j - num)
             return ways % MOD
         
-        return dp(n, 0)
+        return dp(n, target)
