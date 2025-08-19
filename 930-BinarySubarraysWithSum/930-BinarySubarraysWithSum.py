916eaa509e80211deba0f5a2bfc21bda86d68117
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-19 23:14:33 +0800

    File updated at 8/19/2025, 11:14:32 PM

--- 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.py
+++ 930-BinarySubarraysWithSum/930-BinarySubarraysWithSum.py
@@ -1,12 +1,14 @@
-# Last updated: 7/4/2025, 3:45:20 PM
+# Last updated: 8/19/2025, 11:14:32 PM
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        count = defaultdict(int)
-        prefix = ans = 0
-        count[0] = 1
+        prefix = 0
+        freq = defaultdict(int)
+        freq[0] = 1
+        ans = 0
+
         for num in nums:
             prefix += num
-            ans += count[prefix - goal]
-            count[prefix] += 1
+            ans += freq[prefix - goal]
+            freq[prefix] += 1
         
         return ans
