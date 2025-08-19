d949f3e89d9c3ad02b1dd8947feb1c17c28413f7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-19 22:18:09 +0800

    File updated at 8/19/2025, 10:18:08 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,16 +1,15 @@
-# Last updated: 8/18/2025, 12:34:53 AM
+# Last updated: 8/19/2025, 10:18:08 PM
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n + 1)] # dp[i][j] = num. of ways to make i from coins[j:]\
-        for i in range(n):
-            dp[i][0] = 1
 
-        for i in range(n - 1, -1, -1):
-            for j in range(1, amount + 1):
-                if coins[i] > j:
-                    dp[i][j] = dp[i + 1][j]
-                else:
-                    dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]]
+        @cache
+        def dp(i, j):
+            if i < 0 or j == n:
+                return 0
+            if i == 0:
+                return 1
             
-        return dp[0][amount]
+            return dp(i - coins[j], j) + dp(i, j + 1)
+
+        return dp(amount, 0)
