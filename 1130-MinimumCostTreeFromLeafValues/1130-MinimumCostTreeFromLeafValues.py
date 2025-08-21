fd6f05b129ce0ed88e0b3a54a694038e965b9444
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-22 00:22:13 +0800

    File updated at 8/22/2025, 12:22:12 AM

--- 1130-MinimumCostTreeFromLeafValues/1130-MinimumCostTreeFromLeafValues.py
+++ 1130-MinimumCostTreeFromLeafValues/1130-MinimumCostTreeFromLeafValues.py
@@ -1,4 +1,4 @@
-# Last updated: 8/22/2025, 12:19:02 AM
+# Last updated: 8/22/2025, 12:22:12 AM
 class Solution:
     def mctFromLeafValues(self, arr: List[int]) -> int:
         n = len(arr)
@@ -7,10 +7,10 @@ class Solution:
         def dp(i, j):
             if i == j:
                 return 0
-
+            
             smallest = float("inf")
             for k in range(i, j):
                 smallest = min(smallest, max(arr[i:k + 1]) * max(arr[k + 1:j + 1]) + dp(i, k) + dp(k + 1, j))
             return smallest
-
+        
         return dp(0, n - 1)
