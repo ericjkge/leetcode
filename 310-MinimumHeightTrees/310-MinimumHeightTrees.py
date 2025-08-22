25399bacf8f5aa56fb2a6ec5b3dddb27c12bcfa8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-23 00:16:25 +0800

    File updated at 8/23/2025, 12:16:24 AM

--- 310-MinimumHeightTrees/310-MinimumHeightTrees.py
+++ 310-MinimumHeightTrees/310-MinimumHeightTrees.py
@@ -1,28 +1,27 @@
-# Last updated: 6/28/2025, 12:42:51 AM
+# Last updated: 8/23/2025, 12:16:24 AM
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
 
         graph = defaultdict(list)
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+        degree = [0] * n
 
-        edge_count = {}
-        leaves = deque()
-        for source, neighbors in graph.items():
-            if len(neighbors) == 1:
-                leaves.append(source)
-            edge_count[source] = len(neighbors)
-        
-        while leaves:
-            if n <= 2:
-                return list(leaves)
-            for _ in range(len(leaves)):
-                leaf = leaves.popleft()
-                n -= 1
-                for neighbor in graph[leaf]:
-                    edge_count[neighbor] -= 1
-                    if edge_count[neighbor] == 1:
-                        leaves.append(neighbor)
+        for start, end in edges:
+            graph[start].append(end)
+            graph[end].append(start)
+            degree[start] += 1
+            degree[end] += 1
+
+        queue = deque([i for i in range(n) if degree[i] == 1])
+        counter = n
+        while counter > 2:
+            counter -= len(queue)
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                for neighbor in graph[node]:
+                    degree[neighbor] -= 1
+                    if degree[neighbor] == 1:
+                        queue.append(neighbor)
+                        
+        return [val for val in queue]
