198f6ae60dca62f0fc39a95843a170cb5bc6c860
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-24 11:48:21 +0800

    File updated at 8/24/2025, 11:48:20 AM

--- 621-TaskScheduler/621-TaskScheduler.py
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -1,4 +1,4 @@
-# Last updated: 8/24/2025, 11:47:18 AM
+# Last updated: 8/24/2025, 11:48:20 AM
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         freq = Counter(tasks)
@@ -6,7 +6,7 @@ class Solution:
         heapq.heapify(max_heap)
         cooldown = deque()
         time = 0
-        print(max_heap)
+        
         while max_heap or cooldown:
             time += 1
 
@@ -16,38 +16,7 @@ class Solution:
                 if freq != 0:
                     cooldown.append((time + n, freq))
             
-            if cooldown:
-                if cooldown[0][0] == time:
-                    _, freq = cooldown.popleft()
-                    heapq.heappush(max_heap, freq)
-
-        return time
-
-
-
-
-
-
-
-
-
-
-
-        freq = Counter(tasks)
-        max_heap = [-f for f in freq.values()]
-        heapq.heapify(max_heap)
-        cooldown = deque()
-        time = 0
-
-        while max_heap or cooldown:
-            time += 1
-
-            if max_heap:
-                count = heapq.heappop(max_heap) + 1
-                if count != 0:
-                    cooldown.append((time + n, count))
-            
             if cooldown and cooldown[0][0] == time:
                 heapq.heappush(max_heap, cooldown.popleft()[1])
-        
+
         return time
