c64127bba039d2dc13c58e11107cbff2e119e16b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-24 10:50:55 +0800

    File updated at 8/24/2025, 10:50:54 AM

--- 224-BasicCalculator/224-BasicCalculator.py
+++ 224-BasicCalculator/224-BasicCalculator.py
@@ -1,30 +1,27 @@
-# Last updated: 7/1/2025, 12:05:41 PM
+# Last updated: 8/24/2025, 10:50:54 AM
 class Solution:
     def calculate(self, s: str) -> int:
         ans, num, sign = 0, 0, 1
         stack = []
-        
+
         for c in s:
             if c.isdigit():
                 num = num * 10 + int(c)
             elif c in "+-":
-                ans += (sign * num)
-                if c == "+":
-                    sign = 1
-                if c == "-":
-                    sign = -1
+                ans += (sign * num) # Finish adding/subtracting prev number
+                sign = 1 if c == "+" else -1
                 num = 0
             elif c == "(":
                 stack.append(ans)
                 stack.append(sign)
-                ans = 0
-                sign = 1
+                ans, sign = 0, 1
             elif c == ")":
-                ans += (num * sign)
-                ans *= stack.pop()
-                ans += stack.pop()
-                num = 0
-
-        ans += (sign * num) # final number
+                ans += (sign * num)
+                prev_sign, prev_ans = stack.pop(), stack.pop()
+                ans = prev_ans + (prev_sign * ans)
+                num, sign = 0, 1
+            else:
+                continue # whitespace
+        
+        ans += (sign * num)
         return ans
-
