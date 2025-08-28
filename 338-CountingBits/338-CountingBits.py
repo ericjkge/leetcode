2b59f6030a9f6d08ad3f101bde036cdedd7bb7b7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-28 10:49:31 -0400

    File updated at 8/28/2025, 10:49:31 AM

--- 338-CountingBits/338-CountingBits.py
+++ 338-CountingBits/338-CountingBits.py
@@ -1,11 +1,11 @@
-# Last updated: 8/28/2025, 10:45:39 AM
+# Last updated: 8/28/2025, 10:49:31 AM
 class Solution:
     def countBits(self, n: int) -> List[int]:
         def popcount(num):
             count = 0
             while num:
-                num &= num - 1
-                count += 1
+                count += num & 1
+                num >>= 1
             return count
 
         return [popcount(i) for i in range(n + 1)]
