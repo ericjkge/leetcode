ed9cd53ee4f1f9b2d4f9d59bbd885411112c6bbf
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-29 17:28:39 -0400

    File updated at 8/29/2025, 5:28:39 PM

--- 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 7/29/2025, 5:39:20 PM
+# Last updated: 8/29/2025, 5:28:39 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,10 +6,8 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow = fast = head
-        
+        slow, fast = head, head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
         return slow
