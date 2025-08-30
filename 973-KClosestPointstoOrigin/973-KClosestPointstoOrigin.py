7f4aebe60100726b21de39290a05ca346fe9643d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-29 23:23:16 -0400

    File updated at 8/29/2025, 11:23:15 PM

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
@@ -1,11 +1,11 @@
-# Last updated: 8/29/2025, 11:23:00 PM
+# Last updated: 8/29/2025, 11:23:15 PM
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
 
         for x, y in points:
             dist = x ** 2 + y ** 2
-            heapq.heappush(heap, (-dist, [x, y]))
+            heapq.heappush(heap, (-dist, (x, y)))
             if len(heap) > k:
                 heapq.heappop(heap)
         
