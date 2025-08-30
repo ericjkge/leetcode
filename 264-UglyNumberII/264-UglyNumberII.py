53a07e784d6512bf07ef090a6383aa0ad21a4c6a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-29 23:44:56 -0400

    File updated at 8/29/2025, 11:44:56 PM

--- 264-UglyNumberII/264-UglyNumberII.py
+++ 264-UglyNumberII/264-UglyNumberII.py
@@ -1,15 +1,15 @@
-# Last updated: 8/7/2025, 11:30:48 PM
+# Last updated: 8/29/2025, 11:44:56 PM
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         heap = [1]
-        seen = {1}
+        seen = set([1])
         factors = [2, 3, 5]
-        
+
         for _ in range(n):
             ans = heapq.heappop(heap)
             for f in factors:
-                if ans * f not in seen:
-                    seen.add(ans * f)
-                    heapq.heappush(heap, ans * f)
-            
+                if f * ans not in seen:
+                    seen.add(f * ans)
+                    heapq.heappush(heap, f * ans)    
+
         return ans
