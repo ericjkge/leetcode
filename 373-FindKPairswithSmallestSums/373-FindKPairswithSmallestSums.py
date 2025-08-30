9ee0d2f800a473fb9d9a074025937623c1d2ea7e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-08-30 07:29:31 -0400

    File updated at 8/30/2025, 7:29:31 AM

--- 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.py
+++ 373-FindKPairswithSmallestSums/373-FindKPairswithSmallestSums.py
@@ -1,18 +1,19 @@
-# Last updated: 7/10/2025, 4:16:59 PM
+# Last updated: 8/30/2025, 7:29:31 AM
 class Solution:
     def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        k = min(k, len(nums1) * len(nums2))
         heap = [(nums1[0] + nums2[0], 0, 0)]
+        seen = set([(0, 0)])
         ans = []
-        seen = set((0, 0))
 
         for _ in range(k):
-            _, i, j = heapq.heappop(heap)
-            ans.append([nums1[i], nums2[j]])
-            if i + 1 < len(nums1) and (i + 1, j) not in seen:
-                heapq.heappush(heap, (nums1[i + 1] + nums2[j], i + 1, j))
-                seen.add((i + 1, j))
-            if j + 1 < len(nums2) and (i, j + 1) not in seen:
-                heapq.heappush(heap, (nums1[i] + nums2[j + 1], i, j + 1))
-                seen.add((i, j + 1))
+            _, p1, p2 = heapq.heappop(heap)
+            ans.append((nums1[p1], nums2[p2]))
+            if p1 + 1 < len(nums1) and (p1 + 1, p2) not in seen:
+                seen.add((p1 + 1, p2))
+                heapq.heappush(heap, (nums1[p1 + 1] + nums2[p2], p1 + 1, p2))
+            if p2 + 1 < len(nums2) and (p1, p2 + 1) not in seen:
+                seen.add((p1, p2 + 1))
+                heapq.heappush(heap, (nums1[p1] + nums2[p2 + 1], p1, p2 + 1))
         
         return ans
