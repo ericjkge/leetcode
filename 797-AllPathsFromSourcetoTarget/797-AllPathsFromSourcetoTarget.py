be676b55288a983e5ea1883bd519ff633b20e2e5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-01 20:15:45 -0400

    File updated at 9/1/2025, 8:15:45 PM

--- 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.py
+++ 797-AllPathsFromSourcetoTarget/797-AllPathsFromSourcetoTarget.py
@@ -1,8 +1,7 @@
-# Last updated: 9/1/2025, 8:14:14 PM
+# Last updated: 9/1/2025, 8:15:45 PM
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         target = len(graph) - 1
-        seen = set([0])
         ans = []
 
         def backtrack(node, path):
@@ -11,11 +10,8 @@ class Solution:
                 return
 
             for n in graph[node]:
-                if n not in seen:
-                    seen.add(n)
                     path.append(n)
                     backtrack(n, path)
-                    seen.remove(n)
                     path.pop()
 
         backtrack(0, [0])
