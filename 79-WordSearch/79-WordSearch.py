4b17c569867dbdc1805d9a10709177adf6de035b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-01 21:00:29 -0400

    File updated at 9/1/2025, 9:00:29 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,34 +1,30 @@
-# Last updated: 8/17/2025, 12:29:55 AM
+# Last updated: 9/1/2025, 9:00:29 PM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         n = len(word)
         rows, cols = len(board), len(board[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        
+        seen = set()
+
         def backtrack(r, c, index):
             if index == n - 1:
                 return board[r][c] == word[index]
+
             if board[r][c] != word[index]:
                 return False
-            
-            original = board[r][c]
-            board[r][c] = "#"
-            
+
+            seen.add((r, c))
+
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if not (0 <= nr < rows and 0 <= nc < cols) or board[nr][nc] == "#":
-                    continue
-                if backtrack(nr, nc, index + 1):
-                    return True
-            board[r][c] = original
-            return False
-        
-        for r in range(rows):
-            for c in range(cols):
-                if backtrack(r, c, 0):
-                    return True
-        
-        return False
+                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen:
+                    if backtrack(nr, nc, index + 1):
+                        return True
 
-# Time: O(n * m * 3^w)
-# Space: O(w)
+            seen.remove((r, c))
+        
+        for row in range(rows):
+            for col in range(cols):
+                if backtrack(row, col, 0):
+                    return True
+        return False
