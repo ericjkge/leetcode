d14017f102602682398f583446807b7209d4b89e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-03 10:14:41 -0400

    File updated at 9/3/2025, 10:14:41 AM

--- 47-PermutationsII/47-PermutationsII.py
+++ 47-PermutationsII/47-PermutationsII.py
@@ -1,21 +1,26 @@
-# Last updated: 7/20/2025, 9:30:58 AM
+# Last updated: 9/3/2025, 10:14:41 AM
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         ans = []
-        counter = Counter(nums)
+        n = len(nums)
+        seen = set()
 
         def backtrack(path):
-            if len(path) == len(nums):
+            if len(path) == n:
                 ans.append(path[:])
                 return
             
-            for num in counter:
-                if counter[num] > 0:
-                    path.append(num)
-                    counter[num] -= 1
-                    backtrack(path)
-                    path.pop()
-                    counter[num] += 1
-        
+            for i in range(n):
+                if i in seen:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and i - 1 not in seen:
+                    continue
+                seen.add(i)
+                path.append(nums[i])
+                backtrack(path)
+                seen.remove(i)
+                path.pop()
+
         backtrack([])
         return ans
