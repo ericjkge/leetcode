f4f10dd32eede5e3a64af67151fce6f36b5a8aa3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-05 08:59:23 -0400

    File updated at 9/5/2025, 8:59:23 AM

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.py
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 8/29/2025, 8:33:36 PM
+# Last updated: 9/5/2025, 8:59:23 AM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
@@ -8,9 +8,15 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         a, b = headA, headB
-        
-        while a != b:
-            a = a.next if a else headB
-            b = b.next if b else headA
 
-        return a
+        while a != b:
+            if not a:
+                a = headB
+            else:
+                a = a.next
+            if not b:
+                b = headA
+            else:
+                b = b.next
+
+        return a if a!= None else None
