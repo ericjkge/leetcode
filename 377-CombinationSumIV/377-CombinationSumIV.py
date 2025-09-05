1970b25bd8763f894cc5c4b4a89435b628d2c36d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-05 10:03:48 -0400

    File updated at 9/5/2025, 10:03:48 AM

--- 377-CombinationSumIV/377-CombinationSumIV.py
+++ 377-CombinationSumIV/377-CombinationSumIV.py
@@ -1,17 +1,15 @@
-# Last updated: 9/3/2025, 10:37:52 AM
+# Last updated: 9/5/2025, 10:03:48 AM
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
         @cache
-        def dp(total):
-            if total > target:
+        def dp(i):
+            if i > target:
                 return 0
-
-            if total == target:
+            if i == target:
                 return 1
-            
             ways = 0
-            for num in nums:
-                ways += dp(total + num)
+            for n in nums:
+                ways += dp(i + n)
             return ways
 
         return dp(0)
