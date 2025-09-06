49798d9c7a846945f5693537bdd16c17a9519899
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-06 13:47:59 -0400

    File updated at 9/6/2025, 1:47:59 PM

--- 9-PalindromeNumber/9-PalindromeNumber.py
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -1,11 +1,11 @@
-# Last updated: 8/7/2025, 12:00:05 AM
+# Last updated: 9/6/2025, 1:47:59 PM
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         original = x
         reversed = 0
-        
+
         while x > 0:
             reversed = reversed * 10 + x % 10
             x //= 10
-            
+        
         return reversed == original
