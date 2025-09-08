cb051a742a888bb5d5f7d24840243325eab1ec4f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-08 18:38:30 -0400

    File updated at 9/8/2025, 6:38:30 PM

--- 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.py
+++ 513-FindBottomLeftTreeValue/513-FindBottomLeftTreeValue.py
@@ -1,4 +1,4 @@
-# Last updated: 8/1/2025, 5:15:02 PM
+# Last updated: 9/8/2025, 6:38:30 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,18 +9,17 @@ class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         self.max_depth = 0
         self.ans = root
-        
+
         def dfs(node, depth):
             if not node:
                 return
-            
+
             if depth > self.max_depth:
                 self.max_depth = depth
                 self.ans = node
-            
+
             dfs(node.left, depth + 1)
             dfs(node.right, depth + 1)
-        
+
         dfs(root, 0)
         return self.ans.val
-        
