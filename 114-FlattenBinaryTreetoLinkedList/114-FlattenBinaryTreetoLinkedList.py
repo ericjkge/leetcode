0a8289bce12342a5e9a8132d02686becc919e4f2
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-08 19:02:48 -0400

    File updated at 9/8/2025, 7:02:48 PM

--- 114-FlattenBinaryTreetoLinkedList/114-FlattenBinaryTreetoLinkedList.py
+++ 114-FlattenBinaryTreetoLinkedList/114-FlattenBinaryTreetoLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 8/1/2025, 9:46:36 PM
+# Last updated: 9/8/2025, 7:02:48 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -10,9 +10,8 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        
         self.prev = None
-        
+
         def dfs(node):
             if not node:
                 return
@@ -20,12 +19,11 @@ class Solution:
             if self.prev:
                 self.prev.right = node
                 self.prev.left = None
-            
+
             self.prev = node
-            
+
             right = node.right
             dfs(node.left)
             dfs(right)
         
         dfs(root)
-            
