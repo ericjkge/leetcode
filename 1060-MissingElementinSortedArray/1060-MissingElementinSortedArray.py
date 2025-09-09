31fc6dd54668436365ea2f44a586872ea680a91a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-09 15:32:16 -0400

    File updated at 9/9/2025, 3:32:16 PM

--- 1060-MissingElementinSortedArray/1060-MissingElementinSortedArray.py
+++ 1060-MissingElementinSortedArray/1060-MissingElementinSortedArray.py
@@ -1,20 +1,15 @@
-# Last updated: 9/9/2025, 3:29:36 PM
+# Last updated: 9/9/2025, 3:32:16 PM
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
         def findMissing(i):
             return nums[i] - nums[0] - i
 
-        left, right = 0, len(nums) - 1
+        left, right = 0, len(nums)
         while left + 1 < right:
             mid = (left + right) // 2
             if findMissing(mid) >= k:
                 right = mid
             else:
                 left = mid
-        
-        # print(left, right)
-        # print(findMissing(right), k)
 
-        if findMissing(right) < k:
-            return nums[right] + (k - findMissing(right))
         return nums[left] + (k - findMissing(left))
