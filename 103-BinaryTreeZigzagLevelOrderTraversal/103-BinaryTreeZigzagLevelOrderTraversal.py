ab140902230b299a36725f0b83ed9aee5bf04f57
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-10 10:10:49 -0400

    File updated at 9/10/2025, 10:10:49 AM

--- 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.py
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.py
@@ -1,4 +1,4 @@
-# Last updated: 5/30/2025, 12:08:01 PM
+# Last updated: 9/10/2025, 10:10:49 AM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -9,32 +9,24 @@ class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        
-        queue = deque([root])
+
         ans = []
+        queue = deque([root])
         counter = 0
+
         while queue:
             level = []
-            for i in range(len(queue)):
+            for _ in range(len(queue)):
                 node = queue.popleft()
-                print(node.val)
                 level.append(node.val)
-                
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            
+
             if counter % 2 != 0:
                 level.reverse()
-            
-            print(level)
-            ans.append(level)
+                
             counter += 1
-        
+            ans.append(level)
         return ans
-            
-        
-        
-        
-        
