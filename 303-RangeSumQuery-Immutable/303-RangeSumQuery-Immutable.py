49239585b36f253d65950dbd01c78ce4c04e35d1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-17 00:06:00 -0400

    File updated at 9/17/2025, 12:06:00 AM

--- 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.py
+++ 303-RangeSumQuery-Immutable/303-RangeSumQuery-Immutable.py
@@ -1,16 +1,14 @@
-# Last updated: 7/27/2025, 9:36:05 PM
+# Last updated: 9/17/2025, 12:06:00 AM
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-        self.prefix = [nums[0]]
-        for i in range(1, len(nums)):
-            self.prefix.append(self.prefix[i - 1] + self.nums[i])
+        self.prefix = [0]
+        for i in range(len(nums)):
+            self.prefix.append(self.prefix[-1] + nums[i])
 
     def sumRange(self, left: int, right: int) -> int:
-        left = self.prefix[left - 1] if left > 0 else 0
-        right = self.prefix[right]
-        return right - left
+        return self.prefix[right + 1] - self.prefix[left]
+
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
