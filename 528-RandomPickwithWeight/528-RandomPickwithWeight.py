4b89398eab8ac04383abc5ed4bf7109ecb83d4dc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-23 20:30:53 -0400

    File updated at 9/23/2025, 8:30:53 PM

--- 528-RandomPickwithWeight/528-RandomPickwithWeight.py
+++ 528-RandomPickwithWeight/528-RandomPickwithWeight.py
@@ -1,11 +1,11 @@
-# Last updated: 9/23/2025, 8:23:32 PM
+# Last updated: 9/23/2025, 8:30:53 PM
 class Solution:
 
     def __init__(self, w: List[int]):
         self.prefix = [w[0]]
         for i in range(1, len(w)):
             self.prefix.append(self.prefix[-1] + w[i])
-        
+
     def pickIndex(self) -> int:
         import random
 
@@ -14,14 +14,15 @@ class Solution:
         left, right = 0, len(self.prefix) - 1
         while left + 1 < right:
             mid = (left + right) // 2
-            if self.prefix[mid] <= r:
-                left = mid
-            else:
+            if r <= self.prefix[mid]:
                 right = mid
+            else:
+                left = mid
         
         return left if r <= self.prefix[left] else right
 
-
+# [1, 3, 7, 14]
+# [1, 4, 11, 25]
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
 # param_1 = obj.pickIndex()
