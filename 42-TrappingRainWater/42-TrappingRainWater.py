005dea1100e0fdad15dfd4b2f2577c32c707dde7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-24 09:43:34 -0400

    File updated at 9/24/2025, 9:43:34 AM

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,18 +1,18 @@
-# Last updated: 8/6/2025, 12:35:26 AM
+# Last updated: 9/24/2025, 9:43:34 AM
 class Solution:
     def trap(self, height: List[int]) -> int:
         left, right = 0, len(height) - 1
         mleft, mright = height[left], height[right]
-        water = 0
+        total = 0
+
         while left < right:
-            if mleft <= mright:
+            if mleft < mright:
                 left += 1
-                mleft = max(height[left], mleft)
-                water += mleft - height[left] # Note: do mleft instead of min(mleft, mright) here,
-                # since this way it self-corrects to 0 but min could give negative
+                mleft = max(mleft, height[left])
+                total += mleft - height[left]
             else:
                 right -= 1
-                mright = max(height[right], mright)
-                water += mright - height[right]
-        return water
-
+                mright = max(mright, height[right])
+                total += mright - height[right]
+        
+        return total
