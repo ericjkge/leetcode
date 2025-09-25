79f26f3670fb5d42ac52cddb2d3b4df06dc2f949
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-25 09:47:37 -0400

    File updated at 9/25/2025, 9:47:37 AM

--- 75-SortColors/75-SortColors.py
+++ 75-SortColors/75-SortColors.py
@@ -1,9 +1,18 @@
-# Last updated: 6/29/2025, 4:19:52 PM
+# Last updated: 9/25/2025, 9:47:37 AM
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        hashmap = Counter(nums)
-        nums[:] = [0] * hashmap[0] + [1] * hashmap[1] + [2] * hashmap[2]
-        
+        left, it, right = 0, 0, len(nums) - 1
+
+        while it <= right:
+            if nums[it] == 2:
+                nums[it], nums[right] = nums[right], nums[it]
+                right -= 1
+            elif nums[it] == 0:
+                nums[it], nums[left] = nums[left], nums[it]
+                left += 1
+                it += 1
+            else:
+                it += 1
