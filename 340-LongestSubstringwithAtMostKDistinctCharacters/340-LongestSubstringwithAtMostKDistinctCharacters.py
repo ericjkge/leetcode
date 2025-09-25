2b226555f253e61e0e71594ed43f9fa69d64ef5f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-25 09:36:45 -0400

    File updated at 9/25/2025, 9:36:45 AM

--- 340-LongestSubstringwithAtMostKDistinctCharacters/340-LongestSubstringwithAtMostKDistinctCharacters.py
+++ 340-LongestSubstringwithAtMostKDistinctCharacters/340-LongestSubstringwithAtMostKDistinctCharacters.py
@@ -1,15 +1,14 @@
-# Last updated: 7/28/2025, 9:23:48 PM
+# Last updated: 9/25/2025, 9:36:45 AM
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
         hashmap = {}
-        left = right = 0
-        ans = 0
-        
+        left = right = ans = 0
+
         while right < len(s):
             hashmap[s[right]] = hashmap.get(s[right], 0) + 1
             while len(hashmap) > k:
                 hashmap[s[left]] -= 1
-                if hashmap.get(s[left]) == 0:
+                if hashmap[s[left]] == 0:
                     del hashmap[s[left]]
                 left += 1
             ans = max(ans, right - left + 1)
