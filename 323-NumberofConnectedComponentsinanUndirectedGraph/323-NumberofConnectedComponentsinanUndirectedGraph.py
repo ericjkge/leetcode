80fae4e1dc2c7d64d89246aa9ac93ed1e98d1d1e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-09-26 09:32:25 -0400

    File updated at 9/26/2025, 9:32:25 AM

--- 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
+++ 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
@@ -1,30 +1,21 @@
-# Last updated: 8/10/2025, 11:48:45 PM
+# Last updated: 9/26/2025, 9:32:25 AM
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        class UnionFind:
-            def __init__(self, size):
-                self.root = [i for i in range(size)]
-                self.rank = [1] * size
-            
-            def find(self, x):
-                if x == self.root[x]:
-                    return x
-                self.root[x] = self.find(self.root[x])
-                return self.root[x]
-            
-            def union(self, a, b):
-                ra, rb = self.find(a), self.find(b)
-                if ra != rb:
-                    if self.rank[ra] > self.rank[rb]:
-                        self.root[rb] = ra
-                    elif self.rank[ra] < self.rank[rb]:
-                        self.root[ra] = rb
-                    else:
-                        self.root[rb] = ra
-                        self.rank[ra] += 1
-        
-        uf = UnionFind(n)
+        graph = defaultdict(list)
         for start, end in edges:
-            uf.union(start, end)
-        root = set([uf.find(i) for i in range(n)])
-        return len(root)
+            graph[start].append(end)
+            graph[end].append(start)
+
+        seen = set()
+        def dfs(node):
+            seen.add(node)
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    dfs(neighbor)
+
+        ans = 0
+        for i in range(n):
+            if i not in seen:
+                ans += 1
+                dfs(i)
+        return ans
