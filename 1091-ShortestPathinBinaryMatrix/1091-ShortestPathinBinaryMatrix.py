62a7e0960be82a9e368835344fec3b40f71ff8c1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-12 00:39:42 -0400

    File updated at 10/12/2025, 12:39:42 AM

--- 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.py
+++ 1091-ShortestPathinBinaryMatrix/1091-ShortestPathinBinaryMatrix.py
@@ -1,26 +1,26 @@
-# Last updated: 8/11/2025, 9:58:25 PM
+# Last updated: 10/12/2025, 12:39:42 AM
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0] == 1:
             return -1
-        
-        rows, cols = len(grid), len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
-        
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, -1), (-1, 1)]
+        n = len(grid)
+
         queue = deque([(0, 0)])
         seen = set([(0, 0)])
-        dist = 1
-        
+        length = 1
+
         while queue:
             for _ in range(len(queue)):
                 r, c = queue.popleft()
-                if (r, c) == (rows - 1, cols - 1):
-                    return dist
+                if (r, c) == (n - 1, n - 1):
+                    return length
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and grid[nr][nc] == 0:
+                    if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] == 0 and (nr, nc) not in seen:
                         seen.add((nr, nc))
                         queue.append((nr, nc))
-            dist += 1
-        
+            length += 1
+            
         return -1
