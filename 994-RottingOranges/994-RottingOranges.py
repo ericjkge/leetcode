fe4024bf40e4d72e18a328a77ce04c63598018cd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-12 01:16:01 -0400

    File updated at 10/12/2025, 1:16:01 AM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,28 +1,29 @@
-# Last updated: 8/12/2025, 12:43:40 AM
+# Last updated: 10/12/2025, 1:16:01 AM
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        fresh = 0
-        rows, cols = len(grid), len(grid[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         queue = deque()
+        m, n = len(grid), len(grid[0])
         time = 0
-        
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == 1:
-                    fresh += 1
-                elif grid[r][c] == 2:
+        fresh = 0
+
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] == 2:
                     queue.append((r, c))
+                elif grid[r][c] == 1:
+                    fresh += 1
         
         while queue and fresh > 0:
             for _ in range(len(queue)):
                 r, c = queue.popleft()
                 for dr, dc in directions:
                     nr, nc = r + dr, c + dc
-                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
                         fresh -= 1
                         queue.append((nr, nc))
             time += 1
         
         return time if fresh == 0 else -1
+        
