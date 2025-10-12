9980c4e1c40236f5dbbc8b8ede12a96600952c3d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-12 10:37:46 -0400

    File updated at 10/12/2025, 10:37:46 AM

--- 45-JumpGameII/45-JumpGameII.py
+++ 45-JumpGameII/45-JumpGameII.py
@@ -1,14 +1,14 @@
-# Last updated: 8/12/2025, 12:30:58 AM
+# Last updated: 10/12/2025, 10:37:46 AM
 class Solution:
     def jump(self, nums: List[int]) -> int:
         jumps = 0
-        curr = 0
+        curr_end = 0
         farthest = 0
-        
+
         for i in range(len(nums) - 1):
             farthest = max(farthest, i + nums[i])
-            if i == curr:
+            if i == curr_end:
+                curr_end = farthest
                 jumps += 1
-                curr = farthest
-                
+        
         return jumps
