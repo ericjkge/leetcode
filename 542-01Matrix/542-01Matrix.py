cb1ab6183e2b2a4ebe6cee1dc6af06e0a5732f4b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-12 09:48:56 -0400

    File updated at 10/12/2025, 9:48:56 AM

--- 542-01Matrix/542-01Matrix.py
+++ 542-01Matrix/542-01Matrix.py
@@ -1,20 +1,24 @@
-# Last updated: 8/21/2025, 12:53:33 AM
+# Last updated: 10/12/2025, 9:48:56 AM
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        m, n = len(mat), len(mat[0])
         queue = deque()
-        for row in range(len(mat)):
-            for col in range(len(mat[0])):
-                if mat[row][col] == 0:
-                    queue.append([row, col])
-                    continue
-                mat[row][col] = float("inf")
 
+        for r in range(m):
+            for c in range(n):
+                if mat[r][c] == 0:
+                    queue.append((r, c))
+                else:
+                    mat[r][c] = -1
+        
         while queue:
-            x, y = queue.popleft()
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < len(mat) and 0 <= ny < len(mat[0]) and mat[nx][ny] > mat[x][y]:
-                    mat[nx][ny] = mat[x][y] + 1
-                    queue.append([nx, ny])
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n and mat[nr][nc] == -1:
+                        mat[nr][nc] = mat[r][c] + 1
+                        queue.append((nr, nc))
+
         return mat
