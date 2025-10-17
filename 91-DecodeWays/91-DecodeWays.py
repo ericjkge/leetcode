4b31b1d282b5527892900c589447b7df099e4443
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-17 11:21:14 -0400

    File updated at 10/17/2025, 11:21:14 AM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,20 +1,21 @@
-# Last updated: 8/14/2025, 3:53:51 PM
+# Last updated: 10/17/2025, 11:21:14 AM
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        if n == 0:
-            return 0
-        
-        dp = [0] * (n + 1)
-        dp[0] = 1
-        dp[1] = 1 if s[0] != "0" else 0
 
-        for i in range(2, n + 1):
-            one = s[i - 1]
-            two = s[i - 2:i]
-            if one != "0":
-                dp[i] += dp[i - 1]
-            if two[0] != "0" and 10 <= int(two) <= 26:
-                dp[i] += dp[i - 2]
+        @cache
+        def dp(i): # No. of decodings for s[i:]
+            if i == n:
+                return 1
+            if s[i] == "0":
+                return 0
+            
+            res = dp(i + 1)
+            if i + 1 < n and (s[i] == "1" or (s[i] == "2" and s[i + 1] <= "6")):
+                res += dp(i + 2)
+            
+            return res
         
-        return dp[n]
+        return dp(0)
+
+# NOTE: dp[i - 1] is num ways to decode up to prev char (inclusive), s[i - 1] is current char. think about transition as adding up both possible ways, since for one you can just append the current char and for two you just append prev and current char
