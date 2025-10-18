74dd8248d988bfe5b36d95b318490192f84de727
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-18 11:55:55 -0400

    File updated at 10/18/2025, 11:55:54 AM

--- 490-TheMaze/490-TheMaze.py
+++ 490-TheMaze/490-TheMaze.py
@@ -1,26 +1,26 @@
-# Last updated: 8/4/2025, 9:16:22 PM
+# Last updated: 10/18/2025, 11:55:54 AM
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        rows, cols = len(maze), len(maze[0])
+        m, n = len(maze), len(maze[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         seen = set()
-        
+
         def roll(r, c, dr, dc):
-            while 0 <= r + dr < rows and 0 <= c + dc < cols and maze[r + dr][c + dc] == 0:
-                r = r + dr
-                c = c + dc
-            return r, c
-        
+            nr, nc = r, c
+            while 0 <= nr + dr < m and 0 <= nc + dc < n and maze[nr + dr][nc + dc] != 1:
+                nr, nc = nr + dr, nc + dc
+            return nr, nc
+
         def dfs(r, c):
-            if [r, c] == destination:
+            if (r, c) in seen:
+                return False
+            if (r, c) == (destination[0], destination[1]):
                 return True
+            seen.add((r, c))
             for dr, dc in directions:
                 nr, nc = roll(r, c, dr, dc)
-                if (nr, nc) not in seen:
-                    seen.add((nr, nc))
-                    if dfs(nr, nc):
-                        return True
+                if dfs(nr, nc):
+                    return True
             return False
-        
-        seen.add((start[0], start[1]))
+
         return dfs(start[0], start[1])
