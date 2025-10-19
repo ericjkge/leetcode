7c72caa69adb552f1bb300f7221e594b635c788f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-19 08:58:31 -0400

    File updated at 10/19/2025, 8:58:31 AM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,19 +1,18 @@
-# Last updated: 10/19/2025, 8:58:07 AM
+# Last updated: 10/19/2025, 8:58:31 AM
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         ans = 0
         seen = set()
 
         def dfs(i):
+            seen.add(i)
             for j in range(0, len(isConnected)):
                 if isConnected[i][j]:
                     if j not in seen:
-                        seen.add(j)
                         dfs(j)
         
         for i in range(len(isConnected)):
             if i not in seen:
-                seen.add(i)
                 dfs(i)
                 ans += 1
 
