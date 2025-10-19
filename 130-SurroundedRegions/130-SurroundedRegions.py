cf8e419b1e13523171044ca7f6f89d914bdd41c3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-19 09:41:40 -0400

    File updated at 10/19/2025, 9:41:40 AM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,4 +1,4 @@
-# Last updated: 10/19/2025, 9:36:05 AM
+# Last updated: 10/19/2025, 9:41:40 AM
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
@@ -7,10 +7,9 @@ class Solution:
         m, n = len(board), len(board[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         seen = set()
-        regions = []
 
         def dfs(r, c):
-            region.append((r, c))
+            board[r][c] = "S"
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
                 if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in seen and board[nr][nc] == "O":
@@ -18,16 +17,24 @@ class Solution:
                     dfs(nr, nc)
         
         for r in range(m):
-            for c in range(n):
-                if (r, c) not in seen and board[r][c] == "O":
-                    region = []
-                    seen.add((r, c))
-                    dfs(r, c)
-                    regions.append(region)
+            if board[r][0] == "O":
+                seen.add((r, 0))
+                dfs(r, 0)
+            if board[r][n - 1] == "O":
+                seen.add((r, n - 1))
+                dfs(r, n - 1)
+
+        for c in range(n):
+            if board[0][c] == "O":
+                seen.add((0, c))
+                dfs(0, c)
+            if board[m - 1][c] == "O":
+                seen.add((m - 1, c))
+                dfs(m - 1, c)
         
-        for region in regions:
-            if any(r < 1 or r >= m - 1 or c < 1 or c >= n - 1 for r, c in region):
-                continue
-            
-            for r, c in region:
-                board[r][c] = "X"
+        for r in range(m):
+            for c in range(n):
+                if board[r][c] == "S":
+                    board[r][c] = "O"
+                elif board[r][c] == "O":
+                    board[r][c] = "X"
