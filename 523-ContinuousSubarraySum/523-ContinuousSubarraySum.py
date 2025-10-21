3edc3465734ab4d0ef0c67f648ca0ff271c9e238
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-21 14:41:03 -0400

    File updated at 10/21/2025, 2:41:03 PM

--- 523-ContinuousSubarraySum/523-ContinuousSubarraySum.py
+++ 523-ContinuousSubarraySum/523-ContinuousSubarraySum.py
@@ -1,15 +1,15 @@
-# Last updated: 9/17/2025, 11:45:11 PM
+# Last updated: 10/21/2025, 2:41:03 PM
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        hashmap = {0:-1}
         prefix = 0
+        hashmap = {0:-1}
 
         for i, num in enumerate(nums):
             prefix += num
             if prefix % k in hashmap:
-                if i - hashmap[prefix % k] >= 2:
+                if (i - hashmap[prefix % k]) >= 2:
                     return True
             else:
                 hashmap[prefix % k] = i
-
+        
         return False
