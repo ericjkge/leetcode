a8349970d24e789f394b803664432cd9880f7473
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-21 14:57:09 -0400

    File updated at 10/21/2025, 2:57:08 PM

--- 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.py
+++ 713-SubarrayProductLessThanK/713-SubarrayProductLessThanK.py
@@ -1,4 +1,4 @@
-# Last updated: 10/21/2025, 2:56:12 PM
+# Last updated: 10/21/2025, 2:57:08 PM
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
         left = right = ans = 0
@@ -6,11 +6,10 @@ class Solution:
 
         while right < len(nums):
             window *= nums[right]
-            while left < right and window >= k:
+            while left <= right and window >= k:
                 window /= nums[left]
                 left += 1
-            if window < k:
-                ans += (right - left + 1)
+            ans += (right - left + 1)
             right += 1
 
 
