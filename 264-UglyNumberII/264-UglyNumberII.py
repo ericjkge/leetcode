30fcee397d406fa62e9160b39541a82fa9cc16ab
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-26 00:34:50 -0400

    File updated at 10/26/2025, 12:34:50 AM

--- 264-UglyNumberII/264-UglyNumberII.py
+++ 264-UglyNumberII/264-UglyNumberII.py
@@ -1,10 +1,9 @@
-# Last updated: 10/26/2025, 12:34:31 AM
+# Last updated: 10/26/2025, 12:34:50 AM
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
         heap = [1]
         factors = [2, 3, 5]
         seen = set()
-        ans = 1
 
         for i in range(n):
             ans = heapq.heappop(heap)
