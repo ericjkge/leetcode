8b8f8cb5a0608a3ba69173e44cd84d5e7a870ee1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-26 10:26:03 -0400

    File updated at 10/26/2025, 10:26:03 AM

--- 1486-XOROperationinanArray/1486-XOROperationinanArray.py
+++ 1486-XOROperationinanArray/1486-XOROperationinanArray.py
@@ -1,7 +1,9 @@
-# Last updated: 10/25/2025, 11:30:09 PM
+# Last updated: 10/26/2025, 10:26:03 AM
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
         ans = 0
+        
         for i in range(n):
             ans ^= start + 2 * i
+        
         return ans
