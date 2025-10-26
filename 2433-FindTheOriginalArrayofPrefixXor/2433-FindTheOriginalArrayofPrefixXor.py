038ca32dbaf94d8d92f75e40684eea4059e5b4db
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-26 10:25:14 -0400

    File updated at 10/26/2025, 10:25:14 AM

--- 2433-FindTheOriginalArrayofPrefixXor/2433-FindTheOriginalArrayofPrefixXor.py
+++ 2433-FindTheOriginalArrayofPrefixXor/2433-FindTheOriginalArrayofPrefixXor.py
@@ -1,10 +1,11 @@
-# Last updated: 10/25/2025, 11:53:41 PM
+# Last updated: 10/26/2025, 10:25:14 AM
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
-        prefix = 0
         ans = [pref[0]]
 
         for i in range(1, len(pref)):
-            ans.append(pref[i - 1] ^ pref[i])
-
+            ans.append(pref[i] ^ pref[i - 1])
+        
         return ans
+
+    # XOR rule: a ^ b = c -> b = a ^ c  (pref[i] = pref[i - 1] ^ arr[i])
