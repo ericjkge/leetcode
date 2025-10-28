5d7ffd0d721bf50c8439a1dd262efe87ed44b023
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-10-28 09:45:24 -0400

    File updated at 10/28/2025, 9:45:24 AM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,4 +1,4 @@
-# Last updated: 10/28/2025, 9:39:58 AM
+# Last updated: 10/28/2025, 9:45:24 AM
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         left = right = 0
@@ -6,14 +6,21 @@ class Solution:
         scounter = {}
         min_length = float("inf")
         ans = ""
+        have = 0
+        need = len(tcounter)
 
         while right < len(s):
             scounter[s[right]] = scounter.get(s[right], 0) + 1
-            while all(scounter.get(c, 0) >= tcounter[c] for c in tcounter):
+            if s[right] in tcounter and scounter[s[right]] == tcounter[s[right]]:
+                have += 1
+            
+            while have == need:
                 if (right - left + 1) < min_length:
                     min_length = right - left + 1
                     ans = s[left:right + 1]
                 scounter[s[left]] = scounter[s[left]] - 1
+                if s[left] in tcounter and scounter[s[left]] < tcounter[s[left]]:
+                    have -= 1
                 left += 1
             right += 1
         
