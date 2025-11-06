e78dc8a3bdc45b9bf8fe2f79001953d19dc1a9ee
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-06 10:38:19 -0500

    File updated at 11/6/2025, 10:38:18 AM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,24 +1,26 @@
-# Last updated: 6/24/2025, 4:23:07 PM
+# Last updated: 11/6/2025, 10:38:18 AM
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
+        n = len(nums)
         ans = []
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            left = i + 1
-            right = len(nums) - 1
-            while left < right:
-                if -nums[i] < nums[left] + nums[right]:
-                    right -= 1
-                elif -nums[i] > nums[left] + nums[right]:
-                    left += 1
-                else:
-                    ans.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    while left < len(nums) and nums[left] == nums[left - 1]:
-                        left += 1
-        return ans
 
-                
-        
+        for i in range(n):
+            if i and nums[i] == nums[i - 1]:
+                continue
+            target = -nums[i]
+            seen = set()
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[left] + nums[right]
+                if total == target:                        
+                    ans.append([-target, nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                elif total > target:
+                    right -= 1
+                else:
+                    left += 1
+
+        return ans
