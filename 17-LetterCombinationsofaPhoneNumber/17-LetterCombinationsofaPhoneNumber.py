2a02ed3f941b5e7bc405f73f29197207821e87ac
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-07 18:56:13 -0500

    File updated at 11/7/2025, 6:56:12 PM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
@@ -1,9 +1,6 @@
-# Last updated: 9/1/2025, 8:24:45 PM
+# Last updated: 11/7/2025, 6:56:12 PM
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return []
-            
         mappings = {
             "2":"abc",
             "3":"def",
@@ -14,18 +11,18 @@ class Solution:
             "8":"tuv",
             "9":"wxyz"
         }
-        ans = []
-        n = len(digits)
 
-        def backtrack(path, index):
-            if index == n:
+        ans = []
+
+        def backtrack(index, path):
+            if len(path) == len(digits):
                 ans.append("".join(path))
                 return
             
-            for c in mappings[digits[index]]:
-                path.append(c)
-                backtrack(path, index + 1)
+            for letter in mappings[digits[index]]:
+                path.append(letter)
+                backtrack(index + 1, path)
                 path.pop()
-
-        backtrack([], 0)
+                
+        backtrack(0, [])
         return ans
