b2483872fd09598e8d1c3b8c957759a5d0a18ccf
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-08 21:03:36 -0500

    File updated at 11/8/2025, 9:03:36 PM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 10/1/2025, 9:33:27 AM
+# Last updated: 11/8/2025, 9:03:36 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,54 +6,32 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # Version 1 (time: O(N log k), space: O(k)): Heap
-
         if not lists:
             return None
-        
-        curr = dummy = ListNode()
-        heap = []
-        for i in range(len(lists)):
-            if lists[i]:
-                heapq.heappush(heap, (lists[i].val, i, lists[i]))
+            
+        def mergeLists(list1, list2):
+            dummy = curr = ListNode()
 
-        while heap:
-            val, i, node = heapq.heappop(heap)
-            if node.next:
-                heapq.heappush(heap, (node.next.val, i, node.next))
-            curr.next = node
-            curr = curr.next
-        
-        return dummy.next
+            while list1 and list2:
+                if list1.val < list2.val:
+                    curr.next = list1
+                    list1 = list1.next
+                else:
+                    curr.next = list2
+                    list2 = list2.next
+                curr = curr.next
+            
+            curr.next = list1 if list1 else list2
 
-        # Version 2 (time: O(N log k), space: O(1)): Divide and conquer
+            return dummy.next
         
-        # if not lists:
-        #     return None
-
-        # def merge(p1, p2):
-        #     dummy = curr = ListNode()
-        #     while p1 and p2:
-        #         if p1.val < p2.val:
-        #             curr.next = p1
-        #             p1 = p1.next
-        #         else:
-        #             curr.next = p2
-        #             p2 = p2.next
-        #         curr = curr.next
-        #     curr.next = p1 or p2
-        #     return dummy.next
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                if i + 1 < len(lists):
+                    merged.append(mergeLists(lists[i], lists[i + 1]))
+                else:
+                    merged.append(lists[i])
+            lists = merged
         
-        # while len(lists) > 1:
-        #     merged = []
-        #     i = 0
-        #     while i < len(lists):
-        #         if i == len(lists) - 1:
-        #             merged.append(lists[i])
-        #             break
-        #         else:
-        #             merged.append(merge(lists[i], lists[i + 1]))
-        #             i += 2
-        #     lists = merged
-        
-        # return lists[0]
+        return lists[0]
