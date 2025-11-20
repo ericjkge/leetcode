6fc31ce5df29f00042fbbf52d515760e14d773fc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-20 12:04:20 -0500

    File updated at 11/20/2025, 12:04:19 PM

--- 54-SpiralMatrix/54-SpiralMatrix.py
+++ 54-SpiralMatrix/54-SpiralMatrix.py
@@ -1,27 +1,28 @@
-# Last updated: 7/3/2025, 12:12:24 AM
+# Last updated: 11/20/2025, 12:04:19 PM
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top, bottom = 0, len(matrix)
-        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix) - 1
+        left, right = 0, len(matrix[0]) - 1
         ans = []
-        while left < right and top < bottom:
-            for i in range(left, right):
-                ans.append(matrix[top][i])
+
+        while left <= right and top <= bottom:
+            for c in range(left, right + 1):
+                ans.append(matrix[top][c])
             top += 1
-            
-            for i in range(top, bottom):
-                ans.append(matrix[i][right - 1])
+
+            for r in range(top, bottom + 1):
+                ans.append(matrix[r][right])
             right -= 1
 
-            if len(ans) == len(matrix) * len(matrix[0]):
+            if left > right or top > bottom:
                 break
 
-            for i in range(right - 1, left - 1, -1):
-                ans.append(matrix[bottom - 1][i])
+            for c in range(right, left - 1, -1):
+                ans.append(matrix[bottom][c])
             bottom -= 1
-            
-            for i in range(bottom - 1, top - 1, -1):
-                ans.append(matrix[i][left])
+
+            for r in range(bottom, top - 1, -1):
+                ans.append(matrix[r][left])
             left += 1
-        
+
         return ans
