30de37868bd7079a914f4064aee54dfb0a3324c8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-21 14:39:10 -0500

    File updated at 11/21/2025, 2:39:10 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,9 +1,28 @@
-# Last updated: 11/21/2025, 2:22:19 PM
+# Last updated: 11/21/2025, 2:39:10 PM
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+            
         c1 = Counter(s1)
-        
-        for i in range(len(s2) - len(s1) + 1):
-            if c1 == Counter(s2[i:i + len(s1)]): 
+        left = 0
+        right = len(s1) - 1
+        c2 = defaultdict(int)
+
+        for i in range(len(s1)):
+            c2[s2[i]] += 1
+
+        if c1 == c2:
+            return True
+
+        while right < len(s2) - 1:
+            right += 1
+            c2[s2[right]] += 1
+            c2[s2[left]] -= 1
+            if c2[s2[left]] == 0:
+                del c2[s2[left]]
+            left += 1
+            if c1 == c2:
                 return True
+
         return False
