c3abb033a0824431d8c63edaabad4ecfc091a9b1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-21 12:51:50 -0500

    File updated at 11/21/2025, 12:51:49 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,13 +1,15 @@
-# Last updated: 6/28/2025, 10:38:22 AM
+# Last updated: 11/21/2025, 12:51:49 PM
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         intervals.append(newInterval)
         intervals.sort()
+
         ans = []
+
         for start, end in intervals:
             if ans and start <= ans[-1][1]:
                 ans[-1][1] = max(ans[-1][1], end)
             else:
                 ans.append([start, end])
-
+        
         return ans
