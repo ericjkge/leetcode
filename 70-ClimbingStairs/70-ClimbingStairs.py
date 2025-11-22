ebf4faaa521a5f1d141a198a9631f90afb094986
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-22 14:05:13 -0500

    File updated at 11/22/2025, 2:05:12 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,10 +1,8 @@
-# Last updated: 11/22/2025, 2:05:02 PM
+# Last updated: 11/22/2025, 2:05:12 PM
 class Solution:
     def climbStairs(self, n: int) -> int:
         @cache
         def dp(i):
-            if i < 0:
-                return 0
             if i == 0 or i == 1:
                 return 1
             return dp(i - 1) + dp(i - 2)
