b1ee3caf4eed5165d9c36da7c9c93cc2fb09d58f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-24 00:26:45 -0500

    File updated at 11/24/2025, 12:26:45 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,4 +1,4 @@
-# Last updated: 11/24/2025, 12:25:40 AM
+# Last updated: 11/24/2025, 12:26:45 AM
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows, cols = len(board), len(board[0])
@@ -8,14 +8,14 @@ class Solution:
         def backtrack(index, r, c):
             if index == len(word) - 1:
                 return True
-
-            seen.add((r, c))
+            original = board[r][c]
+            board[r][c] = "#"
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in seen and board[nr][nc] == word[index + 1]:
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] != "#" and board[nr][nc] == word[index + 1]:
                     if backtrack(index + 1, nr, nc):
                         return True
-            seen.remove((r, c))
+            board[r][c] = original
         
         for r in range(rows):
             for c in range(cols):
