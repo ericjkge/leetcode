eb083467a5adb3efec40b93da36487adc4ec0294
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-25 19:16:48 -0500

    File updated at 11/25/2025, 7:16:48 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,32 +1,16 @@
-# Last updated: 10/1/2025, 9:48:52 AM
+# Last updated: 11/25/2025, 7:16:48 PM
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
 
         @cache
         def dp(i, j):
-            if i == 0:
-                return 1
             if i < 0 or j == n:
                 return 0
-            return dp(i - coins[j], j) + dp(i, j + 1)
-        
-        return dp(amount, 0)
 
-        # Alternative: 
-        # self.ans = 0
-        # n = len(coins)
-
-        # def backtrack(index, total):
-        #     if total > amount:
-        #         return
-
-        #     if total == amount:
-        #         self.ans += 1
-        #         return
+            if i == 0:
+                return 1
             
-        #     for i in range(index, n):
-        #         backtrack(i, total + coins[i])
-        
-        # backtrack(0, 0)
-        # return self.ans
+            return dp(i - coins[j], j) + dp(i, j + 1)
+
+        return dp(amount, 0)
