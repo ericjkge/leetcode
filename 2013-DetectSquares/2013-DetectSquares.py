55673b796a8b3f6b634d1f6374faf7f0b050ef96
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-29 03:28:15 +0800

    File updated at 11/28/2025, 2:28:15 PM

--- 2013-DetectSquares/2013-DetectSquares.py
+++ 2013-DetectSquares/2013-DetectSquares.py
@@ -1,38 +1,38 @@
-# Last updated: 11/28/2025, 2:24:20 PM
+# Last updated: 11/28/2025, 2:28:15 PM
 1class DetectSquares:
 2
 3    def __init__(self):
 4        self.points = defaultdict(int)
 5
 6    def add(self, point: List[int]) -> None:
-7        self.points[(point[0], point[1])] += 1
-8
-9    def count(self, point: List[int]) -> int:
-10        px, py = point
-11        ans = 0
-12
-13        for (x, y), c1 in self.points.items():
-14            if y != py or x == px:
-15                continue
-16
-17            d = px - x
-18
-19            # coordinates of the two possible third corners
-20            # 1) above square
-21            p2 = (px, py + d)
-22            p3 = (x,  py + d)
+7        x, y = point
+8        self.points[(x, y)] += 1
+9
+10    def count(self, point: List[int]) -> int:
+11        px, py = point
+12        ans = 0
+13
+14        for (x, y), freq in self.points.items():
+15            if y != py or x == px:
+16                continue
+17
+18            side = px - x
+19
+20            # Above
+21            p2 = (px, py + side)
+22            p3 = (x,  py + side)
 23
-24            # 2) below square
-25            p4 = (px, py - d)
-26            p5 = (x,  py - d)
+24            # Below
+25            p4 = (px, py - side)
+26            p5 = (x,  py - side)
 27
-28            # count squares above
+28            # Count above
 29            if p2 in self.points and p3 in self.points:
-30                ans += c1 * self.points[p2] * self.points[p3]
+30                ans += freq * self.points[p2] * self.points[p3]
 31
-32            # count squares below
+32            # Count below
 33            if p4 in self.points and p5 in self.points:
-34                ans += c1 * self.points[p4] * self.points[p5]
+34                ans += freq * self.points[p4] * self.points[p5]
 35
 36        return ans
 37
