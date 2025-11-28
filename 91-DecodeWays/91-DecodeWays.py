7402a53b824280a42b789e93bd0b81b07a95f7a1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-11-29 02:24:09 +0800

    File updated at 11/28/2025, 1:24:09 PM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,19 +1,39 @@
-# Last updated: 10/17/2025, 11:32:38 AM
-class Solution:
-    def numDecodings(self, s: str) -> int:
-        n = len(s)
-
-        @cache
-        def dp(i):
-            if i == n:
-                return 1
-            
-            if s[i] == "0":
-                return 0
-            
-            res = dp(i + 1)
-            if i + 1 < n and (s[i] == "1" or (s[i] == "2" and s[i + 1] <= "6")):
-                res += dp(i + 2)
-            return res
-        
-        return dp(0)
+# Last updated: 11/28/2025, 1:24:09 PM
+1class Solution:
+2    def numDecodings(self, s: str) -> int:
+3        n = len(s)
+4
+5        @cache
+6        def dp(i):
+7            if i == n:
+8                return 1
+9
+10            if s[i] == "0":
+11                return 0
+12            
+13            res = dp(i + 1)
+14
+15            if i + 1 < n and (s[i] == "1" or (s[i] == "2" and s[i + 1] <= "6")):
+16                res += dp(i + 2)
+17
+18            return res
+19
+20        return dp(0)
+21
+22        # n = len(s)
+23
+24        # @cache
+25        # def dp(i):
+26        #     if i == n:
+27        #         return 1
+28            
+29        #     if s[i] == "0":
+30        #         return 0
+31            
+32        #     res = dp(i + 1)
+33        #     if i + 1 < n and (s[i] == "1" or (s[i] == "2" and s[i + 1] <= "6")):
+34        #         res += dp(i + 2)
+35        #     return res
+36        
+37        # return dp(0)
+38
