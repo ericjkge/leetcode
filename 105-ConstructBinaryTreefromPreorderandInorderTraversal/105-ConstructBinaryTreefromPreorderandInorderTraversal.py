4298548c06c5cfeb2bb2764f44f2fc4dbb1fa96b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-02 10:05:51 -0500

    File updated at 12/2/2025, 10:05:50 AM

--- 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ 105-ConstructBinaryTreefromPreorderandInorderTraversal/105-ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -1,19 +1,19 @@
-# Last updated: 9/10/2025, 11:40:21 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder:
-            return None
-        
-        root = TreeNode(preorder[0])
-        index = inorder.index(preorder[0])
-
-        root.left = self.buildTree(preorder[1:index + 1], inorder[:index])
-        root.right = self.buildTree(preorder[index + 1:], inorder[index + 1:])
-
-        return root
+# Last updated: 12/2/2025, 10:05:50 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+9        if not preorder:
+10            return None
+11
+12        root = TreeNode(preorder[0])
+13        mid = inorder.index(root.val)
+14
+15        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+16        root.right = self.buildTree(preorder[mid + 1: ], inorder[mid + 1:])
+17
+18        return root
