4ed923afd4b3f993420ae76161f9865bc0030c04
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-02 10:37:17 -0500

    File updated at 12/2/2025, 10:37:17 AM

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.py
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 12/2/2025, 10:36:31 AM
+# Last updated: 12/2/2025, 10:37:17 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -7,13 +7,13 @@
 6#         self.right = right
 7class Solution:
 8    def isBalanced(self, root: Optional[TreeNode]) -> bool:
-9        def dfs(node, height):
+9        def dfs(node):
 10            if not node:
-11                return True, height
+11                return True, 0
 12            
-13            lbalanced, lheight = dfs(node.left, height + 1)
-14            rbalanced, rheight = dfs(node.right, height + 1)
+13            lbalanced, lheight = dfs(node.left)
+14            rbalanced, rheight = dfs(node.right)
 15
-16            return lbalanced and rbalanced and abs(lheight - rheight) <= 1, max(lheight, rheight)
+16            return (lbalanced and rbalanced and abs(lheight - rheight) <= 1), max(lheight, rheight) + 1
 17
-18        return dfs(root, 0)[0]
+18        return dfs(root)[0]
