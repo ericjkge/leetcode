575ff2299d748ba7b2f34db51028563c7e669f3a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-06 10:20:31 -0500

    File updated at 12/6/2025, 10:20:31 AM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,11 +1,11 @@
-# Last updated: 11/13/2025, 11:24:12 PM
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = prices[0]
-        max_profit = 0
-
-        for price in prices:
-            min_price = min(min_price, price)
-            max_profit = max(max_profit, price - min_price)
-        
-        return max_profit
+# Last updated: 12/6/2025, 10:20:31 AM
+1class Solution:
+2    def maxProfit(self, prices: List[int]) -> int:
+3        min_price = prices[0]
+4        max_profit = 0
+5
+6        for price in prices:
+7            min_price = min(min_price, price)
+8            max_profit = max(price - min_price, max_profit)
+9
+10        return max_profit
