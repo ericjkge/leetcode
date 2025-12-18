2f948bbebabc2d1ea2bf9c63a0abb2c363f32018
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-17 21:17:57 -0500

    File updated at 12/17/2025, 9:17:54 PM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,29 +1,27 @@
-# Last updated: 10/18/2025, 11:36:05 AM
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if not node:
-            return None
-
-        hashmap = {node:Node(node.val)}
-
-        queue = deque([node])
-        while queue:
-            n = queue.popleft()
-            c = hashmap[n]
-            for neighbor in n.neighbors:
-                if neighbor not in hashmap:
-                    copy = Node(neighbor.val)
-                    hashmap[neighbor] = copy
-                    queue.append(neighbor)
-                c.neighbors.append(hashmap[neighbor])
-        
-        return hashmap[node]
+# Last updated: 12/17/2025, 9:17:54 PM
+1"""
+2# Definition for a Node.
+3class Node:
+4    def __init__(self, val = 0, neighbors = None):
+5        self.val = val
+6        self.neighbors = neighbors if neighbors is not None else []
+7"""
+8
+9from typing import Optional
+10class Solution:
+11    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+12        if not node:
+13            return None
+14
+15        hashmap = {node:Node(node.val)} # original:clone
+16
+17        def dfs(n):
+18            for neighbor in n.neighbors:
+19                if neighbor not in hashmap:
+20                    clone = Node(neighbor.val)
+21                    hashmap[neighbor] = clone
+22                    dfs(neighbor)
+23                hashmap[n].neighbors.append(hashmap[neighbor])
+24
+25        dfs(node)
+26        return hashmap[node]
