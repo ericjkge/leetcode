8b9099b963afc68d5f3b0f1f43ce1b34be5f8eab
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-17 20:48:19 -0500

    File updated at 12/17/2025, 8:48:16 PM

--- 131-PalindromePartitioning/131-PalindromePartitioning.py
+++ 131-PalindromePartitioning/131-PalindromePartitioning.py
@@ -1,22 +1,47 @@
-# Last updated: 9/4/2025, 11:32:20 AM
-class Solution:
-    def partition(self, s: str) -> List[List[str]]:
-        ans = []
-        n = len(s)
-
-        def isPalindrome(string):
-            return string == string[::-1]
-
-        def backtrack(start, path):
-            if start == n:
-                ans.append(path[:])
-                return
-            
-            for i in range(start, n):
-                if isPalindrome(s[start:i + 1]):
-                    path.append(s[start:i + 1])
-                    backtrack(i + 1, path)
-                    path.pop()
-            
-        backtrack(0, [])
-        return ans
+# Last updated: 12/17/2025, 8:48:16 PM
+1class Solution:
+2    def partition(self, s: str) -> List[List[str]]:
+3        n = len(s)
+4        ans = []
+5
+6        def backtrack(index, path):
+7            if index == n:
+8                ans.append(path[:])
+9                return
+10
+11            for i in range(index, n):
+12                if s[index:i + 1] == s[index:i + 1][::-1]:
+13                    path.append(s[index:i + 1])
+14                    backtrack(i + 1, path)
+15                    path.pop()
+16        
+17        backtrack(0, [])
+18        return ans
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28        # ans = []
+29        # n = len(s)
+30
+31        # def isPalindrome(string):
+32        #     return string == string[::-1]
+33
+34        # def backtrack(start, path):
+35        #     if start == n:
+36        #         ans.append(path[:])
+37        #         return
+38            
+39        #     for i in range(start, n):
+40        #         if isPalindrome(s[start:i + 1]):
+41        #             path.append(s[start:i + 1])
+42        #             backtrack(i + 1, path)
+43        #             path.pop()
+44            
+45        # backtrack(0, [])
+46        # return ans
