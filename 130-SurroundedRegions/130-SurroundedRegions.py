44653ad627614266d31b47ce8aac081a8c497f4d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-17 20:11:44 -0500

    File updated at 12/17/2025, 8:11:43 PM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,40 +1,35 @@
-# Last updated: 10/19/2025, 9:41:40 AM
-class Solution:
-    def solve(self, board: List[List[str]]) -> None:
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        m, n = len(board), len(board[0])
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        seen = set()
-
-        def dfs(r, c):
-            board[r][c] = "S"
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in seen and board[nr][nc] == "O":
-                    seen.add((nr, nc))
-                    dfs(nr, nc)
-        
-        for r in range(m):
-            if board[r][0] == "O":
-                seen.add((r, 0))
-                dfs(r, 0)
-            if board[r][n - 1] == "O":
-                seen.add((r, n - 1))
-                dfs(r, n - 1)
-
-        for c in range(n):
-            if board[0][c] == "O":
-                seen.add((0, c))
-                dfs(0, c)
-            if board[m - 1][c] == "O":
-                seen.add((m - 1, c))
-                dfs(m - 1, c)
-        
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == "S":
-                    board[r][c] = "O"
-                elif board[r][c] == "O":
-                    board[r][c] = "X"
+# Last updated: 12/17/2025, 8:11:43 PM
+1class Solution:
+2    def solve(self, board: List[List[str]]) -> None:
+3        """
+4        Do not return anything, modify board in-place instead.
+5        """
+6        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+7        rows, cols = len(board), len(board[0])
+8
+9        def dfs(r, c):
+10            board[r][c] = "S"
+11            for dr, dc in directions:
+12                nr, nc = r + dr, c + dc
+13                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "O":
+14                    dfs(nr, nc)
+15
+16        for col in range(cols):
+17            if board[0][col] == "O":
+18                dfs(0, col)
+19            if board[rows - 1][col] == "O":
+20                dfs(rows - 1, col)
+21        
+22        for row in range(rows):
+23            if board[row][0] == "O":
+24                dfs(row, 0)
+25            if board[row][cols - 1] == "O":
+26                dfs(row, cols - 1)
+27        
+28        for row in range(rows):
+29            for col in range(cols):
+30                if board[row][col] == "S":
+31                    board[row][col] = "O"
+32                elif board[row][col] == "O":
+33                    board[row][col] = "X"
+34        
