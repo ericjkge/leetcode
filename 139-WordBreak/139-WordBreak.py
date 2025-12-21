1cc3153691cd7bef5a2b2f40f5ffab9874ae879f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-21 09:15:57 +0800

    File updated at 12/21/2025, 9:15:56 AM

--- 139-WordBreak/139-WordBreak.py
+++ 139-WordBreak/139-WordBreak.py
@@ -1,18 +1,22 @@
-# Last updated: 10/17/2025, 11:42:31 AM
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        wordSet = set(wordDict)
-
-        @cache
-        def dp(i):
-            if i == len(s):
-                return True
-            
-            for word in wordSet:
-                if s[i:i + len(word)] == word:
-                    if dp(i + len(word)):
-                        return True
-            
-            return False
-        
-        return dp(0)
+# Last updated: 12/21/2025, 9:15:56 AM
+1class Solution:
+2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+3        wordSet = set(wordDict)
+4        n = len(s)
+5
+6        @cache
+7        def dp(i):
+8            if i == n:
+9                return True
+10
+11            if i > n:
+12                return False
+13
+14            for word in wordSet:
+15                if s[i:i + len(word)] == word:
+16                    if dp(i + len(word)):
+17                        return True
+18            
+19            return False
+20
+21        return dp(0)
