9b704f24fc8c2fa2db9041993be63e4b981c9e43
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-22 19:05:11 +0800

    File updated at 12/22/2025, 7:05:10 PM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.py
@@ -1,19 +1,20 @@
-# Last updated: 6/25/2025, 3:52:53 PM
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        operations = {
-            "+": lambda a, b: a + b,
-            "-": lambda a, b: a - b,
-            "*": lambda a, b: a * b,
-            "/": lambda a, b: int(a / b)
-        }
-        
-        stack = []
-        for token in tokens:
-            if token not in operations.keys():
-                stack.append(int(token))
-            else:
-                b = stack.pop()
-                a = stack.pop()
-                stack.append(operations[token](a, b))
-        return stack[0]
+# Last updated: 12/22/2025, 7:05:10 PM
+1class Solution:
+2    def evalRPN(self, tokens: List[str]) -> int:
+3        operations = {
+4           "+": lambda a, b: a + b,
+5           "-": lambda a, b: a - b,
+6           "*": lambda a, b: a * b,
+7           "/": lambda a, b: a / b 
+8        }
+9
+10        stack = []
+11        for token in tokens:
+12            if token in operations:
+13                b, a = int(stack.pop()), int(stack.pop())
+14                res = operations[token](a, b)
+15                stack.append(res)
+16            else:
+17                stack.append(token)
+18        
+19        return int(stack[0])
