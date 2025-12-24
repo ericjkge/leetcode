0ac1241390711f5def5256fce8a3791e8524b1af
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-24 10:04:52 +0800

    File updated at 12/24/2025, 10:04:52 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,16 +1,10 @@
-# Last updated: 8/6/2025, 12:07:47 AM
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left, right = 0, len(numbers) - 1
-        
-        while left < right:
-            total = numbers[left] + numbers[right]
-            if total == target:
-                break
-            if total < target:
-                left += 1
-            else:
-                right -= 1
-
-        
-        return [left + 1, right + 1]
+# Last updated: 12/24/2025, 10:04:52 AM
+1class Solution:
+2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+3        hashmap = {}
+4
+5        for i, num in enumerate(numbers):
+6            remainder = target - num
+7            if remainder in hashmap:
+8                return [hashmap[remainder] + 1, i + 1]
+9            hashmap[num] = i
