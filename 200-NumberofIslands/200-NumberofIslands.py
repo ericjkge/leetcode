0b80cc6e71ae3b88b6f65f4441a1f5b417afe57d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-26 23:22:30 +0800

    File updated at 12/26/2025, 11:22:29 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,45 +1,22 @@
-# Last updated: 11/14/2025, 10:17:45 AM
-from typing import List
-import atexit
-atexit.register(lambda: open("display_runtime.txt", "w").write("0"))
-
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.count = 0
-
-    def find(self, i):
-        if self.parent[i] != i:
-            self.parent[i] = self.find(self.parent[i])  # Path compression
-        return self.parent[i]
-
-    def union(self, i, j):
-        irep = self.find(i)
-        jrep = self.find(j)
-        
-        if irep != jrep:
-            self.parent[irep] = jrep
-            self.count -= 1  # Merging two islands reduces count by 1
-
-
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-
-        uf = UnionFind(m * n)
-
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1":
-                    uf.count += 1  # Initialize each "1" as an island
-                    
-                    # look left
-                    if j > 0 and grid[i][j - 1] == "1":
-                        uf.union(i * n + j, i * n + (j - 1))
-
-                    # look up
-                    if i > 0 and grid[i - 1][j] == "1":
-                        uf.union(i * n + j, (i - 1) * n + j)
-
-        return uf.count
+# Last updated: 12/26/2025, 11:22:29 PM
+1class Solution:
+2    def numIslands(self, grid: List[List[str]]) -> int:
+3        rows, cols = len(grid), len(grid[0])
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5
+6        def dfs(r, c):
+7            for dr, dc in directions:
+8                nr, nc = r + dr, c + dc
+9                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
+10                    grid[nr][nc] = "-1"
+11                    dfs(nr, nc)
+12
+13        counter = 0
+14        for r in range(rows):
+15            for c in range(cols):
+16                if grid[r][c] == "1":
+17                    counter += 1
+18                    grid[r][c] = "-1"
+19                    dfs(r, c)
+20        
+21        return counter
