685651ab3695cc45ac0e6e12fcda8c4bce41c9da
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-26 23:03:21 +0800

    File updated at 12/26/2025, 11:03:21 PM

--- 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
@@ -1,25 +1,27 @@
-# Last updated: 9/8/2025, 6:56:23 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return []
-        ans = []
-
-        queue = deque([root])
-        while queue:
-            for i in range(len(queue)):
-                node = queue.popleft()
-                if i == 0:
-                    ans.append(node.val)
-                if node.right:
-                    queue.append(node.right)
-                if node.left:
-                    queue.append(node.left)
-        
-        return ans
+# Last updated: 12/26/2025, 11:03:21 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+9        if not root:
+10            return []
+11
+12        ans = []
+13        queue = deque([root])
+14        while queue:
+15            n = len(queue)
+16            for i in range(n):
+17                node = queue.popleft()
+18                if node.left:
+19                    queue.append(node.left)
+20                if node.right:
+21                    queue.append(node.right)
+22                if i == n - 1:
+23                    ans.append(node.val)
+24        
+25        return ans
+26            
