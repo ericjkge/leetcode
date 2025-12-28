ff3818bd6c8c344d774ef09f34a3ab82b1e03bc6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-28 10:17:13 +0800

    File updated at 12/28/2025, 10:17:13 AM

--- 213-HouseRobberII/213-HouseRobberII.py
+++ 213-HouseRobberII/213-HouseRobberII.py
@@ -1,16 +1,15 @@
-# Last updated: 12/28/2025, 10:15:29 AM
+# Last updated: 12/28/2025, 10:17:13 AM
 1class Solution:
 2    def rob(self, nums: List[int]) -> int:
 3        n = len(nums)
-4
-5        if n == 1:
-6            return nums[0]
-7
-8        @cache
-9        def dp(i, j):
-10            if i >= j:
-11                return 0
-12            
-13            return max(nums[i] + dp(i + 2, j), dp(i + 1, j))
-14        
-15        return max(dp(0, n - 1), dp(1, n))
+4        if n == 1:
+5            return nums[0]
+6
+7        @cache
+8        def dp(i, j):
+9            if i >= j:
+10                return 0
+11            
+12            return max(nums[i] + dp(i + 2, j), dp(i + 1, j))
+13        
+14        return max(dp(0, n - 1), dp(1, n))
