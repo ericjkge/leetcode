8a9a501c77fbe99dacf20efe4785b452e4e90fc8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-28 10:24:55 +0800

    File updated at 12/28/2025, 10:24:54 AM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.py
@@ -1,12 +1,11 @@
-# Last updated: 8/29/2025, 10:36:37 PM
-class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-
-        for n in nums:
-            heapq.heappush(heap, -n)
-        
-        for _ in range(k - 1):
-            heapq.heappop(heap)
-        
-        return -heapq.heappop(heap)
+# Last updated: 12/28/2025, 10:24:54 AM
+1class Solution:
+2    def findKthLargest(self, nums: List[int], k: int) -> int:
+3        heap = []
+4        for num in nums:
+5            heapq.heappush(heap, -num)
+6        
+7        for _ in range(k - 1):
+8            heapq.heappop(heap)
+9
+10        return -heapq.heappop(heap)
