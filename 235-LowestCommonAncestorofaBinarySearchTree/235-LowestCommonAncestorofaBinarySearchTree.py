df8785be6fcc1b23843963efa45e5dae209204cb
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-29 20:40:30 +0800

    File updated at 12/29/2025, 8:40:29 PM

--- 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
+++ 235-LowestCommonAncestorofaBinarySearchTree/235-LowestCommonAncestorofaBinarySearchTree.py
@@ -1,18 +1,25 @@
-# Last updated: 9/15/2025, 1:03:18 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return None
-
-        if root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
-        elif root.val < p.val and root.val < q.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        return root
+# Last updated: 12/29/2025, 8:40:29 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, x):
+4#         self.val = x
+5#         self.left = None
+6#         self.right = None
+7
+8class Solution:
+9    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+10        if not root:
+11            return None
+12
+13        if root == p or root == q:
+14            return root
+15        
+16        left = self.lowestCommonAncestor(root.left, p, q)
+17        right = self.lowestCommonAncestor(root.right, p, q)
+18        
+19        if left and right:
+20            return root
+21        elif left:
+22            return left
+23        else:
+24            return right
