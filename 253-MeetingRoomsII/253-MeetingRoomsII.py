8229d8088a65327d2a8bc322ed7016191e0951a0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-30 23:59:57 +0800

    File updated at 12/30/2025, 11:59:56 PM

--- 253-MeetingRoomsII/253-MeetingRoomsII.py
+++ 253-MeetingRoomsII/253-MeetingRoomsII.py
@@ -1,4 +1,4 @@
-# Last updated: 12/30/2025, 11:58:48 PM
+# Last updated: 12/30/2025, 11:59:56 PM
 1class Solution:
 2    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 3        rooms = 0
@@ -9,8 +9,7 @@
 8            while waiting and waiting[0] <= start:
 9                heapq.heappop(waiting)
 10
-11            if not waiting or start < waiting[0]:
-12                heapq.heappush(waiting, end)
-13                rooms = max(rooms, len(waiting))
-14        
-15        return rooms
+11            heapq.heappush(waiting, end)
+12            rooms = max(rooms, len(waiting))
+13    
+14        return rooms
