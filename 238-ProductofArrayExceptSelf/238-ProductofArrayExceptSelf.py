1338192b99f545502c4a510a5e8e2e0c896fec96
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-30 23:26:59 +0800

    File updated at 12/30/2025, 11:26:59 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,23 +1,21 @@
-# Last updated: 9/19/2025, 10:52:07 PM
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        prefix = [nums[0]]
-        
-        for i in range(1, n):
-            prefix.append(prefix[-1] * nums[i])
-
-        suffix = [nums[n - 1]]
-        
-        for j in range(n - 2, -1, -1):
-            suffix.append(suffix[-1] * nums[j])
-        suffix.reverse()
-
-        ans = []
-
-        for k in range(n):
-            left = prefix[k - 1] if k - 1 >= 0 else 1
-            right = suffix[k + 1] if k + 1 < n else 1
-            ans.append(left * right)
-        
-        return ans
+# Last updated: 12/30/2025, 11:26:59 PM
+1class Solution:
+2    def productExceptSelf(self, nums: List[int]) -> List[int]:
+3        n = len(nums)
+4        ans = [0] * n
+5
+6        prefix = [nums[0]]
+7        for i in range(1, n):
+8            prefix.append(prefix[-1] * nums[i])
+9
+10        suffix = [nums[-1]]
+11        for j in range(n - 2, -1, -1):
+12            suffix.append(suffix[-1] * nums[j])
+13        suffix = suffix[::-1]
+14
+15        for k in range(n):
+16            pre = prefix[k - 1] if k - 1 >= 0 else 1
+17            suf = suffix[k + 1] if k + 1 < n else 1
+18            ans[k] = pre * suf
+19
+20        return ans
