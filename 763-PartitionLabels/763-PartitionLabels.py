7482204976b53db9818173098d2c029fc1bcea15
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-31 00:28:27 +0800

    File updated at 12/31/2025, 12:28:26 AM

--- 763-PartitionLabels/763-PartitionLabels.py
+++ 763-PartitionLabels/763-PartitionLabels.py
@@ -1,21 +1,16 @@
-# Last updated: 12/31/2025, 12:23:32 AM
+# Last updated: 12/31/2025, 12:28:26 AM
 1class Solution:
 2    def partitionLabels(self, s: str) -> List[int]:
 3        last = {char: i for i, char in enumerate(s)}
 4
-5        left = right = 0
-6        partitions = []
-7        counter = 0
-8
-9        while right < len(s):
-10            right = last[s[right]]
-11            counter += 1
-12            while left < right:
-13                right = max(right, last[s[left]])
-14                counter += 1
-15                left += 1
-16            right += 1
-17            partitions.append(counter)
-18            counter = -1
-19
-20        return partitions
+5        partitions = []
+6        start = end = 0
+7
+8        for i, char in enumerate(s):
+9            end = max(end, last[char])
+10
+11            if i == end:
+12                partitions.append(end - start + 1)
+13                start = i + 1
+14
+15        return partitions
