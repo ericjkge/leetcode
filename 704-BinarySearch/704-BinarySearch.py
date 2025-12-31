1285d2038bcb31fae22eb8bf3eb57b993e241625
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-31 11:02:49 +0800

    File updated at 12/31/2025, 11:02:49 AM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,15 +1,17 @@
-# Last updated: 7/24/2025, 3:53:56 PM
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        while left + 1 < right:
-            mid = (left + right) // 2
-            if nums[mid] <= target:
-                left = mid
-            else:
-                right = mid
-        if nums[left] == target:
-            return left
-        if nums[right] == target:
-            return right
-        return -1
+# Last updated: 12/31/2025, 11:02:49 AM
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> int:
+3        left, right = 0, len(nums) - 1
+4
+5        while left + 1 < right:
+6            mid = (left + right) // 2
+7            if nums[mid] > target:
+8                right = mid
+9            else:
+10                left = mid
+11        
+12        if nums[left] == target:
+13            return left
+14        if nums[right] == target:
+15            return right
+16        return -1
