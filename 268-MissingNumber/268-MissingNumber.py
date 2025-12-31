d5989d72be9bf5492a66019417f8d23190b0fdf4
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-31 10:14:27 +0800

    File updated at 12/31/2025, 10:14:27 AM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,8 +1,14 @@
-# Last updated: 5/30/2025, 12:07:57 PM
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        seen = set(nums)
-        for i in range(len(nums) + 1):
-            if i not in seen:
-                return i
-        return
+# Last updated: 12/31/2025, 10:14:27 AM
+1class Solution:
+2    def missingNumber(self, nums: List[int]) -> int:
+3        n = len(nums)
+4        nums.sort()
+5
+6        for i in range(n):
+7            if i > 0 and nums[i] != nums[i - 1] + 1:
+8                return nums[i] - 1
+9        
+10        if nums[0] != 0:
+11            return 0
+12
+13        return n
