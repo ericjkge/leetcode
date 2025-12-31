2c50faecd886e12f63d771e418367d6d4c4ed1f6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2025-12-31 10:20:13 +0800

    File updated at 12/31/2025, 10:20:12 AM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,14 +1,7 @@
-# Last updated: 12/31/2025, 10:14:27 AM
+# Last updated: 12/31/2025, 10:20:12 AM
 1class Solution:
 2    def missingNumber(self, nums: List[int]) -> int:
 3        n = len(nums)
-4        nums.sort()
-5
-6        for i in range(n):
-7            if i > 0 and nums[i] != nums[i - 1] + 1:
-8                return nums[i] - 1
-9        
-10        if nums[0] != 0:
-11            return 0
-12
-13        return n
+4        expected = n * (n + 1) // 2
+5        actual = sum(nums)
+6        return expected - actual
