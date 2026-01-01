6b10393d77d7bd3685d44a5cc6a994ad461a4882
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-01 10:13:45 +0800

    File updated at 1/1/2026, 10:13:44 AM

--- 286-WallsandGates/286-WallsandGates.py
+++ 286-WallsandGates/286-WallsandGates.py
@@ -1,27 +1,27 @@
-# Last updated: 10/12/2025, 9:59:03 AM
-class Solution:
-    def wallsAndGates(self, rooms: List[List[int]]) -> None:
-        """
-        Do not return anything, modify rooms in-place instead.
-        """
-
-        INF = 2 ** 31 - 1
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        queue = deque()
-        rows, cols = len(rooms), len(rooms[0])
-
-        for r in range(rows):
-            for c in range(cols):
-                if rooms[r][c] == 0:
-                    queue.append((r, c))
-        
-        moves = 1
-        while queue:
-            for _ in range(len(queue)):
-                r, c = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == INF:
-                        rooms[nr][nc] = moves
-                        queue.append((nr, nc))
-            moves += 1
+# Last updated: 1/1/2026, 10:13:44 AM
+1class Solution:
+2    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+3        """
+4        Do not return anything, modify rooms in-place instead.
+5        """
+6        INF = 2 ** 31 - 1
+7        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+8        rows, cols = len(rooms), len(rooms[0])
+9        queue = deque()
+10
+11        for r in range(rows):
+12            for c in range(cols):
+13                if rooms[r][c] == 0:
+14                    queue.append((r, c))
+15        
+16        moves = 1
+17        while queue:
+18            for _ in range(len(queue)):
+19                r, c = queue.popleft()
+20                for dr, dc in directions:
+21                    nr, nc = r + dr, c + dc
+22                    if 0 <= nr < rows and 0 <= nc < cols and rooms[nr][nc] == INF:
+23                        rooms[nr][nc] = moves
+24                        queue.append((nr, nc))
+25            moves += 1
+26        
