eb23472a079a26ef24de51d1b25f001a64a00196
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-01 09:54:37 +0800

    File updated at 1/1/2026, 9:54:37 AM

--- 778-SwiminRisingWater/778-SwiminRisingWater.py
+++ 778-SwiminRisingWater/778-SwiminRisingWater.py
@@ -1,29 +1,31 @@
-# Last updated: 1/1/2026, 9:33:41 AM
+# Last updated: 1/1/2026, 9:54:37 AM
 1class Solution:
 2    def swimInWater(self, grid: List[List[int]]) -> int:
 3        n = len(grid)
-4        distances = [[float("inf") for _ in range(n)] for _ in range(n)]
-5        distances[0][0] = grid[0][0]
-6        heap = [(grid[0][0], 0, 0)] # 0, source
-7        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-8
-9        while heap:
-10            dist, r, c = heapq.heappop(heap)
-11
-12            if r == n - 1 and c == n - 1:
-13                return dist
-14
-15            if dist > distances[r][c]:
-16                continue
-17            
-18            for dr, dc in directions:
-19                nr, nc = r + dr, c + dc
-20                if 0 <= nr < n and 0 <= nc < n and distances[nr][nc] > dist + grid[nr][nc]:
-21                    new_dist = max(dist, grid[nr][nc])
-22                    if new_dist < distances[nr][nc]:
-23                        distances[nr][nc] = new_dist
-24                        heapq.heappush(heap, (new_dist, nr, nc))
-25        
-26        return max(max(distances[r]) for r in range(n))
-27
-28
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5
+6        def dfs(r, c, time):
+7            if r == n - 1 and c == n - 1:
+8                return True
+9            seen.add((r, c))
+10
+11            for dr, dc in directions:
+12                nr, nc = r + dr, c + dc
+13                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] <= time and (nr, nc) not in seen:
+14                    if dfs(nr, nc, time):
+15                        return True
+16            return False
+17
+18        left, right = 0, n * n - 1
+19
+20        while left + 1 < right:
+21            seen = set()
+22            mid = (left + right) // 2
+23            if grid[0][0] <= mid and dfs(0, 0, mid):
+24                right = mid
+25            else:
+26                left = mid
+27        
+28        if grid[0][0] <= left and dfs(0, 0, left):
+29            return left
+30        return right
