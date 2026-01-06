16a16313084b549870f13bf8370aba5fd3908d37
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-06 10:10:09 +0800

    File updated at 1/6/2026, 10:10:09 AM

--- 338-CountingBits/338-CountingBits.py
+++ 338-CountingBits/338-CountingBits.py
@@ -1,4 +1,4 @@
-# Last updated: 1/6/2026, 10:09:32 AM
+# Last updated: 1/6/2026, 10:10:09 AM
 1class Solution:
 2    def countBits(self, n: int) -> List[int]:
 3        ans = []
@@ -6,9 +6,8 @@
 5        for i in range(n + 1):
 6            counter = 0
 7            while i:
-8                if i & 1:
-9                    counter += 1
-10                i >>= 1
-11            ans.append(counter)
-12        
-13        return ans
+8                counter += i & 1
+9                i >>= 1
+10            ans.append(counter)
+11        
+12        return ans
