c94d90af303584d54ace2ea563aab18e309a55a1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-06 10:02:34 +0800

    File updated at 1/6/2026, 10:02:34 AM

--- 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
+++ 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
@@ -1,21 +1,25 @@
-# Last updated: 9/26/2025, 9:32:25 AM
-class Solution:
-    def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        for start, end in edges:
-            graph[start].append(end)
-            graph[end].append(start)
-
-        seen = set()
-        def dfs(node):
-            seen.add(node)
-            for neighbor in graph[node]:
-                if neighbor not in seen:
-                    dfs(neighbor)
-
-        ans = 0
-        for i in range(n):
-            if i not in seen:
-                ans += 1
-                dfs(i)
-        return ans
+# Last updated: 1/6/2026, 10:02:34 AM
+1class Solution:
+2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+3        graph = defaultdict(list)
+4        
+5        for u, v in edges:
+6            graph[u].append(v)
+7            graph[v].append(u)
+8
+9        def dfs(node):
+10            for neighbor in graph[node]:
+11                if neighbor not in seen:
+12                    seen.add(neighbor)
+13                    dfs(neighbor)
+14        
+15        seen = set()
+16        counter = 0
+17
+18        for i in range(n):
+19            if i not in seen:
+20                # seen.add(i)
+21                dfs(i)
+22                counter += 1
+23        
+24        return counter
