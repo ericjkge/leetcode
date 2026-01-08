2a54f7ce341d33aa5b0aec0ac0fcfb35a9bce941
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-08 10:59:11 +0800

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -0,0 +1,22 @@
+# Last updated: 1/8/2026, 10:59:11 AM
+1class Solution:
+2    def characterReplacement(self, s: str, k: int) -> int:
+3        left, right = 0, 0
+4        longest = 0
+5        freqs = defaultdict(int)
+6
+7        while right < len(s):
+8            freqs[s[right]] += 1
+9            while len(freqs) > 1 and sum(freqs.values()) - max(freqs.values()) > k:
+10                letter = s[left]
+11                freqs[letter] -= 1
+12                if freqs[letter] == 0:
+13                    del freqs[letter]
+14                left += 1
+15            longest = max(longest, right - left + 1)
+16            right += 1
+17        
+18        return longest
+19
+20
+21        return longest
