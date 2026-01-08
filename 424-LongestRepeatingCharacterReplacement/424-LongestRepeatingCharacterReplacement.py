8318ad92c6fe60534b0bff06f25588e06cef452d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-08 11:02:04 +0800

    File updated at 1/8/2026, 11:02:03 AM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,22 +1,23 @@
-# Last updated: 1/8/2026, 11:00:53 AM
+# Last updated: 1/8/2026, 11:02:03 AM
 1class Solution:
 2    def characterReplacement(self, s: str, k: int) -> int:
-3        left, right = 0, 0
-4        longest = 0
-5        freqs = defaultdict(int)
-6
-7        while right < len(s):
-8            freqs[s[right]] += 1
-9            while sum(freqs.values()) - max(freqs.values()) > k:
-10                letter = s[left]
-11                freqs[letter] -= 1
-12                if freqs[letter] == 0:
-13                    del freqs[letter]
-14                left += 1
-15            longest = max(longest, right - left + 1)
-16            right += 1
-17        
-18        return longest
-19
-20
-21        return longest
+3        left = 0
+4        freqs = {}
+5        max_f = 0
+6        longest = 0
+7
+8        for right in range(len(s)):
+9            # Update frequency and current max frequency
+10            freqs[s[right]] = freqs.get(s[right], 0) + 1
+11            max_f = max(max_f, freqs[s[right]])
+12
+13            # Window is invalid if: (window size - max_f) > k
+14            # We only need to shift the left pointer once (if) 
+15            # because we only care about expanding the maximum window found so far.
+16            if (right - left + 1) - max_f > k:
+17                freqs[s[left]] -= 1
+18                left += 1
+19            
+20            longest = right - left + 1
+21            
+22        return longest
