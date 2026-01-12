7fda8a665b6c6a74064b480e873333b7b762cd30
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-12 10:52:10 +0800

    File updated at 1/12/2026, 10:52:09 AM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,12 +1,13 @@
-# Last updated: 11/19/2025, 11:19:27 AM
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        prefix = min_prefix = 0
-        best = -float("inf")
-
-        for i in range(len(nums)):
-            prefix += nums[i]
-            best = max(best, prefix - min_prefix)
-            min_prefix = min(prefix, min_prefix)
-        
-        return best
+# Last updated: 1/12/2026, 10:52:09 AM
+1class Solution:
+2    def maxSubArray(self, nums: List[int]) -> int:
+3        best = nums[0]
+4        curr = nums[0]
+5
+6        for num in nums[1:]:
+7            if curr < 0:
+8                curr = 0
+9            curr += num
+10            best = max(best, curr)
+11
+12        return best
