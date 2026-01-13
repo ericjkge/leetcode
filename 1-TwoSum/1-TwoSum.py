aa1326731753f448cf2ae14838d4f00d346ecc56
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-13 23:31:57 +0800

    File updated at 1/13/2026, 11:31:56 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,14 +1,15 @@
-# Last updated: 11/13/2025, 10:52:20 PM
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hashmap = {}
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap:
-                return [hashmap[complement], i]
-            hashmap[nums[i]] = i
+# Last updated: 1/13/2026, 11:31:56 PM
+1class Solution(object):
+2    def twoSum(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: List[int]
+7        """
+8        hashmap = {}
+9        for i in range(len(nums)):
+10            complement = target - nums[i]
+11            if complement in hashmap:
+12                return [hashmap[complement], i]
+13            hashmap[nums[i]] = i
+14
