cfa897b7a25194ed781c77262468e689ef81ec8e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-14 23:19:33 +0800

    File updated at 1/14/2026, 11:19:33 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,16 +1,16 @@
-# Last updated: 11/25/2025, 7:16:48 PM
-class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        n = len(coins)
-
-        @cache
-        def dp(i, j):
-            if i < 0 or j == n:
-                return 0
-
-            if i == 0:
-                return 1
-            
-            return dp(i - coins[j], j) + dp(i, j + 1)
-
-        return dp(amount, 0)
+# Last updated: 1/14/2026, 11:19:33 PM
+1class Solution:
+2    def change(self, amount: int, coins: List[int]) -> int:
+3        n = len(coins)
+4        
+5        @cache
+6        def dp(i, j): # index, amount
+7            if j > amount or i == n:
+8                return 0
+9            
+10            if j == amount:
+11                return 1
+12
+13            return dp(i, j + coins[i]) + dp(i + 1, j)
+14
+15        return dp(0, 0)
