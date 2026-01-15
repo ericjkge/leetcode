dc7d4ebb3d5be41790d68cc23f7782d2258b14c2
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-15 11:53:50 +0800

    File updated at 1/15/2026, 11:53:49 AM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,29 +1,32 @@
-# Last updated: 10/12/2025, 1:16:01 AM
-class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        queue = deque()
-        m, n = len(grid), len(grid[0])
-        time = 0
-        fresh = 0
-
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c] == 2:
-                    queue.append((r, c))
-                elif grid[r][c] == 1:
-                    fresh += 1
-        
-        while queue and fresh > 0:
-            for _ in range(len(queue)):
-                r, c = queue.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        fresh -= 1
-                        queue.append((nr, nc))
-            time += 1
-        
-        return time if fresh == 0 else -1
-        
+# Last updated: 1/15/2026, 11:53:49 AM
+1class Solution:
+2    def orangesRotting(self, grid: List[List[int]]) -> int:
+3        time = 0
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5        rows, cols = len(grid), len(grid[0])
+6        rotten = deque()
+7        fresh = 0
+8        time = 0
+9
+10        for r in range(rows):
+11            for c in range(cols):
+12                if grid[r][c] == 2:
+13                    rotten.append((r, c))
+14                if grid[r][c] == 1:
+15                    fresh += 1
+16        
+17        if rotten:
+18            time = -1
+19
+20        while rotten:
+21            for _ in range(len(rotten)):
+22                r, c = rotten.popleft()
+23                for dr, dc in directions:
+24                    nr, nc = r + dr, c + dc
+25                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+26                        grid[nr][nc] = 2
+27                        fresh -= 1
+28                        rotten.append((nr, nc))
+29            time += 1
+30        
+31        return time if fresh == 0 else -1
