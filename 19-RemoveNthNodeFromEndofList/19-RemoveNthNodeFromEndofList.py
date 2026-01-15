8038cc57f38fdc5bd1ff2c675f686b993bc598b9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-15 13:00:22 +0800

    File updated at 1/15/2026, 1:00:21 PM

--- 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
+++ 19-RemoveNthNodeFromEndofList/19-RemoveNthNodeFromEndofList.py
@@ -1,24 +1,20 @@
-# Last updated: 11/7/2025, 7:20:24 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        if not head.next:
-            return None
-
-        dummy = ListNode(0, head)
-        slow = fast = dummy
-
-        for _ in range(n):
-            fast = fast.next
-                
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next
-        
-        slow.next = slow.next.next
-
-        return dummy.next
+# Last updated: 1/15/2026, 1:00:21 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+8        dummy = slow = fast = ListNode(0, head)
+9
+10        for _ in range(n):
+11            fast = fast.next
+12        
+13        while fast and fast.next:
+14            slow = slow.next
+15            fast = fast.next
+16        
+17        slow.next = slow.next.next
+18
+19        return dummy.next
