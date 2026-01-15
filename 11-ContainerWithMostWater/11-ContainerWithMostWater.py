5a960afd29b8136e023ab8f00a9d0cc166fdfa2a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-15 10:19:33 +0800

    File updated at 1/15/2026, 10:19:28 AM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,17 +1,14 @@
-# Last updated: 1/15/2026, 10:17:59 AM
+# Last updated: 1/15/2026, 10:19:28 AM
 1class Solution:
 2    def maxArea(self, height: List[int]) -> int:
 3        left, right = 0, len(height) - 1
-4        mleft, mright = height[left], height[right]
-5        amount = min(mleft, mright) * (right - left)
-6
-7        while left < right:
+4        amount = 0
+5
+6        while left < right:
+7            amount = max(amount, min(height[left], height[right]) * (right - left))
 8            if height[left] < height[right]:
 9                left += 1
-10                mleft = max(height[left], mleft)
-11            else:
-12                right -= 1
-13                mright = max(height[right], mright)
-14            amount = max(amount, min(mleft, mright) * (right - left))
-15
-16        return amount
+10            else:
+11                right -= 1
+12
+13        return amount
