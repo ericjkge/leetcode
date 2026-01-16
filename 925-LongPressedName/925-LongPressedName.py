e9fd7e167bb4d9ad17f5587fffee82cc78e35e93
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-16 19:15:39 +0800

    File updated at 1/16/2026, 7:15:39 PM

--- 925-LongPressedName/925-LongPressedName.py
+++ 925-LongPressedName/925-LongPressedName.py
@@ -1,27 +1,14 @@
-# Last updated: 1/16/2026, 7:10:52 PM
+# Last updated: 1/16/2026, 7:15:39 PM
 1class Solution:
 2    def isLongPressedName(self, name: str, typed: str) -> bool:
-3        p1, p2 = 0, 0
+3        i = 0
 4
-5        if name[0] != typed[0]:
-6            return False
-7
-8        while p1 < len(name) and p2 < len(typed):
-9            if name[p1] == typed[p2]:
-10                p1 += 1
-11                p2 += 1
-12            elif name[p1 - 1] == typed[p2]:
-13                p2 += 1
-14            else:
-15                return False
-16
-17        while p2 < len(typed):
-18            if typed[p2] == name[-1]:
-19                p2 += 1
-20            else:
-21                return False
-22        
-23        if p1 < len(name):
-24            return False
-25
-26        return True
+5        for j in range(len(typed)):
+6            if i < len(name) and name[i] == typed[j]:
+7                i += 1
+8            elif j > 0 and typed[j] == typed[j - 1]:
+9                continue
+10            else:
+11                return False
+12        
+13        return i == len(name)
