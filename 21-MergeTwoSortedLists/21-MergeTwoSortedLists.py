8ac505400405bc7a4bc3c36d92d3afa3709182b5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-17 11:40:37 +0800

    File updated at 1/17/2026, 11:40:37 AM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -1,23 +1,20 @@
-# Last updated: 11/8/2025, 8:36:02 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = curr = ListNode()
-
-        while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
-            else:
-                curr.next = list2
-                list2 = list2.next
-            curr = curr.next
-        
-        curr.next = list1 if list1 else list2
-        
-        return dummy.next
-        
+# Last updated: 1/17/2026, 11:40:37 AM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+8        dummy = curr = ListNode()
+9
+10        while list1 or list2:
+11            if not list2 or list1 and list1.val < list2.val:
+12                curr.next = list1
+13                list1 = list1.next
+14            else:
+15                curr.next = list2
+16                list2 = list2.next
+17            curr = curr.next
+18        
+19        return dummy.next
