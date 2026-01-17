8c763ffd8d42f1929e6ccacfe9cbbcf3def8ca43
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-17 12:57:24 +0800

    File updated at 1/17/2026, 12:57:24 PM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,31 +1,30 @@
-# Last updated: 11/24/2025, 10:29:12 AM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        dummy = group_prev = ListNode(0, head)
-
-        while True:
-            kth = group_prev
-            for _ in range(k):
-                kth = kth.next
-                if not kth:
-                    return dummy.next
-            
-            group_next = kth.next
-
-            prev = group_next
-            curr = group_prev.next
-
-            while curr != group_next:
-                nxt = curr.next
-                curr.next = prev
-                prev = curr
-                curr = nxt
-            
-            temp = group_prev.next
-            group_prev.next = prev
-            group_prev = temp
+# Last updated: 1/17/2026, 12:57:24 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+8        dummy = group_prev = ListNode(0, head)
+9
+10        while True:
+11            kth = group_prev
+12            for _ in range(k):
+13                kth = kth.next
+14                if not kth:
+15                    return dummy.next
+16
+17            group_next = kth.next
+18            prev, curr = group_next, group_prev.next
+19
+20            while curr != group_next:
+21                nxt = curr.next
+22                curr.next = prev
+23                prev = curr
+24                curr = nxt
+25
+26            temp = group_prev.next
+27            group_prev.next = prev
+28            group_prev = temp
+29
