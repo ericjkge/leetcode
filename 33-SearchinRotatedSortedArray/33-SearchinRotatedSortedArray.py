0f98f18951881b6510f22380e4f1cbe2f527d3cc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-18 09:44:31 +0800

    File updated at 1/18/2026, 9:44:30 AM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,23 +1,23 @@
-# Last updated: 11/25/2025, 7:20:48 PM
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-
-        while left + 1 < right:
-            mid = (left + right) // 2
-            if nums[left] < nums[mid]:
-                if nums[left] <= target <= nums[mid]:
-                    right = mid
-                else:
-                    left = mid
-            else:
-                if nums[mid] <= target <= nums[right]:
-                    left = mid
-                else:
-                    right = mid
-        
-        if nums[left] == target:
-            return left
-        elif nums[right] == target:
-            return right
-        return -1
+# Last updated: 1/18/2026, 9:44:30 AM
+1class Solution:
+2    def search(self, nums: List[int], target: int) -> int:
+3        left, right = 0, len(nums) - 1
+4
+5        while left + 1 < right:
+6            mid = (left + right) // 2
+7            if nums[mid] > nums[left]:
+8                if nums[left] <= target <= nums[mid]:
+9                    right = mid
+10                else:
+11                    left = mid
+12            else:
+13                if nums[mid] <= target <= nums[right]:
+14                    left = mid
+15                else:
+16                    right = mid
+17        
+18        if nums[left] == target:
+19            return left
+20        if nums[right] == target:
+21            return right
+22        return -1
