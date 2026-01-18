96c503bec0519e60104ec54ae28523fcdd6c8094
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-18 10:08:43 +0800

    File updated at 1/18/2026, 10:08:43 AM

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,36 +1,27 @@
-# Last updated: 11/9/2025, 10:09:13 PM
-class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = len(board)
-        cols = len(board[0])
-
-        seen = set()
-
-        # Validate rows
-        for row in range(rows):
-            seen = set()
-            for col in range(cols):
-                if board[row][col] in seen and board[row][col].isalnum():
-                    return False
-                seen.add(board[row][col])
-
-
-        # Validate cols
-        for col in range(cols):
-            seen = set()
-            for row in range(rows):
-                if board[row][col] in seen and board[row][col].isalnum():
-                    return False
-                seen.add(board[row][col])
-        
-        # Validate sub-boxes
-        for row in range(0, rows, 3):
-            for col in range(0, cols, 3):
-                seen = set()
-                for r in range(row, row + 3):
-                    for c in range(col, col + 3):
-                        if board[r][c] in seen and board[r][c].isalnum():
-                            return False
-                        seen.add(board[r][c])
-        
-        return True
+# Last updated: 1/18/2026, 10:08:43 AM
+1class Solution:
+2    def isValidSudoku(self, board: List[List[str]]) -> bool:
+3        for row in board:
+4            seen = set()
+5            for num in row:
+6                if num.isalnum() and num in seen:
+7                    return False
+8                seen.add(num)
+9
+10        for i in range(9):
+11            seen = set()
+12            for j in range(9):
+13                if board[j][i].isalnum() and board[j][i] in seen:
+14                    return False
+15                seen.add(board[j][i])
+16
+17        for i in range(0, 9, 3):
+18            for j in range(0, 9, 3):
+19                seen = set()
+20                for k in range(i, i + 3):
+21                    for l in range(j, j + 3):
+22                        if board[k][l].isalnum() and board[k][l] in seen:
+23                            return False
+24                        seen.add(board[k][l])
+25
+26        return True
