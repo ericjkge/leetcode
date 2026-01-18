627b1b35118ea0bbc1c6d1cfad12261f6d931f2c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-18 10:50:28 +0800

    File updated at 1/18/2026, 10:50:27 AM

--- 39-CombinationSum/39-CombinationSum.py
+++ 39-CombinationSum/39-CombinationSum.py
@@ -1,20 +1,21 @@
-# Last updated: 11/11/2025, 9:22:09 AM
-class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        self.ans = []
-
-        def backtrack(index, path):
-            if sum(path) > target:
-                return
-
-            if sum(path) == target:
-                self.ans.append(path[:])
-                return
-            
-            for i in range(index, len(candidates)):
-                path.append(candidates[i])
-                backtrack(i, path)
-                path.pop() 
-
-        backtrack(0, [])
-        return self.ans
+# Last updated: 1/18/2026, 10:50:27 AM
+1class Solution:
+2    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+3        n = len(candidates)
+4        combinations = []
+5
+6        def backtrack(index, path):
+7            if sum(path) == target:
+8                combinations.append(path[:])
+9                return
+10            
+11            if sum(path) > target:
+12                return
+13            
+14            for i in range(index, n):
+15                path.append(candidates[i])
+16                backtrack(i, path)
+17                path.pop()
+18        
+19        backtrack(0, [])
+20        return combinations
