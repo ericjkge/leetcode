000b922ed47763af3c422e6b8e6a0de931f20554
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-24 10:26:25 -0500

    File updated at 1/24/2026, 10:26:25 AM

--- 55-JumpGame/55-JumpGame.py
+++ 55-JumpGame/55-JumpGame.py
@@ -1,42 +1,16 @@
-# Last updated: 11/20/2025, 12:12:01 PM
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        farthest = curr = 0
-        
-        for i in range(len(nums)):
-            farthest = max(farthest, i + nums[i]) 
-            if i == curr:
-                curr = farthest
-
-        return curr >= len(nums) - 1
-
-        # Option 1: Greedy (backwards)
-        # goal = len(nums) - 1
-
-        # for i in range(len(nums) - 2, -1, -1):
-        #     if i + nums[i] >= goal:
-        #         goal = i
-        
-        # return goal == 0
-
-
-        # Option 2: Greedy (forwards)
-        # farthest = 0
-        # for i, num in enumerate(nums):
-        #     if i > farthest:
-        #         return False
-        #     farthest = max(farthest, i + num)
-        # return True
-
-
-        # Option 3: DP
-        # @cache
-        # def dp(i):
-        #     if i >= len(nums) - 1:
-        #         return True
-        #     for j in range(1, nums[i] + 1):
-        #         if dp(i + j):
-        #             return True
-        #     return False
-
-        # return dp(0)
+# Last updated: 1/24/2026, 10:26:25 AM
+1class Solution:
+2    def canJump(self, nums: List[int]) -> bool:
+3        farthest = 0
+4        curr_end = 0
+5
+6        for i in range(len(nums)):
+7            farthest = max(farthest, i + nums[i])
+8
+9            if i == curr_end:
+10                curr_end = farthest
+11
+12            if i > curr_end:
+13                return False
+14
+15        return True
