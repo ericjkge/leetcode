c5e537f5e2fd57ee29db0a5bb7b601292514f50c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-24 10:32:05 -0500

    File updated at 1/24/2026, 10:32:05 AM

--- 56-MergeIntervals/56-MergeIntervals.py
+++ 56-MergeIntervals/56-MergeIntervals.py
@@ -1,13 +1,16 @@
-# Last updated: 11/21/2025, 12:46:08 PM
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals.sort()
-        ans = []
-        
-        for start, end in intervals:
-            if ans and start <= ans[-1][1]:
-                ans[-1][1] = max(ans[-1][1], end)
-            else:
-                ans.append([start, end])
-        
-        return ans
+# Last updated: 1/24/2026, 10:32:05 AM
+1class Solution:
+2    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+3        results = []
+4        intervals.sort()
+5        prev = None
+6
+7        for start, end in intervals:
+8            if prev is not None and start <= prev:
+9                results[-1][1] = max(end, prev)
+10                prev = results[-1][1]
+11            else:
+12                results.append([start, end])    
+13                prev = end
+14
+15        return results
