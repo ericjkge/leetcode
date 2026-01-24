e1594d111e23cc0ee0ddb0942854cb0bc8a346dc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-24 10:18:54 -0500

    File updated at 1/24/2026, 10:18:54 AM

--- 54-SpiralMatrix/54-SpiralMatrix.py
+++ 54-SpiralMatrix/54-SpiralMatrix.py
@@ -1,28 +1,28 @@
-# Last updated: 11/20/2025, 12:04:19 PM
-class Solution:
-    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        top, bottom = 0, len(matrix) - 1
-        left, right = 0, len(matrix[0]) - 1
-        ans = []
-
-        while left <= right and top <= bottom:
-            for c in range(left, right + 1):
-                ans.append(matrix[top][c])
-            top += 1
-
-            for r in range(top, bottom + 1):
-                ans.append(matrix[r][right])
-            right -= 1
-
-            if left > right or top > bottom:
-                break
-
-            for c in range(right, left - 1, -1):
-                ans.append(matrix[bottom][c])
-            bottom -= 1
-
-            for r in range(bottom, top - 1, -1):
-                ans.append(matrix[r][left])
-            left += 1
-
-        return ans
+# Last updated: 1/24/2026, 10:18:54 AM
+1class Solution:
+2    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+3        elements = []
+4        left, right = 0, len(matrix[0]) - 1
+5        top, bottom = 0, len(matrix) - 1
+6
+7        while left <= right and top <= bottom:
+8            for c in range(left, right + 1):
+9                elements.append(matrix[top][c])
+10            top += 1
+11
+12            for r in range(top, bottom + 1):
+13                elements.append(matrix[r][right])
+14            right -= 1
+15
+16            if not (left <= right and top <= bottom):
+17                break
+18
+19            for c in range(right, left - 1, -1):
+20                elements.append(matrix[bottom][c])
+21            bottom -= 1
+22            
+23            for r in range(bottom, top - 1, -1):
+24                elements.append(matrix[r][left])
+25            left += 1
+26
+27        return elements
