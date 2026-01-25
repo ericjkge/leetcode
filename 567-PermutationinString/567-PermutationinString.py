2bd41e7a2a9bd6c58eac961002d0c68a889d09a8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-25 07:50:10 -0500

    File updated at 1/25/2026, 7:50:09 AM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,14 +1,21 @@
-# Last updated: 1/25/2026, 7:33:32 AM
+# Last updated: 1/25/2026, 7:50:09 AM
 1class Solution:
 2    def checkInclusion(self, s1: str, s2: str) -> bool:
-3        n = len(s2)
-4        letters = Counter(s1)
-5        start, end = 0, len(s1)
+3        n, k = len(s2), len(s1)
+4        c1 = Counter(s1)
+5        c2 = Counter(s2[:k])
 6
-7        while end < n + 1:
-8            if letters == Counter(s2[start:end]):
-9                return True
-10            start += 1
-11            end += 1
-12
-13        return False
+7        if c1 == c2:
+8            return True
+9
+10        for i in range(k, n):
+11            c2[s2[i]] += 1
+12            c2[s2[i - k]] -= 1
+13
+14            if not c2[s2[i - k]]:
+15                del c2[s2[i - k]]
+16
+17            if c1 == c2:
+18                return True
+19
+20        return False
