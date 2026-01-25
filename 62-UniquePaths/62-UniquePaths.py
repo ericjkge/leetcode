133140f02a99945944f3962a3bf11321b3ee69e8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-25 08:25:59 -0500

    File updated at 1/25/2026, 8:25:58 AM

--- 62-UniquePaths/62-UniquePaths.py
+++ 62-UniquePaths/62-UniquePaths.py
@@ -1,12 +1,11 @@
-# Last updated: 11/21/2025, 3:30:39 PM
-class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def dp(i, j):
-            if i > m - 1 or j > n - 1:
-                return 0
-            if i == m - 1 and j == n - 1:
-                return 1
-            return dp(i + 1, j) + dp(i, j + 1)
-        
-        return dp(0, 0)
+# Last updated: 1/25/2026, 8:25:58 AM
+1class Solution:
+2    def uniquePaths(self, m: int, n: int) -> int:
+3        @cache
+4        def dp(i, j):
+5            if i == m - 1 or j == n - 1:
+6                return 1
+7            
+8            return dp(i + 1, j) + dp(i, j + 1)
+9        
+10        return dp(0, 0)
