aa593cf5fa2b7cd81c277fe22c1fcbb6b406ac37
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-26 09:07:27 -0500

    File updated at 1/26/2026, 9:07:27 AM

--- 72-EditDistance/72-EditDistance.py
+++ 72-EditDistance/72-EditDistance.py
@@ -1,22 +1,22 @@
-# Last updated: 11/22/2025, 2:26:14 PM
-class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        a, b = len(word1), len(word2)
-
-        @cache
-        def dp(i, j):
-            if i == a and j == b:
-                return 0
-            
-            if i == a:
-                return len(word2[j:]) # same as b - j
-            
-            if j == b:
-                return len(word1[i:])
-
-            if word1[i] == word2[j]:
-                return dp(i + 1, j + 1)
-
-            return 1 + min(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1))
-        
-        return dp(0, 0)
+# Last updated: 1/26/2026, 9:07:27 AM
+1class Solution:
+2    def minDistance(self, word1: str, word2: str) -> int:
+3        m, n = len(word1), len(word2)
+4        
+5        @cache
+6        def dp(i, j):
+7            if i == m and j == n:
+8                return 0 # Done
+9            
+10            if i == m:
+11                return n - j # All inserts
+12            
+13            if j == n:
+14                return m - i # All deletes
+15            
+16            if word1[i] == word2[j]:
+17                return dp(i + 1, j + 1)
+18            
+19            return 1 + min(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1)) # Delete, insert, replace
+20
+21        return dp(0, 0)
