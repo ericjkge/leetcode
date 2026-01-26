3652397a1966073f723f7fbb994781c10ff3e28b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-26 13:09:37 -0500

    File updated at 1/26/2026, 1:09:37 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,18 +1,18 @@
-# Last updated: 11/23/2025, 11:53:59 PM
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-
-        def backtrack(path, index):
-            if index > len(nums):
-                return
-
-            ans.append(path[:])
-
-            for i in range(index, len(nums)):
-                path.append(nums[i])
-                backtrack(path, i + 1)
-                path.pop()
-            
-        backtrack([], 0)
-        return ans
+# Last updated: 1/26/2026, 1:09:37 PM
+1class Solution:
+2    def subsets(self, nums: List[int]) -> List[List[int]]:
+3        subsets = []
+4
+5        def backtrack(index, path):
+6            if index > len(nums):
+7                return
+8
+9            subsets.append(path[:])
+10
+11            for i in range(index, len(nums)):
+12                path.append(nums[i])
+13                backtrack(i + 1, path)
+14                path.pop()
+15
+16        backtrack(0, [])
+17        return subsets
