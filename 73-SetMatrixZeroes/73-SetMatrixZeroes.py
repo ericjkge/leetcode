5f0c045d02889f26755d4d1ddb08cbd1e0627aa0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-26 12:28:48 -0500

    File updated at 1/26/2026, 12:28:47 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.py
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -1,41 +1,37 @@
-# Last updated: 11/22/2025, 3:06:21 PM
-class Solution:
-    def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        # Remember first row/col zeroes
-        first_row_zeroes = any(matrix[0][c] == 0 for c in range(cols))
-        first_col_zeroes = any(matrix[r][0] == 0 for r in range(rows))
-
-        # Mark rows/cols
-        for r in range(rows):
-            for c in range(cols):
-                if matrix[r][c] == 0:
-                    matrix[r][0] = 0
-                    matrix[0][c] = 0
-
-        # Zero rows
-        for r in range(1, rows):
-            if matrix[r][0] == 0:
-                for c in range(1, cols):
-                    matrix[r][c] = 0
-        
-        # Zero cols
-        for c in range(1, cols):
-            if matrix[0][c] == 0:
-                for r in range(1, rows):
-                    matrix[r][c] = 0
-        
-        # Zero first row
-        if first_row_zeroes:
-            for c in range(cols):
-                matrix[0][c] = 0
-        
-        # Zero first col
-        if first_col_zeroes:
-            for r in range(rows):
-                matrix[r][0] = 0
+# Last updated: 1/26/2026, 12:28:47 PM
+1class Solution:
+2    def setZeroes(self, matrix: List[List[int]]) -> None:
+3        """
+4        Do not return anything, modify matrix in-place instead.
+5        """
+6        rows, cols = len(matrix), len(matrix[0])
+7
+8        zero_first_row = any(matrix[0][c] == 0 for c in range(cols))
+9        zero_first_col = any(matrix[r][0] == 0 for r in range(rows))
+10
+11        for r in range(rows):
+12            for c in range(cols):
+13                if matrix[r][c] == 0:
+14                    matrix[r][0] = "*"
+15                    matrix[0][c] = "*"
+16        
+17        for r in range(1, rows):
+18            if matrix[r][0] == "*":
+19                matrix[r][0] = 0
+20                for c in range(1, cols):
+21                    matrix[r][c] = 0
+22
+23        for c in range(1, cols):
+24            if matrix[0][c] == "*":
+25                matrix[0][c] = 0
+26                for r in range(1, rows):
+27                    matrix[r][c] = 0
+28        
+29        if zero_first_row:
+30            for c in range(cols):
+31                matrix[0][c] = 0
+32        
+33        if zero_first_col:
+34            for r in range(rows):
+35                matrix[r][0] = 0
+36
