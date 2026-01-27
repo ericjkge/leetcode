85834f2ec74a2ec9364a7c2c31980307eb227aa6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-27 11:35:58 -0500

    File updated at 1/27/2026, 11:35:58 AM

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.py
@@ -1,4 +1,4 @@
-# Last updated: 11/30/2025, 10:15:51 AM
+# Last updated: 1/27/2026, 11:35:58 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -7,18 +7,18 @@
 6#         self.right = right
 7class Solution:
 8    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-9        arr = []
+9        tree = []
 10
 11        def dfs(node):
 12            if not node:
 13                return
-14            
-15            dfs(node.left)
-16            arr.append(node.val)
+14
+15            dfs(node.left)            
+16            tree.append(node.val)
 17            dfs(node.right)
 18        
 19        dfs(root)
-20        for i in range(len(arr)):
-21            if i and arr[i] <= arr[i - 1]:
+20        for i in range(len(tree)):
+21            if i > 0 and tree[i] <= tree[i - 1]:
 22                return False
 23        return True
