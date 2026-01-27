8ccde67035769fc465440d3f05bf9084656145ca
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-27 09:23:37 -0500

    File updated at 1/27/2026, 9:23:37 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,27 +1,27 @@
-# Last updated: 11/24/2025, 12:27:04 AM
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        rows, cols = len(board), len(board[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        def backtrack(index, r, c):
-            if index == len(word) - 1:
-                return True
-
-            original = board[r][c]
-            board[r][c] = "#"
-
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] != "#" and board[nr][nc] == word[index + 1]:
-                    if backtrack(index + 1, nr, nc):
-                        return True
-            
-            board[r][c] = original
-        
-        for r in range(rows):
-            for c in range(cols):
-                if board[r][c] == word[0]:
-                    if backtrack(0, r, c):
-                        return True
-        return False
+# Last updated: 1/27/2026, 9:23:37 AM
+1class Solution:
+2    def exist(self, board: List[List[str]], word: str) -> bool:
+3        rows, cols = len(board), len(board[0])
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5
+6        def backtrack(r, c, index):
+7            if index == len(word):
+8                return True
+9    
+10            original = board[r][c]
+11            board[r][c] = "*"
+12
+13            for dr, dc in directions:
+14                nr, nc = r + dr, c + dc
+15                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == word[index]:
+16                    if backtrack(nr, nc, index + 1):
+17                        return True
+18
+19            board[r][c] = original
+20            return False
+21
+22        for r in range(rows):
+23            for c in range(cols):
+24                if board[r][c] == word[0] and backtrack(r, c, 1):
+25                    return True
+26        return False
