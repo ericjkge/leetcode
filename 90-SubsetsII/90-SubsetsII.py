0021b7d47ce45923c097e13ad56bacca87fc120e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-27 11:33:52 -0500

    File updated at 1/27/2026, 11:33:52 AM

--- 90-SubsetsII/90-SubsetsII.py
+++ 90-SubsetsII/90-SubsetsII.py
@@ -1,22 +1,23 @@
-# Last updated: 11/24/2025, 12:34:33 AM
-class Solution:
-    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        ans = []
-
-        def backtrack(path, index):
-            if index > len(nums):
-                return
-
-            ans.append(path[:])
-            
-            for i in range(index, len(nums)):
-                if i > index and nums[i] == nums[i - 1]:
-                    continue
-                else:
-                    path.append(nums[i])
-                    backtrack(path, i + 1)
-                    path.pop()
-            
-        backtrack([], 0)
-        return ans
+# Last updated: 1/27/2026, 11:33:52 AM
+1class Solution:
+2    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+3        nums.sort()
+4        subsets = []
+5
+6        def backtrack(index, path):
+7            if index == len(nums) + 1:
+8                return
+9
+10            subsets.append(path[:])
+11
+12            for i in range(index, len(nums)):
+13                if i > index and nums[i] == nums[i - 1]:
+14                    continue
+15                path.append(nums[i])
+16                backtrack(i + 1, path)
+17                path.pop()
+18            
+19        backtrack(0, [])
+20        return subsets
+21
+22
