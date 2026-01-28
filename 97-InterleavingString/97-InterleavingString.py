b6e23f5951a1f597f861c6893f7a021a94493bfc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-28 09:16:26 -0500

    File updated at 1/28/2026, 9:16:26 AM

--- 97-InterleavingString/97-InterleavingString.py
+++ 97-InterleavingString/97-InterleavingString.py
@@ -1,22 +1,20 @@
-# Last updated: 11/30/2025, 10:06:53 AM
+# Last updated: 1/28/2026, 9:16:26 AM
 1class Solution:
 2    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
 3        if len(s1) + len(s2) != len(s3):
 4            return False
-5
-6        a, b, c = len(s1), len(s2), len(s3)
-7
-8        @cache
-9        def dp(i, j):
-10            if i == a and j == b:
-11                return True
-12
-13            if i < a and s1[i] == s3[i + j] and dp(i + 1, j):
-14                return True
-15            
-16            if j < b and s2[j] == s3[i + j] and dp(i, j + 1):
-17                return True
-18            
-19            return False
-20        
-21        return dp(0, 0)
+5            
+6        @cache
+7        def dp(i, j):
+8            if i + j == len(s3):
+9                return True
+10
+11            if i < len(s1) and s1[i] == s3[i + j] and dp(i + 1, j):
+12                return True
+13                
+14            if j < len(s2) and s2[j] == s3[i + j] and dp(i, j + 1):
+15                return True
+16
+17            return False
+18
+19        return dp(0, 0)
