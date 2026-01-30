5ff97a6ec0d87af4fbb28b8da2a60488e34f0af5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-30 10:35:26 -0500

    File updated at 1/30/2026, 10:35:25 AM

--- 134-GasStation/134-GasStation.py
+++ 134-GasStation/134-GasStation.py
@@ -1,18 +1,17 @@
-# Last updated: 12/17/2025, 9:58:47 PM
+# Last updated: 1/30/2026, 10:35:25 AM
 1class Solution:
 2    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-3        n = len(gas)
-4        start = 0
+3        start = 0
+4        total = 0
 5        tank = 0
-6        total = 0
-7
-8        for i in range(n):
-9            change = gas[i] - cost[i]
-10            tank += change
-11            total += change
-12
-13            if tank < 0:
+6
+7        for i in range(len(gas)):
+8            change = gas[i] - cost[i]
+9
+10            total += change
+11            tank += change
+12            if tank < 0:
+13                tank = 0
 14                start = i + 1
-15                tank = 0
-16        
-17        return start if total >= 0 else -1
+15        
+16        return start if total >= 0 else -1
