445aded970bd16e2d440448e29b16071f54eb197
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-31 13:18:10 -0500

    File updated at 1/31/2026, 1:18:10 PM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.py
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -1,17 +1,17 @@
-# Last updated: 12/17/2025, 10:08:29 PM
+# Last updated: 1/31/2026, 1:18:10 PM
 1class Solution:
 2    def countSubstrings(self, s: str) -> int:
 3        n = len(s)
 4        self.ans = 0
 5
-6        def expand(l, r):
-7            while l >= 0 and r < n and s[l] == s[r]:
+6        def expand(i, j):
+7            while i >= 0 and j < n and s[i] == s[j]:
 8                self.ans += 1
-9                l -= 1
-10                r += 1
-11        
-12        for i in range(n):
-13            expand(i, i)
-14            expand(i, i + 1)
-15        
+9                i -= 1
+10                j += 1
+11                
+12        for k in range(n):
+13            expand(k, k)
+14            expand(k, k + 1)
+15
 16        return self.ans
