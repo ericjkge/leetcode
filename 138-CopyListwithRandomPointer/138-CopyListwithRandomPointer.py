9772a44cb6cd6b2f9977a77e1255b6fa93397ab1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-01-31 13:30:02 -0500

    File updated at 1/31/2026, 1:30:01 PM

--- 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.py
+++ 138-CopyListwithRandomPointer/138-CopyListwithRandomPointer.py
@@ -1,4 +1,4 @@
-# Last updated: 12/21/2025, 9:09:36 AM
+# Last updated: 1/31/2026, 1:30:01 PM
 1"""
 2# Definition for a Node.
 3class Node:
@@ -10,26 +10,20 @@
 9
 10class Solution:
 11    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-12        if not head:
-13            return None
-14            
-15        hashmap = {}
-16        p1 = head
-17
-18        while p1:
-19            clone = Node(p1.val)
-20            hashmap[p1] = clone
-21            p1 = p1.next
-22        
-23        p2 = head
-24        
-25        while p2:
-26            if p2.next:
-27                hashmap[p2].next = hashmap[p2.next]
-28            if p2.random:
-29                hashmap[p2].random = hashmap[p2.random]
-30            p2 = p2.next
-31        
-32        return hashmap[head]
-33
-34
+12        mapping = {None:None}
+13        curr = head
+14
+15        while curr:
+16            clone = Node(curr.val)
+17            mapping[curr] = clone
+18            curr = curr.next
+19
+20        curr = head
+21        while curr:
+22            clone = mapping[curr]
+23            clone.next = mapping[curr.next]
+24            clone.random = mapping[curr.random]
+25            curr = curr.next
+26        
+27        return mapping[head]
+28
