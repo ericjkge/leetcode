a6eba579929a161d6d0929c076cc66bbb52dc443
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-03 09:30:41 -0500

    File updated at 2/3/2026, 9:30:40 AM

--- 678-ValidParenthesisString/678-ValidParenthesisString.py
+++ 678-ValidParenthesisString/678-ValidParenthesisString.py
@@ -1,21 +1,22 @@
-# Last updated: 8/6/2025, 10:37:40 PM
-class Solution:
-    def checkValidString(self, s: str) -> bool:
-        lo, hi = 0, 0 # min, max open
-        for char in s:
-            if char == "(":
-                lo += 1
-                hi += 1
-            elif char == ")":
-                lo -= 1
-                hi -= 1
-            else:
-                lo -= 1
-                hi += 1
-        
-            if hi < 0:
-                return False
-            
-            lo = max(lo, 0)
-        
-        return lo == 0
+# Last updated: 2/3/2026, 9:30:40 AM
+1class Solution:
+2    def checkValidString(self, s: str) -> bool:
+3        low = high = 0 # low is min possible open parentheses
+4
+5        for c in s:
+6            if c == "(":
+7                low += 1
+8                high += 1
+9            elif c == ")":
+10                low -= 1
+11                high -= 1
+12            else:
+13                low -= 1
+14                high += 1
+15
+16            if high < 0:
+17                return False
+18            
+19            low = max(low, 0)
+20
+21        return low == 0
