68d2d1f6a024417867811badbb36f110e274f59f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-03 10:15:32 -0500

    File updated at 2/3/2026, 10:15:32 AM

--- 695-MaxAreaofIsland/695-MaxAreaofIsland.py
+++ 695-MaxAreaofIsland/695-MaxAreaofIsland.py
@@ -1,24 +1,25 @@
-# Last updated: 12/25/2025, 8:30:02 PM
+# Last updated: 2/3/2026, 10:15:32 AM
 1class Solution:
 2    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 3        rows, cols = len(grid), len(grid[0])
-4        ans = 0
-5        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5        self.max = 0
 6
 7        def dfs(r, c):
-8            area = 1
+8            grid[r][c] = -1
 9            for dr, dc in directions:
 10                nr, nc = r + dr, c + dc
 11                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
-12                    grid[nr][nc] = -1
-13                    area += dfs(nr, nc)
-14            return area
-15        
+12                    self.area += 1
+13                    dfs(nr, nc)
+14
+15
 16        for r in range(rows):
 17            for c in range(cols):
-18                if grid[r][c] == 1:
-19                    grid[r][c] = -1
-20                    area = dfs(r, c)
-21                    ans = max(ans, area)
-22        
-23        return ans
+18                self.area = 0
+19                if grid[r][c] == 1: 
+20                    self.area += 1
+21                    dfs(r, c)
+22                self.max = max(self.max, self.area)
+23        
+24        return self.max
