660e6324b5bd27b9ae02a68bf7c213de70aa16a9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-03 09:48:49 -0500

    File updated at 2/3/2026, 9:48:49 AM

--- 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
+++ 167-TwoSumII-InputArrayIsSorted/167-TwoSumII-InputArrayIsSorted.py
@@ -1,12 +1,14 @@
-# Last updated: 12/24/2025, 10:06:38 AM
+# Last updated: 2/3/2026, 9:48:49 AM
 1class Solution:
 2    def twoSum(self, numbers: List[int], target: int) -> List[int]:
 3        left, right = 0, len(numbers) - 1
 4
 5        while left < right:
-6            if numbers[left] + numbers[right] > target:
-7                right -= 1
-8            elif numbers[left] + numbers[right] < target:
-9                left += 1
-10            else:
-11                return [left + 1, right + 1]
+6            total = numbers[left] + numbers[right]
+7            if total > target:
+8                right -= 1
+9            elif total < target:
+10                left += 1
+11            else:
+12                return [left + 1, right + 1]
+13    
