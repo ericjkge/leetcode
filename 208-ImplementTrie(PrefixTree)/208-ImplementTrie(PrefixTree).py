f433a326d502390f4b922207634301f22cd09a45
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-06 10:06:19 -0500

    File updated at 2/6/2026, 10:06:19 AM

--- 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).py
+++ 208-ImplementTrie(PrefixTree)/208-ImplementTrie(PrefixTree).py
@@ -1,8 +1,8 @@
-# Last updated: 12/27/2025, 10:52:01 PM
+# Last updated: 2/6/2026, 10:06:19 AM
 1class TrieNode:
 2    def __init__(self):
-3        self.is_end = False
-4        self.children = {}
+3        self.children = {}
+4        self.is_end = False
 5
 6class Trie:
 7
@@ -10,32 +10,31 @@
 9        self.root = TrieNode()
 10
 11    def insert(self, word: str) -> None:
-12        curr = self.root
+12        node = self.root
 13        for c in word:
-14            if c not in curr.children:
-15                curr.children[c] = TrieNode()
-16            curr = curr.children[c]
-17        curr.is_end = True
+14            if c not in node.children:
+15                node.children[c] = TrieNode()
+16            node = node.children[c]
+17        node.is_end = True
 18
 19    def search(self, word: str) -> bool:
-20        curr = self.root
+20        node = self.root
 21        for c in word:
-22            if c not in curr.children:
+22            if c not in node.children:
 23                return False
-24            curr = curr.children[c]
-25        return curr.is_end
+24            node = node.children[c]
+25        return node.is_end
 26
 27    def startsWith(self, prefix: str) -> bool:
-28        curr = self.root
+28        node = self.root
 29        for c in prefix:
-30            if c not in curr.children:
+30            if c not in node.children:
 31                return False
-32            curr = curr.children[c]
+32            node = node.children[c]
 33        return True
 34
-35
-36# Your Trie object will be instantiated and called as such:
-37# obj = Trie()
-38# obj.insert(word)
-39# param_2 = obj.search(word)
-40# param_3 = obj.startsWith(prefix)
+35# Your Trie object will be instantiated and called as such:
+36# obj = Trie()
+37# obj.insert(word)
+38# param_2 = obj.search(word)
+39# param_3 = obj.startsWith(prefix)
