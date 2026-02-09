d6abe3b343bf964eeab15c827f6f675b378d65a4
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-08 19:29:34 -0500

    File updated at 2/8/2026, 7:29:34 PM

--- 743-NetworkDelayTime/743-NetworkDelayTime.py
+++ 743-NetworkDelayTime/743-NetworkDelayTime.py
@@ -1,4 +1,4 @@
-# Last updated: 12/29/2025, 8:34:24 PM
+# Last updated: 2/8/2026, 7:29:34 PM
 1class Solution:
 2    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
 3        graph = defaultdict(list)
@@ -6,21 +6,22 @@
 5        for u, v, w in times:
 6            graph[u].append((v, w))
 7        
-8        distances = [float("inf")] * (n + 1) # Keep 0th index empty and use rest as 1-indexed
-9        distances[0] = -1
-10        distances[k] = 0
-11        heap = [(0, k)]
+8        distances = [float("inf")] * n
+9        distances[k - 1] = 0
+10        heap = [(0, k)]
+11        time = 0
 12
 13        while heap:
 14            dist, node = heapq.heappop(heap)
-15            if dist > distances[node]:
+15            if dist > distances[node - 1]:
 16                continue
 17            
 18            for neighbor, weight in graph[node]:
-19                if dist + weight < distances[neighbor]:
-20                    distances[neighbor] = dist + weight
-21                    heapq.heappush(heap, (dist + weight, neighbor))
-22        
-23        return max(distances) if max(distances) != float("inf") else -1
-24
+19                if distances[node - 1] + weight < distances[neighbor - 1]:
+20                    distances[neighbor - 1] = distances[node - 1] + weight
+21                    heapq.heappush(heap, (distances[neighbor - 1], neighbor))
+22
+23        ans = max(distances)        
+24        return ans if ans != float("inf") else -1
 25
+26
