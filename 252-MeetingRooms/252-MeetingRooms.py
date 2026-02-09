2ab25606c62a4f9c01f4d8e73181a567e6c89573
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-09 16:50:45 -0500

    File updated at 2/9/2026, 4:50:45 PM

--- 252-MeetingRooms/252-MeetingRooms.py
+++ 252-MeetingRooms/252-MeetingRooms.py
@@ -1,12 +1,12 @@
-# Last updated: 9/6/2025, 2:18:07 PM
-class Solution:
-    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort()
-        prev = -float("inf")
-
-        for start, end in intervals:
-            if start < prev:
-                return False
-            prev = end
-        
-        return True
+# Last updated: 2/9/2026, 4:50:45 PM
+1class Solution:
+2    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+3        intervals.sort()
+4        prev = None
+5
+6        for start, end in intervals:
+7            if prev and start < prev:
+8                return False
+9            prev = end
+10        
+11        return True
