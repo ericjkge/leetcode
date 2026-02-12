155b0292479334420850f566c5ebd21a20f58f24
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-12 11:51:20 -0500

    File updated at 2/12/2026, 11:51:19 AM

--- 704-BinarySearch/704-BinarySearch.py
+++ 704-BinarySearch/704-BinarySearch.py
@@ -1,17 +1,17 @@
-# Last updated: 12/31/2025, 11:02:49 AM
+# Last updated: 2/12/2026, 11:51:19 AM
 1class Solution:
 2    def search(self, nums: List[int], target: int) -> int:
 3        left, right = 0, len(nums) - 1
 4
 5        while left + 1 < right:
 6            mid = (left + right) // 2
-7            if nums[mid] > target:
-8                right = mid
+7            if nums[mid] < target:
+8                left = mid
 9            else:
-10                left = mid
+10                right = mid
 11        
 12        if nums[left] == target:
 13            return left
-14        if nums[right] == target:
+14        elif nums[right] == target:
 15            return right
 16        return -1
