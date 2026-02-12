960fd74389fb9ce00b85722c538ff34cc19de5be
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-12 13:42:13 -0500

    File updated at 2/12/2026, 1:42:13 PM

--- 778-SwiminRisingWater/778-SwiminRisingWater.py
+++ 778-SwiminRisingWater/778-SwiminRisingWater.py
@@ -1,31 +1,36 @@
-# Last updated: 1/1/2026, 9:54:37 AM
+# Last updated: 2/12/2026, 1:42:13 PM
 1class Solution:
 2    def swimInWater(self, grid: List[List[int]]) -> int:
-3        n = len(grid)
-4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+3        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+4        n = len(grid)
 5
 6        def dfs(r, c, time):
-7            if r == n - 1 and c == n - 1:
-8                return True
-9            seen.add((r, c))
-10
-11            for dr, dc in directions:
-12                nr, nc = r + dr, c + dc
-13                if 0 <= nr < n and 0 <= nc < n and grid[nr][nc] <= time and (nr, nc) not in seen:
-14                    if dfs(nr, nc, time):
-15                        return True
-16            return False
-17
-18        left, right = 0, n * n - 1
-19
-20        while left + 1 < right:
-21            seen = set()
-22            mid = (left + right) // 2
-23            if grid[0][0] <= mid and dfs(0, 0, mid):
-24                right = mid
-25            else:
-26                left = mid
-27        
-28        if grid[0][0] <= left and dfs(0, 0, left):
-29            return left
-30        return right
+7            if grid[r][c] > time:
+8                return False
+9
+10            if (r, c) == (n - 1, n - 1):
+11                return True
+12            
+13            seen.add((r, c))
+14
+15            for dr, dc in directions:
+16                nr, nc = r + dr, c + dc
+17                if 0 <= nr < n and 0 <= nc < n and (nr, nc) not in seen and grid[nr][nc] <= time:
+18                    if dfs(nr, nc, time):
+19                        return True
+20            
+21            return False
+22        
+23        left, right = 0, max(max(row) for row in grid)
+24        while left + 1 < right:
+25            seen = set()
+26            mid = (left + right) // 2
+27            if dfs(0, 0, mid):
+28                right = mid
+29            else:
+30                left = mid
+31        
+32        seen = set()
+33        if dfs(0, 0, left):
+34            return left
+35        return right
