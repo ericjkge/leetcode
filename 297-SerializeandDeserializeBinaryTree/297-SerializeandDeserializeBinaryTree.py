1a402fda8ecd1c6b451051163ceafb3fa99076be
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-14 18:22:12 -0500

    File updated at 2/14/2026, 6:22:12 PM

--- 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.py
+++ 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 1/2/2026, 9:24:23 PM
+# Last updated: 2/14/2026, 6:22:12 PM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, x):
@@ -14,46 +14,45 @@
 13        :type root: TreeNode
 14        :rtype: str
 15        """
-16        res = []
-17        queue = deque([root])
-18        while queue:
-19            node = queue.popleft()
-20            if not node:
-21                res.append("null")
-22                continue
-23            
-24            res.append(str(node.val))
-25            queue.append(node.left)
-26            queue.append(node.right)
-27
-28        return ",".join(res)
-29
-30    def deserialize(self, data):
-31        """Decodes your encoded data to tree.
-32        
-33        :type data: str
-34        :rtype: TreeNode
-35        """
-36        if not data:
-37            return []
-38
-39        data = data.split(",")
-40        nodes = [TreeNode(int(d)) if d != "null" else None for d in data]
-41        slow, fast = 0, 1
+16        serialization = []
+17
+18        queue = deque([root])
+19        while queue:
+20            node = queue.popleft()
+21
+22            if not node:
+23                serialization.append("null")
+24                continue
+25
+26            serialization.append(str(node.val))
+27            queue.append(node.left)
+28            queue.append(node.right)
+29        
+30        return ",".join(serialization)
+31
+32    def deserialize(self, data):
+33        """Decodes your encoded data to tree.
+34        
+35        :type data: str
+36        :rtype: TreeNode
+37        """
+38        vals = data.split(",")
+39        nodes = [TreeNode(int(v)) if v != "null" else None for v in vals]
+40        slow = 0
+41        fast = 1
 42
 43        while fast < len(nodes):
 44            nodes[slow].left = nodes[fast]
 45            fast += 1
 46            nodes[slow].right = nodes[fast]
-47            fast += 1
-48            slow += 1
-49            while slow < len(nodes) and nodes[slow] == None:
-50                slow += 1
-51        
+47            slow += 1
+48            while slow < fast and nodes[slow] is None:
+49                slow += 1
+50            fast += 1
+51
 52        return nodes[0]
 53
-54
-55# Your Codec object will be instantiated and called as such:
-56# ser = Codec()
-57# deser = Codec()
-58# ans = deser.deserialize(ser.serialize(root))
+54# Your Codec object will be instantiated and called as such:
+55# ser = Codec()
+56# deser = Codec()
+57# ans = deser.deserialize(ser.serialize(root))
