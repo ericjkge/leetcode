30b2cff7213a404f2a482407ff75beb9118fb9e7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-15 12:34:27 -0500

    File updated at 2/15/2026, 12:34:27 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,19 +1,17 @@
-# Last updated: 2/15/2026, 9:33:16 AM
+# Last updated: 2/15/2026, 12:34:27 PM
 1class Solution:
 2    def coinChange(self, coins: List[int], amount: int) -> int:
 3        @cache
-4        def dp(i, j):
-5            if j == amount:
-6                return 0
+4        def dp(i):
+5            if i > amount:
+6                return float("inf")
 7            
-8            if j > amount or i == len(coins):
-9                return float("inf")
-10
+8            if i == amount:
+9                return 0
+10        
 11            fewest = float("inf")
-12            for k in range(i, len(coins)):
-13                fewest = min(fewest, 1 + dp(k, j + coins[k]))
-14            
-15            return fewest
-16
-17        ans = dp(0, 0)
-18        return ans if ans != float("inf") else -1
+12            for coin in coins:
+13                fewest = min(fewest, 1 + dp(i + coin))
+14            return fewest
+15
+16        return dp(0) if dp(0) != float("inf") else -1
