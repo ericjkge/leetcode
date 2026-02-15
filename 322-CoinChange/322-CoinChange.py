a6137f32f641f8bda4ef89b4614880b846d841ee
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-15 09:33:16 -0500

    File updated at 2/15/2026, 9:33:16 AM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,38 +1,19 @@
-# Last updated: 9/5/2025, 9:19:34 AM
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def dp(i): # min way to get amount i
-            if i == 0:
-                return 0
-            
-            if i < 0:
-                return float("inf")
-
-            ways = float("inf")
-            for c in coins:
-                ways = min(ways, 1 + dp(i - c))
-            return ways
-
-        ans = dp(amount)
-        return ans if ans != float("inf") else -1
-
-
-        # Alternative: backtracking TLE but logically correct
-        
-        # self.ans = float("inf")
-        # n = len(coins)
-
-        # def backtrack(total, length):
-        #     if total > amount:
-        #         return
-
-        #     if total == amount:
-        #         self.ans = min(self.ans, length)
-        #         return
-            
-        #     for c in coins:
-        #         backtrack(total + c, length + 1)
-        
-        # backtrack(0, 0)
-        # return self.ans if self.ans != float("inf") else -1
+# Last updated: 2/15/2026, 9:33:16 AM
+1class Solution:
+2    def coinChange(self, coins: List[int], amount: int) -> int:
+3        @cache
+4        def dp(i, j):
+5            if j == amount:
+6                return 0
+7            
+8            if j > amount or i == len(coins):
+9                return float("inf")
+10
+11            fewest = float("inf")
+12            for k in range(i, len(coins)):
+13                fewest = min(fewest, 1 + dp(k, j + coins[k]))
+14            
+15            return fewest
+16
+17        ans = dp(0, 0)
+18        return ans if ans != float("inf") else -1
