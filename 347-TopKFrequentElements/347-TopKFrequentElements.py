d282d787f0e2bfe71a237f9d7f9781555f333e5c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-16 11:28:27 -0500

    File updated at 2/16/2026, 11:28:27 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,14 +1,13 @@
-# Last updated: 1/6/2026, 10:12:37 AM
+# Last updated: 2/16/2026, 11:28:27 AM
 1class Solution:
 2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-3        freqs = Counter(nums)
-4        heap = [(-freq, num) for num, freq in freqs.items()]
-5        heapq.heapify(heap)
-6        ans = []
-7
+3        counter = Counter(nums)
+4        freqs = [(-val, key) for key, val in counter.items()]
+5        heapq.heapify(freqs)
+6
+7        ans = []
 8        for _ in range(k):
-9            freq, num = heapq.heappop(heap)
-10            ans.append(num)
-11        
+9            _, key = heapq.heappop(freqs)
+10            ans.append(key)
+11
 12        return ans
-13
