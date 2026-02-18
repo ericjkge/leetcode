c8845693297dc7bf91991ad9386d35cd8a061af5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-18 14:34:04 -0500

    File updated at 2/18/2026, 2:34:04 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,23 +1,17 @@
-# Last updated: 1/8/2026, 11:02:03 AM
+# Last updated: 2/18/2026, 2:34:04 PM
 1class Solution:
 2    def characterReplacement(self, s: str, k: int) -> int:
-3        left = 0
-4        freqs = {}
-5        max_f = 0
-6        longest = 0
-7
-8        for right in range(len(s)):
-9            # Update frequency and current max frequency
-10            freqs[s[right]] = freqs.get(s[right], 0) + 1
-11            max_f = max(max_f, freqs[s[right]])
-12
-13            # Window is invalid if: (window size - max_f) > k
-14            # We only need to shift the left pointer once (if) 
-15            # because we only care about expanding the maximum window found so far.
-16            if (right - left + 1) - max_f > k:
-17                freqs[s[left]] -= 1
-18                left += 1
-19            
-20            longest = right - left + 1
-21            
-22        return longest
+3        left = right = 0
+4        window = defaultdict(int)
+5        longest = 0
+6
+7        while right < len(s):
+8            window[s[right]] += 1
+9            while left < right and sum(window.values()) - max(window.values()) > k:
+10                window[s[left]] -= 1
+11                left += 1
+12            longest = max(longest, right - left + 1)
+13            right += 1
+14        
+15        return longest
+16
