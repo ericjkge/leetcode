5a9aa405989ed12c26737363fcf0f8a11407a402
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-19 09:47:35 -0500

    File updated at 2/19/2026, 9:47:35 AM

--- 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
@@ -1,31 +1,21 @@
-# Last updated: 1/9/2026, 11:07:58 AM
+# Last updated: 2/19/2026, 9:47:35 AM
 1class TimeMap:
 2
 3    def __init__(self):
-4        self.dict = defaultdict(list)
+4        self.storage = defaultdict(list) # k : [(v1, t1), (v2, t2), ...]
 5
 6    def set(self, key: str, value: str, timestamp: int) -> None:
-7        self.dict[key].append((value, timestamp))
+7        self.storage[key].append((value, timestamp))
 8
-9    def get(self, key: str, timestamp: int) -> str:    
-10        if key not in self.dict or self.dict[key][0][1] > timestamp:
-11            return ""
-12        
-13        options = self.dict[key]
-14        left, right = 0, len(options) - 1
-15        while left + 1 < right:
-16            mid = (left + right) // 2
-17            if options[mid][1] < timestamp:
-18                left = mid
-19            else:
-20                right = mid
-21
-22        if options[right][1] <= timestamp:
-23            return options[right][0]
-24        return options[left][0]
-25
-26
-27# Your TimeMap object will be instantiated and called as such:
-28# obj = TimeMap()
-29# obj.set(key,value,timestamp)
-30# param_2 = obj.get(key,timestamp)
+9    def get(self, key: str, timestamp: int) -> str:
+10        if key in self.storage:
+11            for v, t in reversed(self.storage[key]):
+12                if t <= timestamp:
+13                    return v
+14        return ""
+15
+16
+17# Your TimeMap object will be instantiated and called as such:
+18# obj = TimeMap()
+19# obj.set(key,value,timestamp)
+20# param_2 = obj.get(key,timestamp)
