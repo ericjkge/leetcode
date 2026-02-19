39d4a9a603f531e305a3095669d7cf080ebb595b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-19 09:53:35 -0500

    File updated at 2/19/2026, 9:53:35 AM

--- 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
@@ -1,4 +1,4 @@
-# Last updated: 2/19/2026, 9:47:35 AM
+# Last updated: 2/19/2026, 9:53:35 AM
 1class TimeMap:
 2
 3    def __init__(self):
@@ -8,14 +8,23 @@
 7        self.storage[key].append((value, timestamp))
 8
 9    def get(self, key: str, timestamp: int) -> str:
-10        if key in self.storage:
-11            for v, t in reversed(self.storage[key]):
-12                if t <= timestamp:
-13                    return v
-14        return ""
-15
-16
-17# Your TimeMap object will be instantiated and called as such:
-18# obj = TimeMap()
-19# obj.set(key,value,timestamp)
-20# param_2 = obj.get(key,timestamp)
+10        if key not in self.storage or self.storage[key][0][1] > timestamp:
+11            return ""
+12
+13        left, right = 0, len(self.storage[key]) - 1
+14        while left + 1 < right:
+15            mid = (left + right) // 2
+16            if self.storage[key][mid][1] > timestamp:
+17                right = mid
+18            else:
+19                left = mid
+20        
+21        if self.storage[key][right][1] <= timestamp:
+22            return self.storage[key][right][0]
+23        return self.storage[key][left][0]
+24
+25
+26# Your TimeMap object will be instantiated and called as such:
+27# obj = TimeMap()
+28# obj.set(key,value,timestamp)
+29# param_2 = obj.get(key,timestamp)
