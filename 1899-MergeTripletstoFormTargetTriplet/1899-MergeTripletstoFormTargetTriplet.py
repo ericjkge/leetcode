8e49a6016a6b9938ad0944fb8c583ed12892a041
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-19 09:21:48 -0500

    File updated at 2/19/2026, 9:21:48 AM

--- 1899-MergeTripletstoFormTargetTriplet/1899-MergeTripletstoFormTargetTriplet.py
+++ 1899-MergeTripletstoFormTargetTriplet/1899-MergeTripletstoFormTargetTriplet.py
@@ -1,14 +1,16 @@
-# Last updated: 2/19/2026, 9:19:40 AM
+# Last updated: 2/19/2026, 9:21:48 AM
 1class Solution:
 2    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
 3        x, y, z = target
 4        flags = [False, False, False]
 5
 6        for a, b, c in triplets:
-7            if a <= x and b <= y and c <= z:
-8                flags[0] = flags[0] or (a == x)
-9                flags[1] = flags[1] or (b == y)
-10                flags[2] = flags[2] or (c == z)
-11        
-12        return True if all(flags) else False
-13
+7            if a == x and b <= y and c <= z:
+8                flags[0] = True
+9            if b == y and a <= x and c <= z:
+10                flags[1] = True
+11            if c == z and a <= x and b <= y:
+12                flags[2] = True
+13        
+14        return all(flags)
+15
