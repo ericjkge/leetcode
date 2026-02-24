939b2bb321fe370084e9f16136d4709c12283590
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-24 08:43:09 -0500

    File updated at 2/24/2026, 8:43:08 AM

--- 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.py
+++ 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.py
@@ -1,16 +1,20 @@
-# Last updated: 8/20/2025, 10:59:15 PM
-class Solution:
-    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
-        m, n = len(matrix), len(matrix[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        @cache
-        def dp(i, j):
-            best = 1
-            for dr, dc in directions:
-                nr, nc = i + dr, j + dc
-                if 0 <= nr < m and 0 <= nc < n and matrix[nr][nc] > matrix[i][j]:
-                    best = max(best, 1 + dp(nr, nc))
-            return best
-        
-        return max(dp(i, j) for i in range(m) for j in range(n))
+# Last updated: 2/24/2026, 8:43:08 AM
+1class Solution:
+2    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+3        rows, cols = len(matrix), len(matrix[0])
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5
+6        @cache
+7        def dp(r, c):
+8            path = 0
+9            for dr, dc in directions:
+10                nr, nc = r + dr, c + dc
+11                if 0 <= nr < rows and 0 <= nc < cols and matrix[nr][nc] > matrix[r][c]:
+12                    path = max(path, dp(nr, nc))
+13            return path + 1
+14
+15        longest = 0
+16        for r in range(rows):
+17            for c in range(cols):
+18                longest = max(longest, dp(r, c))
+19        return longest
