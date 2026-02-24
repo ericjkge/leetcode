6ccf319045d9f64be0fcf66e913e7e4ae9e68f7b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-23 20:18:24 -0500

    File updated at 2/23/2026, 8:18:24 PM

--- 3397-MaximumNumberofDistinctElementsAfterOperations/3397-MaximumNumberofDistinctElementsAfterOperations.py
+++ 3397-MaximumNumberofDistinctElementsAfterOperations/3397-MaximumNumberofDistinctElementsAfterOperations.py
@@ -1,4 +1,4 @@
-# Last updated: 2/22/2026, 11:39:51 PM
+# Last updated: 2/23/2026, 8:18:24 PM
 1class Solution:
 2    def maxDistinctElements(self, nums: List[int], k: int) -> int:
 3        nums.sort()
