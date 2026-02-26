aba73f0e9d76640534e67f0c9b131d82e00be95c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-26 09:51:19 -0500

    File updated at 2/26/2026, 9:51:19 AM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,16 +1,15 @@
-# Last updated: 1/14/2026, 11:19:33 PM
+# Last updated: 2/26/2026, 9:51:19 AM
 1class Solution:
 2    def change(self, amount: int, coins: List[int]) -> int:
-3        n = len(coins)
-4        
-5        @cache
-6        def dp(i, j): # index, amount
-7            if j > amount or i == n:
-8                return 0
-9            
-10            if j == amount:
-11                return 1
-12
-13            return dp(i, j + coins[i]) + dp(i + 1, j)
+3        @cache
+4        def dp(i, j): # index, total
+5            if j == amount:
+6                return 1
+7            
+8            if i == len(coins) or j > amount:
+9                return 0
+10            
+11            return dp(i, j + coins[i]) + dp(i + 1, j)
+12        
+13        return dp(0, 0)
 14
-15        return dp(0, 0)
