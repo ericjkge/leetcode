59e5d83d659ae389ed581f8b0ae397ed3f8e683b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-27 08:39:20 -0500

    File updated at 2/27/2026, 8:39:19 AM

--- 7-ReverseInteger/7-ReverseInteger.py
+++ 7-ReverseInteger/7-ReverseInteger.py
@@ -1,19 +1,20 @@
-# Last updated: 1/14/2026, 11:25:41 PM
+# Last updated: 2/27/2026, 8:39:19 AM
 1class Solution:
 2    def reverse(self, x: int) -> int:
-3        MAX = 2 ** 31 - 1
-4        MIN = -2 ** 31
-5
-6        flag = 1 if x >= 0 else -1
-7        x = abs(x)
-8        reverse = 0
-9
-10        while x:
-11            reverse *= 10
-12            reverse += x % 10
-13            x //= 10
-14            if reverse > MAX or reverse < MIN:
-15                return 0
+3        MIN = -2**31
+4        MAX = 2**31 - 1
+5        rev = 0
+6        sign = 1
+7
+8        if x < 0:
+9            sign = -1
+10            x *= -1
+11
+12        while x:
+13            rev *= 10
+14            rev += x % 10
+15            x //= 10
 16        
-17        return reverse * flag
+17        rev *= sign
 18
+19        return rev if MIN <= rev <= MAX else 0
