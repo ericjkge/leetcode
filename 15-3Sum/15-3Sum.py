8627535892908de8311980d32f9df98835ed0f2d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-27 09:14:24 -0500

    File updated at 2/27/2026, 9:14:24 AM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,24 +1,24 @@
-# Last updated: 1/15/2026, 12:35:57 PM
+# Last updated: 2/27/2026, 9:14:24 AM
 1class Solution:
-2    def threeSum(self, nums: List[int]) -> List[List[int]]:
+2    def threeSum(self, nums: list[int]) -> list[list[int]]:
 3        nums.sort()
-4        solutions = []
-5        n = len(nums)
+4        print(nums)
+5        triplets = []
 6
-7        for i in range(n):
-8            if i > 0 and nums[i] == nums[i - 1]:
+7        for i, num in enumerate(nums):
+8            if i > 0 and num == nums[i - 1]:
 9                continue
-10            target = -nums[i]
-11            left, right = i + 1, n - 1
+10            target = -num
+11            left, right = i + 1, len(nums) - 1
 12            while left < right:
-13                if nums[left] + nums[right] < target:
-14                    left += 1
-15                elif nums[left] + nums[right] > target:
-16                    right -= 1
+13                if nums[left] + nums[right] > target:
+14                    right -= 1
+15                elif nums[left] + nums[right] < target:
+16                    left += 1
 17                else:
-18                    solutions.append([nums[i], nums[left], nums[right]])
+18                    triplets.append([num, nums[left], nums[right]])
 19                    left += 1
 20                    while left < right and nums[left] == nums[left - 1]:
 21                        left += 1
 22
-23        return solutions
+23        return triplets
