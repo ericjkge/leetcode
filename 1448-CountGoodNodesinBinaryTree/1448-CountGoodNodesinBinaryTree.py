dc5860714d54192ce37aaeff7a177014395106fc
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-27 08:44:38 -0500

    File updated at 2/27/2026, 8:44:37 AM

--- 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.py
+++ 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 1/14/2026, 11:33:36 PM
+# Last updated: 2/27/2026, 8:44:37 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -9,16 +9,17 @@
 8    def goodNodes(self, root: TreeNode) -> int:
 9        self.counter = 0
 10
-11        def dfs(node, max_node):
+11        def dfs(node, maximum):
 12            if not node:
 13                return
-14
-15            if node.val >= max_node:
-16                self.counter += 1
-17            
-18            max_node = max(max_node, node.val)
-19            dfs(node.left, max_node)
-20            dfs(node.right, max_node)
-21            
-22        dfs(root, root.val)
-23        return self.counter
+14            
+15            if node.val >= maximum:
+16                self.counter += 1            
+17                dfs(node.left, node.val)
+18                dfs(node.right, node.val)
+19            else:
+20                dfs(node.left, maximum)
+21                dfs(node.right, maximum)
+22    
+23        dfs(root, root.val)
+24        return self.counter
