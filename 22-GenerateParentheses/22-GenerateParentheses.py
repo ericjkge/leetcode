7d9a0971ad5aa985cf29e9ef79cdec4e76d6edf9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-28 10:07:10 -0500

    File updated at 2/28/2026, 10:07:10 AM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,23 +1,22 @@
-# Last updated: 1/5/2026, 9:26:21 AM
+# Last updated: 2/28/2026, 10:07:10 AM
 1class Solution:
 2    def generateParenthesis(self, n: int) -> List[str]:
 3        ans = []
 4
 5        def backtrack(open, closed, path):
-6            if open == n and closed == n:
+6            if len(path) == n * 2:
 7                ans.append("".join(path))
 8                return
 9            
-10            if open < n:
+10            if open > 0:
 11                path.append("(")
-12                backtrack(open + 1, closed, path)
+12                backtrack(open - 1, closed, path)
 13                path.pop()
-14        
-15            if open > closed:
+14
+15            if closed > open:
 16                path.append(")")
-17                backtrack(open, closed + 1, path)
+17                backtrack(open, closed - 1, path)
 18                path.pop()
 19
-20        backtrack(0, 0, [])
+20        backtrack(n, n, [])
 21        return ans
-22
