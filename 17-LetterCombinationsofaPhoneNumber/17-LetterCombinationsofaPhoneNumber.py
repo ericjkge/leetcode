c674531bfc682883f46a58fafdc82c40eb231fa8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-28 09:44:57 -0500

    File updated at 2/28/2026, 9:44:57 AM

--- 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
+++ 17-LetterCombinationsofaPhoneNumber/17-LetterCombinationsofaPhoneNumber.py
@@ -1,28 +1,28 @@
-# Last updated: 11/7/2025, 6:56:12 PM
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        mappings = {
-            "2":"abc",
-            "3":"def",
-            "4":"ghi",
-            "5":"jkl",
-            "6":"mno",
-            "7":"pqrs",
-            "8":"tuv",
-            "9":"wxyz"
-        }
-
-        ans = []
-
-        def backtrack(index, path):
-            if len(path) == len(digits):
-                ans.append("".join(path))
-                return
-            
-            for letter in mappings[digits[index]]:
-                path.append(letter)
-                backtrack(index + 1, path)
-                path.pop()
-                
-        backtrack(0, [])
-        return ans
+# Last updated: 2/28/2026, 9:44:57 AM
+1class Solution:
+2    def letterCombinations(self, digits: str) -> List[str]:
+3        mappings = {
+4            "2":"abc",
+5            "3":"def",
+6            "4":"ghi",
+7            "5":"jkl",
+8            "6":"mno",
+9            "7":"pqrs",
+10            "8":"tuv",
+11            "9":"wxyz"
+12        }
+13
+14        ans = []
+15
+16        def backtrack(index, path):
+17            if len(path) == len(digits):
+18                ans.append("".join(path))
+19                return
+20            
+21            for char in mappings[digits[index]]:
+22                path.append(char)
+23                backtrack(index + 1, path)
+24                path.pop()
+25        
+26        backtrack(0, [])
+27        return ans
