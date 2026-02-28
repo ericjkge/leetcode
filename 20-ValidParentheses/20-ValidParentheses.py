accead309a1e45ab9244b8967fda7b91cbd5dff6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-02-28 09:56:11 -0500

    File updated at 2/28/2026, 9:56:11 AM

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,14 +1,20 @@
-# Last updated: 11/14/2025, 11:13:48 AM
-class Solution:
-    def isValid(self, s: str) -> bool:
-        mappings = {"(":")", "{":"}", "[":"]"}
-        stack = []
-
-        for c in s:
-            if c in mappings:
-                stack.append(c)
-            else:
-                if not stack or mappings[stack.pop()] != c:
-                    return False
-        
-        return not stack
+# Last updated: 2/28/2026, 9:56:11 AM
+1class Solution:
+2    def isValid(self, s: str) -> bool:
+3        mappings = {
+4            ")":"(",
+5            "}":"{",
+6            "]":"["
+7        }
+8        stack = []
+9
+10        for char in s:
+11            if char in mappings:
+12                if stack and stack[-1] == mappings[char]:
+13                    stack.pop()
+14                else:
+15                    return False
+16            else:
+17                stack.append(char)
+18        
+19        return not stack
