70d048765d36e47801b0150dd0de4fb1743344a7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-01 13:20:41 -0500

    File updated at 3/1/2026, 1:20:40 PM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.py
@@ -1,4 +1,4 @@
-# Last updated: 1/18/2026, 9:26:53 AM
+# Last updated: 3/1/2026, 1:20:40 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -7,23 +7,23 @@
 6class Solution:
 7    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 8        dummy = group_prev = ListNode(0, head)
-9
-10        while True:
-11            kth = group_prev
+9        group_next = head
+10
+11        while True:
 12            for _ in range(k):
-13                kth = kth.next
-14                if not kth:
-15                    return dummy.next
-16
-17            group_next = kth.next
-18            prev, curr = group_next, group_prev.next
-19
-20            while curr != group_next:
-21                nxt = curr.next
-22                curr.next = prev
-23                prev = curr
-24                curr = nxt
-25
-26            temp = group_prev.next
+13                if not group_next:
+14                    return dummy.next
+15                group_next = group_next.next            
+16            
+17            curr = group_prev.next
+18            prev = group_prev
+19            while curr != group_next:
+20                nxt = curr.next
+21                curr.next = prev
+22                prev = curr
+23                curr = nxt
+24            
+25            temp = group_prev.next
+26            temp.next = group_next
 27            group_prev.next = prev
-28            group_prev = temp
+28            group_prev = temp 
