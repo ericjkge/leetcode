06f30d0902f34191b6651d68278bbdf6e9f23867
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-02 09:25:26 -0500

    File updated at 3/2/2026, 9:25:26 AM

--- 40-CombinationSumII/40-CombinationSumII.py
+++ 40-CombinationSumII/40-CombinationSumII.py
@@ -1,24 +1,23 @@
-# Last updated: 1/18/2026, 10:58:15 AM
+# Last updated: 3/2/2026, 9:25:26 AM
 1class Solution:
 2    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-3        n = len(candidates)
-4        combinations = []
-5        candidates.sort()
-6
-7        def backtrack(index, path):
-8            if sum(path) == target:
-9                combinations.append(path[:])
-10                return
-11            
-12            if sum(path) > target:
-13                return
-14            
-15            for i in range(index, n):
-16                if i > index and candidates[i] == candidates[i - 1]:
-17                    continue
-18                path.append(candidates[i])
-19                backtrack(i + 1, path)
-20                path.pop()
-21        
-22        backtrack(0, [])
-23        return combinations
+3        combinations = []
+4        candidates.sort()
+5
+6        def backtrack(index, path):
+7            if sum(path) == target:
+8                combinations.append(path[:])
+9                return
+10            
+11            if sum(path) > target:
+12                return
+13
+14            for i in range(index, len(candidates)):
+15                if i > index and candidates[i] == candidates[i - 1]:
+16                    continue
+17                path.append(candidates[i])
+18                backtrack(i + 1, path)
+19                path.pop()
+20
+21        backtrack(0, [])
+22        return combinations
