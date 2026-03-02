872284320d31ef62cfbac576b703f5f718d5a7a3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-02 09:36:27 -0500

    File updated at 3/2/2026, 9:36:26 AM

--- 43-MultiplyStrings/43-MultiplyStrings.py
+++ 43-MultiplyStrings/43-MultiplyStrings.py
@@ -1,28 +1,22 @@
-# Last updated: 11/17/2025, 9:49:50 AM
-class Solution:
-    def multiply(self, num1: str, num2: str) -> str:
-        if num1 == "0" or num2 == "0":
-            return "0"
-
-        m, n = len(num1), len(num2)
-        res = [0] * (m + n)
-
-        for j in range(n - 1, -1, -1):
-            for i in range(m - 1, -1, -1):
-                prod = (ord(num1[i]) - 48) * (ord(num2[j]) - 48)
-                s = prod + res[i + j + 1]
-
-                res[i + j + 1] = s % 10
-                res[i + j] += s // 10 # Store carry for next prod
-        
-        i = 0
-        while i < len(res) and res[i] == 0:
-            i += 1
-
-        return "".join(str(x) for x in res[i:])
-        
-
-
-
-
-        return ans
+# Last updated: 3/2/2026, 9:36:26 AM
+1class Solution:
+2    def multiply(self, num1: str, num2: str) -> str:
+3        if num1 == "0" or num2 == "0":
+4            return "0"
+5
+6        m, n = len(num1), len(num2)
+7        res = [0] * (m + n)
+8
+9        for i in range(m - 1, -1, -1):
+10            for j in range(n - 1, -1, -1):
+11                prod = (ord(num1[i]) - 48) * (ord(num2[j]) - 48)
+12                s = prod + res[i + j + 1]
+13
+14                res[i + j + 1] = s % 10
+15                res[i + j] += s // 10
+16        
+17        i = 0
+18        while i < len(res) and res[i] == 0:
+19            i += 1
+20
+21        return "".join(str(x) for x in res[i:])
