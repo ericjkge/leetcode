6f21eb97fe101a1e44446b5c30fb0e4f880830b2
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-03 09:07:59 -0500

    File updated at 3/3/2026, 9:07:58 AM

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,4 +1,4 @@
-# Last updated: 3/3/2026, 9:07:04 AM
+# Last updated: 3/3/2026, 9:07:58 AM
 1class Solution:
 2    def myPow(self, x: float, n: int) -> float:
 3        def binExp(b, p):
@@ -8,8 +8,9 @@
 7            if p < 0:
 8                return 1/binExp(b, -p)
 9
-10            if p % 2 == 0:
-11                return binExp(b * b, p // 2)
-12            return b * binExp(b * b, (p - 1) // 2)
-13        
-14        return binExp(x, n)
+10            res = binExp(b, p // 2)
+11            if p % 2 == 0:
+12                return res * res
+13            return b * res * res
+14        
+15        return binExp(x, n)
