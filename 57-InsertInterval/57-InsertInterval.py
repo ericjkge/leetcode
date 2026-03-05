912898dfcbdc67bc7a403ac0f6932789f58e989b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-05 10:04:48 -0500

    File updated at 3/5/2026, 10:04:48 AM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,22 +1,26 @@
-# Last updated: 1/25/2026, 7:21:05 AM
+# Last updated: 3/5/2026, 10:04:48 AM
 1class Solution:
 2    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
 3        index = 0
-4        results = []
-5        n = len(intervals)
-6
-7        while index < n and intervals[index][1] < newInterval[0]:
-8            results.append(intervals[index])
-9            index += 1
-10
-11        while index < n and intervals[index][0] <= newInterval[1]:
-12            newInterval[0] = min(newInterval[0], intervals[index][0])
-13            newInterval[1] = max(newInterval[1], intervals[index][1])
-14            index += 1
-15        results.append(newInterval)
-16
-17        while index < n:
-18            results.append(intervals[index])
-19            index += 1
-20        
-21        return results
+4        res = []
+5
+6        while index < len(intervals) and newInterval[0] > intervals[index][1]:
+7            res.append(intervals[index])
+8            index += 1
+9        
+10        res.append(newInterval)
+11
+12        while index < len(intervals) and res[-1][1] >= intervals[index][0]:
+13            start = min(res[-1][0], intervals[index][0])
+14            end = max(res[-1][1], intervals[index][1])
+15            res[-1] = [start, end]
+16            index += 1
+17        
+18        while index < len(intervals):
+19            res.append(intervals[index])
+20            index += 1
+21
+22        return res
+23        
+24
+25
