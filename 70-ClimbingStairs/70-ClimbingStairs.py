b9f2512ebb1ea96c956b36badb89b366011f4b3d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-06 11:59:40 -0500

    File updated at 3/6/2026, 11:59:40 AM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,10 +1,14 @@
-# Last updated: 11/22/2025, 2:05:16 PM
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        @cache
-        def dp(i):
-            if i == 0 or i == 1:
-                return 1
-            return dp(i - 1) + dp(i - 2)
-        
-        return dp(n)
+# Last updated: 3/6/2026, 11:59:40 AM
+1class Solution:
+2    def climbStairs(self, n: int) -> int:
+3        @cache
+4        def dp(i):
+5            if i == n:
+6                return 1
+7            
+8            if i > n:
+9                return 0
+10
+11            return dp(i + 1) + dp(i + 2)
+12        
+13        return dp(0)
