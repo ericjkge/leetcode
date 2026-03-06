7585b3db28b11bd57cb04c6a000f5642509b6d33
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-06 11:44:49 -0500

    File updated at 3/6/2026, 11:44:49 AM

--- 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
+++ 572-SubtreeofAnotherTree/572-SubtreeofAnotherTree.py
@@ -1,4 +1,4 @@
-# Last updated: 1/25/2026, 8:16:06 AM
+# Last updated: 3/6/2026, 11:44:49 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -7,18 +7,20 @@
 6#         self.right = right
 7class Solution:
 8    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-9        def isSameTree(n1, n2):
-10            if not n1 and not n2:
-11                return True
-12            elif not n1 or not n2:
-13                return False
-14            
-15            if n1.val != n2.val:
-16                return False
-17            
-18            return isSameTree(n1.left, n2.left) and isSameTree(n1.right, n2.right)
-19
-20        if not root:
+9        if not root:
+10            return False
+11            
+12        def isSameTree(n1, n2):
+13            if not n1 and not n2:
+14                return True
+15
+16            if not n1 or not n2:
+17                return False
+18            
+19            if n1.val == n2.val: 
+20                return isSameTree(n1.left, n2.left) and isSameTree(n1.right, n2.right)
 21            return False
-22            
-23        return isSameTree(root, subRoot) or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+22        
+23        if isSameTree(root, subRoot):
+24            return True
+25        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
