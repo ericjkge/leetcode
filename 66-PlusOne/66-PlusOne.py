1a5c11809ef9335de8b461665d50692273d84546
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-06 11:56:47 -0500

    File updated at 3/6/2026, 11:56:46 AM

--- 66-PlusOne/66-PlusOne.py
+++ 66-PlusOne/66-PlusOne.py
@@ -1,16 +1,14 @@
-# Last updated: 1/25/2026, 8:35:37 AM
+# Last updated: 3/6/2026, 11:56:46 AM
 1class Solution:
 2    def plusOne(self, digits: List[int]) -> List[int]:
-3        n = len(digits)
-4        carry = 1
-5
-6        for i in range(n - 1, -1, -1):
-7            total = digits[i] + carry
-8            if total > 9:
-9                carry = 1
-10                digits[i] = total % 10
-11            else:
-12                carry = 0
-13                digits[i] = total
-14        
-15        return [1] + digits if carry else digits 
+3        carry = 1
+4
+5        for i in range(len(digits) - 1, -1, -1):
+6            total = digits[i] + carry
+7            digits[i] = total % 10
+8            carry = total // 10
+9        
+10        if carry:
+11            digits = [1] + digits
+12        
+13        return digits
