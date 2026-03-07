f1eb73042ffaf360d4ef3f0092daa9bc035f2d21
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-07 11:29:44 -0500

    File updated at 3/7/2026, 11:29:44 AM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,18 +1,18 @@
-# Last updated: 2/24/2026, 10:02:42 AM
+# Last updated: 3/7/2026, 11:29:44 AM
 1class Solution:
 2    def minWindow(self, s: str, t: str) -> str:
-3        left = right = 0
-4        s_counter = Counter()
-5        t_counter = Counter(t)
+3        s_counter = defaultdict(int)
+4        t_counter = Counter(t)
+5        left = right = 0
 6        ans = None
 7
 8        while right < len(s):
 9            s_counter[s[right]] += 1
-10            while all(s_counter[k] >= t_counter[k] for k in t_counter.keys()):
-11                if not ans or right - left + 1 < len(ans):
+10            while all(s_counter[c] >= t_counter[c] for c in t_counter.keys()):
+11                if ans is None or right - left + 1 < len(ans):
 12                    ans = s[left:right + 1]
 13                s_counter[s[left]] -= 1
 14                left += 1
-15            right += 1 
-16
+15            right += 1
+16        
 17        return ans if ans else ""
