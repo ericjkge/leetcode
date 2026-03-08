581fae554d3c844af52c53c6ab020ffe4e921a42
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-08 09:18:29 -0400

    File updated at 3/8/2026, 9:18:29 AM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,4 +1,4 @@
-# Last updated: 3/8/2026, 9:15:44 AM
+# Last updated: 3/8/2026, 9:18:29 AM
 1class Solution:
 2    def exist(self, board: List[List[str]], word: str) -> bool:
 3        m, n = len(board), len(board[0])
@@ -8,27 +8,23 @@
 7            if index == len(word):
 8                return True
 9            
-10            for dr, dc in directions:
-11                nr, nc = r + dr, c + dc
-12                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == word[index]:
-13                    old = board[nr][nc]
-14                    board[nr][nc] = "#"
-15                    if backtrack(nr, nc, index + 1):
-16                        return True
-17                    board[nr][nc] = old
-18
-19            return False
-20
-21        for r in range(m):
-22            for c in range(n):
-23                if board[r][c] == word[0]:
-24                    old = board[r][c]
-25                    board[r][c] = "#"
-26                    if backtrack(r, c, 1):
-27                        return True
-28                    board[r][c] = old
-29
-30        return False
-31
-32
-33                    
+10            old = board[r][c]
+11            board[r][c] = "#"
+12
+13            for dr, dc in directions:
+14                nr, nc = r + dr, c + dc
+15                if 0 <= nr < m and 0 <= nc < n and board[nr][nc] == word[index]:
+16                    if backtrack(nr, nc, index + 1):
+17                        return True
+18            
+19            board[r][c] = old
+20            return False
+21
+22        for r in range(m):
+23            for c in range(n):
+24                if board[r][c] == word[0] and backtrack(r, c, 1):
+25                    return True
+26        return False
+27
+28
+29                    
