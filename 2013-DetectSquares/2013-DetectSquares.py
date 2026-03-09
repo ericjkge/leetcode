19d971dc40a31a0cb6b254f7c34678e37eace249
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-09 14:27:01 -0400

    File updated at 3/9/2026, 2:27:01 PM

--- 2013-DetectSquares/2013-DetectSquares.py
+++ 2013-DetectSquares/2013-DetectSquares.py
@@ -1,7 +1,7 @@
-# Last updated: 1/28/2026, 9:06:35 AM
+# Last updated: 3/9/2026, 2:27:01 PM
 1class DetectSquares:
 2
-3    def __init__(self):        
+3    def __init__(self):
 4        self.points = Counter()
 5
 6    def add(self, point: List[int]) -> None:
@@ -12,11 +12,11 @@
 11        px, py = point
 12
 13        for (x, y), freq in self.points.items():
-14            if abs(x - px) == abs(y - py) and x != px:
-15                total += freq * self.points[(x, py)] * self.points[(px, y)]
+14            if abs(px - x) != abs(py - y) or (x, y) == (px, py):
+15                continue
 16
-17        return total
-18
+17            total += self.points[(px, y)] * self.points[(x, py)] * freq
+18        return total
 19
 20# Your DetectSquares object will be instantiated and called as such:
 21# obj = DetectSquares()
