e08ca44b9544569ef7c459b9056501d67ef87c6d
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-09 13:52:18 -0400

    File updated at 3/9/2026, 1:52:18 PM

--- 91-DecodeWays/91-DecodeWays.py
+++ 91-DecodeWays/91-DecodeWays.py
@@ -1,17 +1,19 @@
-# Last updated: 1/27/2026, 12:03:00 PM
+# Last updated: 3/9/2026, 1:52:18 PM
 1class Solution:
 2    def numDecodings(self, s: str) -> int:
 3        @cache
 4        def dp(i):
 5            if i == len(s):
 6                return 1
-7
-8            if int(s[i]) == 0:
+7            
+8            if s[i] == "0":
 9                return 0
-10            
+10
 11            ways = dp(i + 1)
-12            if i + 1 < len(s) and (int(s[i]) == 1 or (int(s[i]) == 2 and 0 <= int(s[i + 1]) <= 6)):
-13                ways += dp(i + 2)
-14            return ways
-15
-16        return dp(0)
+12
+13            if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and 0 <= int(s[i + 1]) <= 6)):
+14                ways += dp(i + 2)
+15            
+16            return ways
+17        
+18        return dp(0)
