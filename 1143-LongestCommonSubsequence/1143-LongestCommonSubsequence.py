865c216ea16f10d9f3718c89d065094f8dacf1d0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-12 14:13:41 -0400

    File updated at 3/12/2026, 2:13:41 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
@@ -1,14 +1,14 @@
-# Last updated: 2/8/2026, 4:53:49 PM
+# Last updated: 3/12/2026, 2:13:41 PM
 1class Solution:
 2    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
 3        @cache
 4        def dp(i, j):
 5            if i == len(text1) or j == len(text2):
 6                return 0
-7            
+7
 8            if text1[i] == text2[j]:
 9                return 1 + dp(i + 1, j + 1)
 10            
-11            return max(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1))
+11            return max(dp(i + 1, j), dp(i, j + 1))
 12        
 13        return dp(0, 0)
