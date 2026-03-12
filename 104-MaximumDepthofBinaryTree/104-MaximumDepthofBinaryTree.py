76113a84588ed4b950f06d0143a304434a94e7f6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-11 23:38:46 -0400

    File updated at 3/11/2026, 11:38:46 PM

--- 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 3/11/2026, 11:38:21 PM
+# Last updated: 3/11/2026, 11:38:46 PM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -7,12 +7,6 @@
 6#         self.right = right
 7class Solution:
 8    def maxDepth(self, root: Optional[TreeNode]) -> int:
-9        def dfs(node):
-10            if not node:
-11                return 0
-12            
-13            left = dfs(node.left)
-14            right = dfs(node.right)
-15            return 1 + max(left, right)
-16        
-17        return dfs(root)
+9        if not root:
+10            return 0
+11        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
