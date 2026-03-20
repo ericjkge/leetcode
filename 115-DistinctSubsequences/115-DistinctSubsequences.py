5990f6fe0936519da5071627ab8410c8c459b949
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-20 13:15:52 -0600

    File updated at 3/20/2026, 3:15:52 PM

--- 115-DistinctSubsequences/115-DistinctSubsequences.py
+++ 115-DistinctSubsequences/115-DistinctSubsequences.py
@@ -1,17 +1,21 @@
-# Last updated: 2/22/2026, 12:22:12 PM
+# Last updated: 3/20/2026, 3:15:52 PM
 1class Solution:
 2    def numDistinct(self, s: str, t: str) -> int:
 3        @cache
 4        def dp(i, j):
 5            if j == len(t):
 6                return 1
-7
+7            
 8            if i == len(s):
 9                return 0
-10        
-11            if s[i] == t[j]:
-12                return dp(i + 1, j + 1) + dp(i + 1, j)
-13
-14            return dp(i + 1, j)
-15        
-16        return dp(0, 0)
+10
+11            ways = 0
+12            
+13            if s[i] == t[j]:
+14                ways += dp(i + 1, j + 1)
+15            
+16            ways += dp(i + 1, j)
+17
+18            return ways
+19        
+20        return dp(0, 0)
