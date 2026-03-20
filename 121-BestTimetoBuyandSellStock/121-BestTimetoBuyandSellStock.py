0792dd84b6947c7ec486bd74276df38913931628
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-20 13:19:18 -0600

    File updated at 3/20/2026, 3:19:17 PM

--- 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
+++ 121-BestTimetoBuyandSellStock/121-BestTimetoBuyandSellStock.py
@@ -1,8 +1,8 @@
-# Last updated: 2/15/2026, 12:40:03 PM
+# Last updated: 3/20/2026, 3:19:17 PM
 1class Solution:
 2    def maxProfit(self, prices: List[int]) -> int:
 3        lowest = float("inf")
-4        profit = -float("inf")
+4        profit = 0
 5
 6        for price in prices:
 7            lowest = min(lowest, price)
