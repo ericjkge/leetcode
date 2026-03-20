dba012b17235ac3cac7d382181eba82b061746cd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-20 13:36:51 -0600

    File updated at 3/20/2026, 3:36:50 PM

--- 125-ValidPalindrome/125-ValidPalindrome.py
+++ 125-ValidPalindrome/125-ValidPalindrome.py
@@ -1,21 +1,9 @@
-# Last updated: 1/29/2026, 10:50:59 AM
+# Last updated: 3/20/2026, 3:36:50 PM
 1class Solution:
 2    def isPalindrome(self, s: str) -> bool:
-3        left, right = 0, len(s) - 1
-4
-5        while left < right:
-6            if not s[left].isalnum():
-7                left += 1
-8                continue
-9            
-10            if not s[right].isalnum():
-11                right -= 1
-12                continue
-13            
-14            if s[left].lower() != s[right].lower():
-15                return False
-16            
-17            left += 1
-18            right -= 1
-19        
-20        return True
+3        clean = []
+4        for char in s:
+5            if char.isalnum():
+6                clean.append(char.lower())
+7        
+8        return clean == clean[::-1]
