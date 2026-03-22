e23888712618163bafc496436315c9c666faf53e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-22 07:40:11 -0600

    File updated at 3/22/2026, 9:40:11 AM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.py
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -1,17 +1,16 @@
-# Last updated: 1/31/2026, 1:18:10 PM
+# Last updated: 3/22/2026, 9:40:11 AM
 1class Solution:
 2    def countSubstrings(self, s: str) -> int:
-3        n = len(s)
-4        self.ans = 0
-5
-6        def expand(i, j):
-7            while i >= 0 and j < n and s[i] == s[j]:
-8                self.ans += 1
-9                i -= 1
-10                j += 1
-11                
-12        for k in range(n):
-13            expand(k, k)
-14            expand(k, k + 1)
-15
-16        return self.ans
+3        self.count = 0
+4
+5        def expand(i, j):
+6            while i >= 0 and j < len(s) and s[i] == s[j]:
+7                self.count += 1
+8                i -= 1
+9                j += 1
+10
+11        for i in range(len(s)):
+12            expand(i, i)
+13            expand(i, i + 1)
+14        
+15        return self.count
