10787b8d17a62a2eb57903cd9b28a5d18118d033
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-22 07:41:21 -0600

    File updated at 3/22/2026, 9:41:20 AM

--- 136-SingleNumber/136-SingleNumber.py
+++ 136-SingleNumber/136-SingleNumber.py
@@ -1,7 +1,9 @@
-# Last updated: 1/31/2026, 1:08:21 PM
+# Last updated: 3/22/2026, 9:41:20 AM
 1class Solution:
 2    def singleNumber(self, nums: List[int]) -> int:
 3        total = 0
-4        for num in nums:
-5            total ^= num
-6        return total
+4        
+5        for num in nums:
+6            total ^= num
+7        
+8        return total
