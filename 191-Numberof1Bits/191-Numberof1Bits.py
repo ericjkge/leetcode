ce6c3b1dbc5c9d6b0f21bfd9f32e533131789da5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-30 09:20:20 -0400

    File updated at 3/30/2026, 9:20:20 AM

--- 191-Numberof1Bits/191-Numberof1Bits.py
+++ 191-Numberof1Bits/191-Numberof1Bits.py
@@ -1,10 +1,10 @@
-# Last updated: 2/5/2026, 8:39:12 AM
+# Last updated: 3/30/2026, 9:20:20 AM
 1class Solution:
 2    def hammingWeight(self, n: int) -> int:
-3        counter = 0
+3        bits = 0
 4
 5        while n:
-6            counter += n & 1
+6            bits += (n & 1)
 7            n >>= 1
 8        
-9        return counter
+9        return bits
