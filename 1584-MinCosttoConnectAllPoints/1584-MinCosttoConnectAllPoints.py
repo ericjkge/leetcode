cae966f397fa2fb38ee715821b633e3a896fc4b0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-29 20:46:45 -0400

    File updated at 3/29/2026, 8:46:45 PM

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
@@ -1,4 +1,4 @@
-# Last updated: 3/29/2026, 8:46:31 PM
+# Last updated: 3/29/2026, 8:46:45 PM
 1class Solution:
 2    def minCostConnectPoints(self, points: List[List[int]]) -> int:
 3        seen = set()
@@ -12,8 +12,8 @@
 11            seen.add(u)
 12            cost += w
 13            for v in range(len(points)):
-14                if v not in seen:
-15                    heapq.heappush(heap, (abs(points[v][0] - points[u][0]) + abs(points[v][1] - points[u][1]), v))
+14                # if v not in seen:
+15                heapq.heappush(heap, (abs(points[v][0] - points[u][0]) + abs(points[v][1] - points[u][1]), v))
 16        
 17        return cost
 18
