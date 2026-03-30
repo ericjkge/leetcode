22996df8f8a4f54b13038efd73e6c32fc619e296
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-03-30 09:16:16 -0400

    File updated at 3/30/2026, 9:16:16 AM

--- 190-ReverseBits/190-ReverseBits.py
+++ 190-ReverseBits/190-ReverseBits.py
@@ -1,11 +1,11 @@
-# Last updated: 2/3/2026, 10:30:23 AM
+# Last updated: 3/30/2026, 9:16:16 AM
 1class Solution:
 2    def reverseBits(self, n: int) -> int:
-3        res = 0
-4        
+3        reversed = 0
+4
 5        for i in range(32):
-6            res <<= 1
-7            res |= (n & 1)
+6            reversed <<= 1
+7            reversed |= (n&1)
 8            n >>= 1
 9        
-10        return res
+10        return reversed
