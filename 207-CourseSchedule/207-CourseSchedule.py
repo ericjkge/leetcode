269dc5ad5c7dedfa4fc79356b38326e3776ff6ae
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-02 09:51:38 -0400

    File updated at 4/2/2026, 9:51:38 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,22 +1,21 @@
-# Last updated: 3/12/2026, 2:33:13 PM
+# Last updated: 4/2/2026, 9:51:38 AM
 1class Solution:
 2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 3        graph = defaultdict(list)
-4        indegrees = defaultdict(int)
+4        indegrees = [0] * numCourses
 5
-6        for a, b in prerequisites:
-7            graph[b].append(a)
-8            indegrees[a] += 1
-9        
-10        queue = deque([k for k in range(numCourses) if indegrees[k] == 0])
-11        count = 0
-12
-13        while queue:
+6        for u, v in prerequisites:
+7            graph[v].append(u)
+8            indegrees[u] += 1
+9    
+10        count = 0
+11        queue = deque([i for i in range(numCourses) if indegrees[i] == 0])
+12        while queue:
+13            count += 1
 14            node = queue.popleft()
-15            count += 1
-16            for neighbor in graph[node]:
-17                indegrees[neighbor] -= 1
-18                if indegrees[neighbor] == 0:
-19                    queue.append(neighbor)
-20        
-21        return count == numCourses
+15            for neighbor in graph[node]:
+16                indegrees[neighbor] -= 1
+17                if indegrees[neighbor] == 0:
+18                    queue.append(neighbor)
+19        
+20        return count == numCourses
