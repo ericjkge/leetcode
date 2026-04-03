78c3ac62528049d76ba97f8d4e8f4839e3de726c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-03 09:54:04 -0400

    File updated at 4/3/2026, 9:54:04 AM

--- 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
+++ 211-DesignAddandSearchWordsDataStructure/211-DesignAddandSearchWordsDataStructure.py
@@ -1,8 +1,8 @@
-# Last updated: 2/6/2026, 10:33:59 AM
-1class TrieNode():
+# Last updated: 4/3/2026, 9:54:04 AM
+1class TrieNode:
 2    def __init__(self):
-3        self.children = {}
-4        self.is_end = False
+3        self.is_end = False
+4        self.children = {}
 5
 6class WordDictionary:
 7
@@ -22,21 +22,20 @@
 21        for i in range(len(word)):
 22            c = word[i]
 23            if c == ".":
-24                for c, n in node.children.items():
-25                    if self.dfs(word[i + 1:], n):
+24                for child in node.children.values():
+25                    if self.dfs(word[i + 1:], child):
 26                        return True
 27                return False
-28            else:
-29                if c not in node.children:
-30                    return False
-31            node = node.children[c]
-32        return node.is_end
-33
-34    def search(self, word: str) -> bool:
-35        return self.dfs(word, self.root)
+28            if c not in node.children:
+29                return False
+30            node = node.children[c]
+31        return node.is_end
+32
+33    def search(self, word: str) -> bool:
+34        return self.dfs(word, self.root)
+35
 36
-37
-38# Your WordDictionary object will be instantiated and called as such:
-39# obj = WordDictionary()
-40# obj.addWord(word)
-41# param_2 = obj.search(word)
+37# Your WordDictionary object will be instantiated and called as such:
+38# obj = WordDictionary()
+39# obj.addWord(word)
+40# param_2 = obj.search(word)
