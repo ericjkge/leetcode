ef2e3449c257bac018901948f072a3bf0b2af999
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-03 09:13:57 -0400

    File updated at 4/3/2026, 9:13:57 AM

--- 210-CourseScheduleII/210-CourseScheduleII.py
+++ 210-CourseScheduleII/210-CourseScheduleII.py
@@ -1,22 +1,21 @@
-# Last updated: 2/6/2026, 10:15:16 AM
+# Last updated: 4/3/2026, 9:13:57 AM
 1class Solution:
 2    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
 3        graph = defaultdict(list)
-4        indegree = [0] * numCourses
+4        indegrees = [0] * numCourses
 5
 6        for a, b in prerequisites:
 7            graph[b].append(a)
-8            indegree[a] += 1
-9
-10        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
-11        order = []
-12
-13        while queue:
-14            node = queue.popleft()
-15            order.append(node)
-16            for neighbor in graph[node]:
-17                indegree[neighbor] -= 1
-18                if indegree[neighbor] == 0:
-19                    queue.append(neighbor)
-20        
-21        return order if len(order) == numCourses else []
+8            indegrees[a] += 1
+9        
+10        order = []
+11        queue = deque([i for i in range(numCourses) if indegrees[i] == 0])
+12        while queue:
+13            course = queue.popleft()
+14            order.append(course)
+15            for neighbor in graph[course]:
+16                indegrees[neighbor] -= 1
+17                if indegrees[neighbor] == 0:
+18                    queue.append(neighbor)
+19        
+20        return order if len(order) == numCourses else []
