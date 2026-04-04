0cdaa09d626f54f8dcda4bd452ae4fcb8902b6b9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-04 10:02:04 -0400

    File updated at 4/4/2026, 10:02:03 AM

--- 684-RedundantConnection/684-RedundantConnection.py
+++ 684-RedundantConnection/684-RedundantConnection.py
@@ -1,32 +1,32 @@
-# Last updated: 3/29/2026, 9:46:13 PM
+# Last updated: 4/4/2026, 10:02:03 AM
 1class Solution:
 2    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
 3        class UnionFind:
-4            def __init__(self, size):
-5                self.root = [i for i in range(size)]
-6                self.rank = [1] * size
+4            def __init__(self, n):
+5                self.roots = [i for i in range(n + 1)]
+6                self.heights = [0] * (n + 1)
 7            
 8            def find(self, x):
-9                if self.root[x] != x:
-10                    self.root[x] = self.find(self.root[x])
-11                return self.root[x]
+9                if self.roots[x] != x:
+10                    self.roots[x] = self.find(self.roots[x])
+11                return self.roots[x]
 12
-13            def union(self, x, y):
-14                rootX = self.find(x)
-15                rootY = self.find(y)
-16                if rootX == rootY:
-17                    return False
-18                if self.rank[rootX] < self.rank[rootY]:
-19                    self.root[rootX] = rootY
-20                elif self.rank[rootX] > self.rank[rootY]:
-21                    self.root[rootY] = rootX
+13            def union(self, a, b):
+14                x, y = self.find(a), self.find(b)
+15                if self.roots[x] == self.roots[y]:
+16                    return True
+17                
+18                if self.heights[x] < self.heights[y]:
+19                    self.roots[x] = self.roots[y]
+20                elif self.heights[x] > self.heights[y]:
+21                    self.roots[y] = self.roots[x]
 22                else:
-23                    self.root[rootY] = rootX
-24                    self.rank[rootX] += 1
-25                return True
-26        
-27        uf = UnionFind(len(edges) + 1)
-28
-29        for x, y in edges:
-30            if not (uf.union(x, y)):
-31                return [x, y]
+23                    self.roots[y] = self.roots[x]
+24                    self.heights[x] += 1
+25                return False
+26            
+27        uf = UnionFind(len(edges))
+28        
+29        for a, b in edges:
+30            if uf.union(a, b):
+31                return [a, b]
