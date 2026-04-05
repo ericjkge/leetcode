e650ec22b2ed78d23349d456688fb1592ee24d1c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-05 10:31:02 -0400

    File updated at 4/5/2026, 10:31:02 AM

--- 217-ContainsDuplicate/217-ContainsDuplicate.py
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -1,11 +1,10 @@
-# Last updated: 2/7/2026, 1:48:18 PM
+# Last updated: 4/5/2026, 10:31:02 AM
 1class Solution:
 2    def containsDuplicate(self, nums: List[int]) -> bool:
 3        seen = set()
-4
-5        for num in nums:
-6            if num in seen:
-7                return True
-8            seen.add(num)
-9
-10        return False
+4        for n in nums:
+5            if n in seen:
+6                return True
+7            seen.add(n)
+8        
+9        return False
