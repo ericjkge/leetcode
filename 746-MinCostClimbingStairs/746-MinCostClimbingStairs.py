aec1efe09feb615c0e961fd28cee648551a2690b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-06 23:24:24 -0400

    File updated at 4/6/2026, 11:24:24 PM

--- 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
+++ 746-MinCostClimbingStairs/746-MinCostClimbingStairs.py
@@ -1,11 +1,11 @@
-# Last updated: 2/8/2026, 4:52:06 PM
+# Last updated: 4/6/2026, 11:24:24 PM
 1class Solution:
 2    def minCostClimbingStairs(self, cost: List[int]) -> int:
 3        @cache
 4        def dp(i):
 5            if i >= len(cost):
 6                return 0
-7
-8            return cost[i] + min(dp(i + 1), dp (i + 2))
+7            
+8            return cost[i] + min(dp(i + 1), dp(i + 2))
 9        
 10        return min(dp(0), dp(1))
