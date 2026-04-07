9da264f913672a0a102898262f4f27806ece3f2f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-06 23:49:06 -0400

    File updated at 4/6/2026, 11:49:06 PM

--- 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
+++ 238-ProductofArrayExceptSelf/238-ProductofArrayExceptSelf.py
@@ -1,20 +1,22 @@
-# Last updated: 2/9/2026, 4:43:47 PM
+# Last updated: 4/6/2026, 11:49:06 PM
 1class Solution:
 2    def productExceptSelf(self, nums: List[int]) -> List[int]:
-3        prefix = [nums[0]]
-4        for num in nums[1:]:
-5            prefix.append(prefix[-1] * num)
+3        prefix = [1]
+4        for n in nums:
+5            prefix.append(prefix[-1] * n)
 6        
-7        suffix = [nums[-1]]
-8        for j in range(len(nums) - 2, -1, -1):
-9            suffix.append(suffix[-1] * nums[j])
+7        suffix = [1]
+8        for i in range(len(nums) - 1, -1, -1):
+9            suffix.append(suffix[-1] * nums[i])
 10        suffix = suffix[::-1]
 11
-12        answer = [0] * len(nums)
-13
-14        for i in range(len(nums)):
-15            left = prefix[i - 1] if i - 1 >= 0 else 1
-16            right = suffix[i + 1] if i + 1 < len(nums) else 1
-17            answer[i] = left * right
-18        
-19        return answer
+12        prefix = prefix[1:]
+13        suffix = suffix[:-1]
+14
+15        ans = [0] * len(nums)
+16        for i in range(len(nums)):
+17            left = prefix[i - 1] if i > 0 else 1
+18            right = suffix[i + 1] if i < len(nums) - 1 else 1
+19            ans[i] = left * right
+20
+21        return ans
