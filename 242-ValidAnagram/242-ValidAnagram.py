84e10a88773ab97ca04909291388a183842add79
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-08 10:44:59 -0400

    File updated at 4/8/2026, 10:44:59 AM

--- 242-ValidAnagram/242-ValidAnagram.py
+++ 242-ValidAnagram/242-ValidAnagram.py
@@ -1,4 +1,4 @@
-# Last updated: 12/30/2025, 11:31:12 PM
+# Last updated: 4/8/2026, 10:44:59 AM
 1class Solution:
 2    def isAnagram(self, s: str, t: str) -> bool:
 3        return Counter(s) == Counter(t)
