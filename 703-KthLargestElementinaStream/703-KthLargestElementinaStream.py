9cff92e6b732ac0fa33ed3a610ab082f736ebd7a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-14 10:05:11 -0400

    File updated at 4/14/2026, 10:05:11 AM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
@@ -1,16 +1,16 @@
-# Last updated: 2/11/2026, 3:20:28 PM
+# Last updated: 4/14/2026, 10:05:11 AM
 1class KthLargest:
 2
 3    def __init__(self, k: int, nums: List[int]):
-4        self.heap = nums
-5        heapq.heapify(self.heap)
-6        self.k = k
-7        while len(self.heap) > self.k:
+4        self.k = k
+5        self.heap = nums
+6        heapq.heapify(self.heap)
+7        while len(self.heap) > k:
 8            heapq.heappop(self.heap)
 9
 10    def add(self, val: int) -> int:
 11        heapq.heappush(self.heap, val)
-12        while len(self.heap) > self.k:
+12        if len(self.heap) > self.k:
 13            heapq.heappop(self.heap)
 14        return self.heap[0]
 15
