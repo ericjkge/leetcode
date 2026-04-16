6248988541e811fd4238970ba522d2977370ca4c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-15 20:53:34 -0400

    File updated at 4/15/2026, 8:53:34 PM

--- 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.py
@@ -1,22 +1,21 @@
-# Last updated: 2/13/2026, 9:54:17 AM
+# Last updated: 4/15/2026, 8:53:34 PM
 1class Solution:
 2    def findDuplicate(self, nums: List[int]) -> int:
-3        def counter(target):
-4            count = 0
+3        def num_below(target):
+4            counter = 0
 5            for num in nums:
 6                if num <= target:
-7                    count += 1
-8            return count
-9        
-10        left, right = 1, max(nums)
-11
-12        while left + 1 < right:
-13            mid = (left + right) // 2
-14            if counter(mid) > mid:
-15                right = mid
-16            else:
-17                left = mid
-18        
-19        if counter(left) > left:
-20            return left
-21        return right
+7                    counter += 1
+8            return counter
+9
+10        left, right = 0, len(nums)
+11        while left + 1 < right:
+12            mid = (left + right) // 2
+13            if num_below(mid) > mid:
+14                right = mid
+15            else:
+16                left = mid
+17        
+18        if num_below(left) > left:
+19            return left
+20        return right
