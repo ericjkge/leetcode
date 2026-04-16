049652dfeca726085795b23b7ebe075c11a1a6a0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-15 21:03:07 -0400

    File updated at 4/15/2026, 9:03:06 PM

--- 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
+++ 271-EncodeandDecodeStrings/271-EncodeandDecodeStrings.py
@@ -1,35 +1,30 @@
-# Last updated: 2/12/2026, 1:21:01 PM
+# Last updated: 4/15/2026, 9:03:06 PM
 1class Codec:
 2    def encode(self, strs: List[str]) -> str:
 3        """Encodes a list of strings to a single string.
 4        """
 5        encoding = []
-6
-7        for s in strs:
-8            encoding.append(str(len(s)) + "#" + s)
+6        for s in strs:
+7            encoding.append(str(len(s)) + "#" + s)
+8        return "".join(encoding)
 9
-10        return "".join(encoding)
-11
-12    5#63/Rc1#h13#BmI3FS~J9#vmk
-13
-14    def decode(self, s: str) -> List[str]:
-15        """Decodes a single string to a list of strings.
-16        """
-17        decoding = []
-18        i = 0
-19
-20        while i < len(s):
-21            start = i
-22            while s[i] != "#":
-23                i += 1
-24            length = int(s[start:i])
-25            string = s[i + 1: i + 1 + length]
-26            i = i + 1 + length
-27            decoding.append(string)
-28        
-29        return decoding
-30
-31
-32# Your Codec object will be instantiated and called as such:
-33# codec = Codec()
-34# codec.decode(codec.encode(strs))
+10    def decode(self, s: str) -> List[str]:
+11        """Decodes a single string to a list of strings.
+12        """
+13        decoding = []
+14        index = 0
+15        while index < len(s):
+16            length = 0
+17            while s[index].isalnum():
+18                length *= 10
+19                length += int(s[index])
+20                index += 1
+21            index += 1 # Skip "#"
+22            decoding.append(s[index:index + length])
+23            index = index + length
+24        return decoding
+25
+26
+27# Your Codec object will be instantiated and called as such:
+28# codec = Codec()
+29# codec.decode(codec.encode(strs))
