896f84f84e8ae7e258c48d61b0567514174c069e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-23 09:51:19 -0400

    File updated at 4/23/2026, 9:51:18 AM

--- 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.py
+++ 297-SerializeandDeserializeBinaryTree/297-SerializeandDeserializeBinaryTree.py
@@ -1,4 +1,4 @@
-# Last updated: 2/14/2026, 6:22:12 PM
+# Last updated: 4/23/2026, 9:51:18 AM
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, x):
@@ -14,45 +14,48 @@
 13        :type root: TreeNode
 14        :rtype: str
 15        """
-16        serialization = []
-17
-18        queue = deque([root])
-19        while queue:
-20            node = queue.popleft()
-21
-22            if not node:
-23                serialization.append("null")
-24                continue
-25
-26            serialization.append(str(node.val))
-27            queue.append(node.left)
-28            queue.append(node.right)
-29        
-30        return ",".join(serialization)
-31
-32    def deserialize(self, data):
-33        """Decodes your encoded data to tree.
-34        
-35        :type data: str
-36        :rtype: TreeNode
-37        """
-38        vals = data.split(",")
-39        nodes = [TreeNode(int(v)) if v != "null" else None for v in vals]
-40        slow = 0
-41        fast = 1
-42
-43        while fast < len(nodes):
-44            nodes[slow].left = nodes[fast]
-45            fast += 1
-46            nodes[slow].right = nodes[fast]
-47            slow += 1
-48            while slow < fast and nodes[slow] is None:
-49                slow += 1
-50            fast += 1
-51
-52        return nodes[0]
-53
-54# Your Codec object will be instantiated and called as such:
-55# ser = Codec()
-56# deser = Codec()
-57# ans = deser.deserialize(ser.serialize(root))
+16        if not root:
+17            return ""
+18
+19        serial = []
+20        queue = deque([root])
+21        while queue:
+22            node = queue.popleft()
+23            if not node:
+24                serial.append("null")
+25                continue
+26
+27            serial.append(str(node.val))
+28            queue.append(node.left)
+29            queue.append(node.right)
+30        
+31        return ",".join(serial)
+32
+33    def deserialize(self, data):
+34        """Decodes your encoded data to tree.
+35        
+36        :type data: str
+37        :rtype: TreeNode
+38        """
+39        if not data:
+40            return None
+41
+42        lst = data.split(",")
+43        tree = [TreeNode(val) if val != "null" else None for val in lst]
+44
+45        p1, p2 = 0, 1
+46        while p2 < len(tree):
+47            tree[p1].left = tree[p2]
+48            p2 += 1
+49            tree[p1].right = tree[p2]
+50            p2 += 1
+51            p1 += 1
+52            while p1 < len(tree) and tree[p1] == None:
+53                p1 += 1
+54        
+55        return tree[0]
+56
+57# Your Codec object will be instantiated and called as such:
+58# ser = Codec()
+59# deser = Codec()
+60# ans = deser.deserialize(ser.serialize(root))
