cb6c693332fbd44c6ca5a06cd9fa7150cd9840b9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-23 09:16:05 -0400

    File updated at 4/23/2026, 9:16:04 AM

--- 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
+++ 295-FindMedianfromDataStream/295-FindMedianfromDataStream.py
@@ -1,4 +1,4 @@
-# Last updated: 2/14/2026, 5:59:11 PM
+# Last updated: 4/23/2026, 9:16:04 AM
 1class MedianFinder:
 2
 3    def __init__(self):
@@ -7,18 +7,19 @@
 6
 7    def addNum(self, num: int) -> None:
 8        heapq.heappush(self.lower, -num)
-9        heapq.heappush(self.upper, -heapq.heappop(self.lower))
-10
-11        if len(self.lower) + 1 < len(self.upper):
-12            heapq.heappush(self.lower, -heapq.heappop(self.upper))
-13
-14    def findMedian(self) -> float:
-15        if len(self.lower) == len(self.upper):
-16            return (-self.lower[0] + self.upper[0]) / 2
-17        return self.upper[0]
-18
-19
-20# Your MedianFinder object will be instantiated and called as such:
-21# obj = MedianFinder()
-22# obj.addNum(num)
-23# param_2 = obj.findMedian()
+9
+10        heapq.heappush(self.upper, -heapq.heappop(self.lower))
+11
+12        if len(self.upper) > len(self.lower) + 1:
+13            heapq.heappush(self.lower, -heapq.heappop(self.upper))
+14
+15    def findMedian(self) -> float:
+16        # print(self.lower, self.upper)
+17        if len(self.lower) == len(self.upper):
+18            return (-self.lower[0] + self.upper[0]) / 2
+19        return self.upper[0]
+20
+21# Your MedianFinder object will be instantiated and called as such:
+22# obj = MedianFinder()
+23# obj.addNum(num)
+24# param_2 = obj.findMedian()
