a0597b687953127884a817ebae42e5af3c14050f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-25 13:04:22 -0400

    File updated at 4/25/2026, 1:04:22 PM

--- 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
+++ 323-NumberofConnectedComponentsinanUndirectedGraph/323-NumberofConnectedComponentsinanUndirectedGraph.py
@@ -1,4 +1,4 @@
-# Last updated: 2/15/2026, 12:37:22 PM
+# Last updated: 4/25/2026, 1:04:22 PM
 1class Solution:
 2    def countComponents(self, n: int, edges: List[List[int]]) -> int:
 3        graph = defaultdict(list)
@@ -14,11 +14,10 @@
 13                    dfs(neighbor)
 14        
 15        seen = set()
-16        counter = 0
-17
-18        for node in range(n):
-19            if node not in seen:
-20                counter += 1
-21                dfs(node)
-22        
-23        return counter
+16        count = 0
+17        for i in range(n):
+18            if i not in seen:
+19                dfs(i)
+20                count += 1
+21        
+22        return count
