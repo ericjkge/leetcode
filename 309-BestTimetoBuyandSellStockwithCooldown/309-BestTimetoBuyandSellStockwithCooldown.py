1a444d3ccd446ef6f094b04e750572d7bbe31a1a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-24 22:17:06 -0400

    File updated at 4/24/2026, 10:17:05 PM

--- 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.py
+++ 309-BestTimetoBuyandSellStockwithCooldown/309-BestTimetoBuyandSellStockwithCooldown.py
@@ -1,17 +1,16 @@
-# Last updated: 2/15/2026, 1:03:54 PM
+# Last updated: 4/24/2026, 10:17:05 PM
 1class Solution:
 2    def maxProfit(self, prices: List[int]) -> int:
 3        @cache
-4        def dp(i, j): # day, status (0 resting, 1 holding, 2 cooldown)
+4        def dp(i, j): # j = 0 (nothing), 1 (holding), 2 (cooldown)
 5            if i == len(prices):
 6                return 0
 7
 8            if j == 0:
 9                return max(dp(i + 1, 0), -prices[i] + dp(i + 1, 1))
-10            
-11            if j == 1:
-12                return max(prices[i] + dp(i + 1, 2), dp(i + 1, 1))
-13            
-14            return dp(i + 1, 0)
-15
-16        return max(dp(i, 0) for i in range(len(prices)))
+10            elif j == 1:
+11                return max(dp(i + 1, 1), prices[i] + dp(i + 1, 2))
+12            else:
+13                return dp(i + 1, 0)
+14        
+15        return dp(0, 0)
