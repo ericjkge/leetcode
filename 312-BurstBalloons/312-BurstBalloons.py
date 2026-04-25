dd5f63ac592cfdac8597601a7dc6a7658a8183e0
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-04-24 23:13:22 -0400

    File updated at 4/24/2026, 11:13:22 PM

--- 312-BurstBalloons/312-BurstBalloons.py
+++ 312-BurstBalloons/312-BurstBalloons.py
@@ -1,18 +1,17 @@
-# Last updated: 2/22/2026, 9:03:46 AM
+# Last updated: 4/24/2026, 11:13:22 PM
 1class Solution:
 2    def maxCoins(self, nums: List[int]) -> int:
 3        arr = [1] + nums + [1]
-4        n = len(arr)
-5
-6        @cache
-7        def dp(i, j):
-8            if i + 1 == j:
-9                return 0
-10            
-11            best = 0
-12            for k in range(i + 1, j):
-13                best = max(best, arr[i] * arr[k] * arr[j] + dp(i, k) + dp(k, j))
-14            return best
+4
+5        @cache
+6        def dp(i, j):
+7            if i + 1 == j:
+8                return 0
+9            
+10            coins = 0
+11            for k in range(i + 1, j):
+12                coins = max(coins, arr[i] * arr[k] * arr[j] + dp(i, k) + dp(k, j))
+13
+14            return coins
 15
-16        return dp(0, n - 1)
-17
+16        return dp(0, len(arr) - 1)
