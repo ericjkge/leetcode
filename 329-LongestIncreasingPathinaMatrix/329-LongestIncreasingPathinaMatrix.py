4d321824b7281c8ae355ddc2c3fed44eee6ad3f6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-03 10:44:14 -0400

    File updated at 5/3/2026, 10:44:14 AM

--- 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.py
+++ 329-LongestIncreasingPathinaMatrix/329-LongestIncreasingPathinaMatrix.py
@@ -1,23 +1,20 @@
-# Last updated: 4/25/2026, 1:15:29 PM
+# Last updated: 5/3/2026, 10:44:14 AM
 1class Solution:
 2    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
 3        rows, cols = len(matrix), len(matrix[0])
 4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-5        
+5
 6        @cache
 7        def dp(r, c):
-8            longest = 1
+8            path = 1
 9            for dr, dc in directions:
 10                nr, nc = r + dr, c + dc
-11                if 0 <= nr < rows and 0 <= nc < cols and matrix[r][c] < matrix[nr][nc]:
-12                    longest = max(longest, 1 + dp(nr, nc))
-13            
-14            return longest
-15        
-16        ans = 0
-17        for r in range(rows):
-18            for c in range(cols):
-19                ans = max(ans, dp(r, c))
-20        
-21        return ans
-22
+11                if 0 <= nr < rows and 0 <= nc < cols and matrix[nr][nc] > matrix[r][c]:
+12                    path = max(path, 1 + dp(nr, nc))
+13            return path
+14        
+15        longest = 0
+16        for r in range(rows):
+17            for c in range(cols):
+18                longest = max(longest, dp(r, c))
+19        return longest
