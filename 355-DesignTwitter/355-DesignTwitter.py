ede66920796a930c37c3a3d54c8aace0657d8d98
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-03 11:26:10 -0400

    File updated at 5/3/2026, 11:26:10 AM

--- 355-DesignTwitter/355-DesignTwitter.py
+++ 355-DesignTwitter/355-DesignTwitter.py
@@ -1,46 +1,44 @@
-# Last updated: 2/16/2026, 1:09:36 PM
+# Last updated: 5/3/2026, 11:26:10 AM
 1class Twitter:
 2
 3    def __init__(self):
-4        self.tweets = defaultdict(list) # userId: [(t1, tweet1), (t2, tweet2), ...]
-5        self.following = defaultdict(list)
-6        self.followers = defaultdict(list)
-7        self.time = 0
-8        
-9    def postTweet(self, userId: int, tweetId: int) -> None:
-10        self.time += 1
-11        self.tweets[userId].append((self.time, tweetId))
-12
-13    def getNewsFeed(self, userId: int) -> List[int]:
-14        relevant_tweets = []
-15        for user, lst in self.tweets.items():
-16            if user in self.following[userId] or user == userId:
-17                relevant_tweets.extend(lst[-10:])
-18
-19        relevant_tweets.sort(reverse=True)
-20        feed = []
-21        for k, v in relevant_tweets:
-22            feed.append(v)
-23        return feed[:10]
+4        self.users = defaultdict(list) # [user1: [following]]
+5        self.tweets = defaultdict(list)
+6        self.time = 0
+7
+8    def postTweet(self, userId: int, tweetId: int) -> None:
+9        self.tweets[userId].append((self.time, tweetId))
+10        self.time -= 1
+11
+12    def getNewsFeed(self, userId: int) -> List[int]:
+13        relevant = self.tweets[userId][-10:]
+14        for followeeId in self.users[userId]:
+15            relevant.extend(self.tweets[followeeId][-10:])
+16
+17        heapq.heapify(relevant)
+18        feed = []
+19        for _ in range(min(10, len(relevant))):
+20            _, tweetId = heapq.heappop(relevant)            
+21            feed.append(tweetId)
+22
+23        return feed
 24
 25    def follow(self, followerId: int, followeeId: int) -> None:
-26        if followerId == followeeId:
+26        if followeeId in self.users[followerId]:
 27            return
-28
-29        self.following[followerId].append(followeeId)
-30        self.followers[followeeId].append(followerId)
-31
-32    def unfollow(self, followerId: int, followeeId: int) -> None:
-33        if followeeId not in self.following[followerId]:
-34            return
-35
-36        self.following[followerId].remove(followeeId)
-37        self.followers[followeeId].remove(followerId)
-38
-39
-40# Your Twitter object will be instantiated and called as such:
-41# obj = Twitter()
-42# obj.postTweet(userId,tweetId)
-43# param_2 = obj.getNewsFeed(userId)
-44# obj.follow(followerId,followeeId)
-45# obj.unfollow(followerId,followeeId)
+28            
+29        self.users[followerId].append(followeeId)
+30
+31    def unfollow(self, followerId: int, followeeId: int) -> None:
+32        if followeeId not in self.users[followerId]:
+33            return 
+34        
+35        self.users[followerId].remove(followeeId)
+36
+37
+38# Your Twitter object will be instantiated and called as such:
+39# obj = Twitter()
+40# obj.postTweet(userId,tweetId)
+41# param_2 = obj.getNewsFeed(userId)
+42# obj.follow(followerId,followeeId)
+43# obj.unfollow(followerId,followeeId)
