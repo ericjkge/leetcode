0e68f69f55e11da8ef184906f80a16458d54913a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-03 11:02:52 -0400

    File updated at 5/3/2026, 11:02:52 AM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,4 +1,4 @@
-# Last updated: 5/3/2026, 11:02:40 AM
+# Last updated: 5/3/2026, 11:02:52 AM
 1class Solution:
 2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 3        counts = [(-val, key) for key, val in Counter(nums).items()]
@@ -6,7 +6,7 @@
 5
 6        ans = []
 7        for _ in range(k):
-8            val, key = heapq.heappop(counts)
+8            _, key = heapq.heappop(counts)
 9            ans.append(key)
 10        
 11        return ans
