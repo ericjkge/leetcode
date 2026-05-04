26b868356560930f1f95a4c8493ca60e7c6fc6e7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-04 12:37:59 -0400

    File updated at 5/4/2026, 12:37:58 PM

--- 853-CarFleet/853-CarFleet.py
+++ 853-CarFleet/853-CarFleet.py
@@ -1,14 +1,12 @@
-# Last updated: 2/17/2026, 9:39:38 AM
+# Last updated: 5/4/2026, 12:37:58 PM
 1class Solution:
 2    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-3        pairs = sorted(zip(position, speed), reverse=True)
-4        times = []
+3        pairs = sorted([(p, s) for p, s in zip(position, speed)], reverse=True)
+4        stack = []
 5
-6        for position, speed in pairs:
-7            distance = target - position
-8            time = distance / speed
-9            if times and time <= times[-1]:
-10                continue
-11            times.append(time)
-12
-13        return len(times)
+6        for p, s in pairs:
+7            time = (target - p) / s
+8            if not stack or time > stack[-1]:
+9                stack.append(time)
+10
+11        return len(stack)
