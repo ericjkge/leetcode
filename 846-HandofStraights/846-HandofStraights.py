3f686cffbc9e07e39cc051df662f05828a565372
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-04 12:15:30 -0400

    File updated at 5/4/2026, 12:15:29 PM

--- 846-HandofStraights/846-HandofStraights.py
+++ 846-HandofStraights/846-HandofStraights.py
@@ -1,18 +1,17 @@
-# Last updated: 2/17/2026, 8:59:32 AM
+# Last updated: 5/4/2026, 12:15:29 PM
 1class Solution:
 2    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
 3        if len(hand) % groupSize != 0:
 4            return False
 5        
 6        freqs = Counter(hand)
-7        keys = list(freqs.keys())
-8        keys.sort()
-9
-10        for key in keys:
-11            while freqs[key] != 0:
-12                for i in range(groupSize):
-13                    if freqs[key + i] == 0:
-14                        return False
-15                    freqs[key + i] -= 1
-16        
-17        return True
+7        keys = sorted(freqs.keys())
+8
+9        for key in keys:
+10            while freqs[key] != 0:
+11                for i in range(groupSize):
+12                    if freqs[key + i] == 0:
+13                        return False
+14                    freqs[key + i] -= 1
+15        
+16        return True
