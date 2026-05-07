53367f5972e712de62b4ecdbb2913ef9f3e06fcf
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-07 13:18:09 -0400

    File updated at 5/7/2026, 1:18:09 PM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,31 +1,28 @@
-# Last updated: 2/18/2026, 1:48:56 PM
+# Last updated: 5/7/2026, 1:18:09 PM
 1class Solution:
 2    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-3        result = []
-4        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-5        rows, cols = len(heights), len(heights[0])
-6
-7        def dfs(r, c, s):
-8            s.add((r, c))
-9            for dr, dc in directions:
-10                nr, nc = r + dr, c + dc
-11                if 0 <= nr < rows and 0 <= nc < cols and heights[r][c] <= heights[nr][nc] and (nr, nc) not in s:
-12                    dfs(nr, nc, s) 
-13        
-14        pacific = set()
-15        atlantic = set()
-16
+3        rows, cols = len(heights), len(heights[0])
+4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+5
+6        def dfs(r, c, s):
+7            s.add((r, c))
+8            for dr, dc in directions:
+9                nr, nc = r + dr, c + dc
+10                if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in s:
+11                    dfs(nr, nc, s)
+12        
+13        pacific = set()
+14        for r in range(rows):
+15            dfs(r, 0, pacific)
+16        
 17        for c in range(cols):
 18            dfs(0, c, pacific)
 19        
-20        for r in range(rows):
-21            dfs(r, 0, pacific)
-22        
-23        for c in range(cols):
-24            dfs(rows - 1, c, atlantic)
-25        
-26        for r in range(rows):
-27            dfs(r, cols - 1, atlantic)
-28
-29        both = pacific & atlantic
-30        return [[r, c] for r, c in both]
+20        atlantic = set()
+21        for r in range(rows):
+22            dfs(r, cols - 1, atlantic)
+23        
+24        for c in range(cols):
+25            dfs(rows - 1, c, atlantic)
+26        
+27        return [[r, c] for r, c in pacific & atlantic]
