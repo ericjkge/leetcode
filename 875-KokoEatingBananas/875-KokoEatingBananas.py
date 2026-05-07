14f464fce877ab996ba1fc922edea6fc1aec9dc6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-07 13:04:32 -0400

    File updated at 5/7/2026, 1:04:31 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,20 +1,20 @@
-# Last updated: 2/17/2026, 9:09:07 AM
+# Last updated: 5/7/2026, 1:04:31 PM
 1class Solution:
 2    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-3        def canFinish(speed):
+3        def canEat(speed):
 4            time = 0
 5            for pile in piles:
-6                time += -(-pile // speed)
+6                time += (pile + speed - 1) // speed
 7            return time <= h
-8
+8        
 9        left, right = 1, max(piles)
 10        while left + 1 < right:
 11            mid = (left + right) // 2
-12            if canFinish(mid):
+12            if canEat(mid):
 13                right = mid
 14            else:
 15                left = mid
 16        
-17        if canFinish(left):
+17        if canEat(left):
 18            return left
 19        return right
