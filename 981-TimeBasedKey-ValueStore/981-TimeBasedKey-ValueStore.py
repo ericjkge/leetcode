97602c8ac716d438d8670f3e658262341b396a18
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-09 10:29:51 -0400

    File updated at 5/9/2026, 10:29:51 AM

--- 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
+++ 981-TimeBasedKey-ValueStore/981-TimeBasedKey-ValueStore.py
@@ -1,30 +1,31 @@
-# Last updated: 2/19/2026, 9:53:35 AM
+# Last updated: 5/9/2026, 10:29:51 AM
 1class TimeMap:
 2
 3    def __init__(self):
-4        self.storage = defaultdict(list) # k : [(v1, t1), (v2, t2), ...]
+4        self.dict = defaultdict(list)
 5
 6    def set(self, key: str, value: str, timestamp: int) -> None:
-7        self.storage[key].append((value, timestamp))
+7        self.dict[key].append((timestamp, value))
 8
 9    def get(self, key: str, timestamp: int) -> str:
-10        if key not in self.storage or self.storage[key][0][1] > timestamp:
+10        if key not in self.dict or self.dict[key][0][0] > timestamp:
 11            return ""
-12
-13        left, right = 0, len(self.storage[key]) - 1
-14        while left + 1 < right:
-15            mid = (left + right) // 2
-16            if self.storage[key][mid][1] > timestamp:
-17                right = mid
-18            else:
-19                left = mid
-20        
-21        if self.storage[key][right][1] <= timestamp:
-22            return self.storage[key][right][0]
-23        return self.storage[key][left][0]
-24
+12        
+13        relevant = self.dict[key]
+14        left, right = 0, len(relevant) - 1
+15        while left + 1 < right:
+16            mid = (left + right) // 2
+17            if relevant[mid][0] > timestamp:
+18                right = mid
+19            else:
+20                left = mid
+21        
+22        if relevant[right][0] <= timestamp:
+23            return relevant[right][1]
+24        return relevant[left][1]
 25
-26# Your TimeMap object will be instantiated and called as such:
-27# obj = TimeMap()
-28# obj.set(key,value,timestamp)
-29# param_2 = obj.get(key,timestamp)
+26
+27# Your TimeMap object will be instantiated and called as such:
+28# obj = TimeMap()
+29# obj.set(key,value,timestamp)
+30# param_2 = obj.get(key,timestamp)
