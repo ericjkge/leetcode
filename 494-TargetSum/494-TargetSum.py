553262eda596856a65e3dc73ab199a8b49173024
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-09 10:13:33 -0400

    File updated at 5/9/2026, 10:13:33 AM

--- 494-TargetSum/494-TargetSum.py
+++ 494-TargetSum/494-TargetSum.py
@@ -1,14 +1,14 @@
-# Last updated: 2/19/2026, 9:25:47 AM
+# Last updated: 5/9/2026, 10:13:33 AM
 1class Solution:
 2    def findTargetSumWays(self, nums: List[int], target: int) -> int:
 3        @cache
 4        def dp(i, j):
-5            if i == len(nums):
-6                if j == target:
-7                    return 1
-8                return 0
-9            
-10            return dp(i + 1, j + nums[i]) + dp(i + 1, j - nums[i])
-11        
-12        return dp(0, 0)
-13
+5            if i == len(nums) and j == target:
+6                return 1
+7            
+8            if i == len(nums):
+9                return 0
+10            
+11            return dp(i + 1, j - nums[i]) + dp(i + 1, j + nums[i])
+12        
+13        return dp(0, 0)
