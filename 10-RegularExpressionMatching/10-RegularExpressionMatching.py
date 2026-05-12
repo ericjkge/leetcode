169cd6bd93ecf8707fe7d0b03ba20eaad7d144f3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-12 09:36:09 -0400

    File updated at 5/12/2026, 9:36:09 AM

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.py
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.py
@@ -1,16 +1,15 @@
-# Last updated: 5/11/2026, 10:05:01 AM
+# Last updated: 5/12/2026, 9:36:09 AM
 1class Solution:
 2    def isMatch(self, s: str, p: str) -> bool:
 3        @cache
 4        def dp(i, j):
 5            if j == len(p):
 6                return i == len(s)
-7            
+7
 8            match = i < len(s) and (s[i] == p[j] or p[j] == ".")
-9
-10            if j + 1 < len(p) and p[j + 1] == "*":
-11                return dp(i, j + 2) or (match and dp(i + 1, j))
-12
-13            return match and dp(i + 1, j + 1)
-14
-15        return dp(0, 0)
+9            if j + 1 < len(p) and p[j + 1] == "*":
+10                return dp(i, j + 2) or (match and dp(i + 1, j))
+11            
+12            return match and dp(i + 1, j + 1)
+13        
+14        return dp(0, 0)
