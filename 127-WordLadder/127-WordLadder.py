358f4db9fdd5535d93945be2993ce6f3e661c2a6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-18 16:01:12 -0400

    File updated at 5/18/2026, 4:01:12 PM

--- 127-WordLadder/127-WordLadder.py
+++ 127-WordLadder/127-WordLadder.py
@@ -1,38 +1,31 @@
-# Last updated: 2/25/2026, 4:29:26 PM
+# Last updated: 5/18/2026, 4:01:12 PM
 1class Solution:
 2    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-3        if endWord not in wordList:
-4            return 0
-5        
-6        patterns = defaultdict(list)
-7        for word in wordList:
-8            for i in range(len(word)):
-9                patterns[word[:i] + "*" + word[i + 1:]].append(word)
-10        
-11        def neighbors(word):
-12            neighbors = []
-13            for i in range(len(word)):
-14                neighbors.append(word[:i] + "*" + word[i + 1:])
-15            return neighbors
+3        patterns = defaultdict(list) # [hit:[*it, h*t, hi*], dot:[*ot, d*t, do*], ...]
+4
+5        for word in wordList:
+6            for i in range(len(word)):
+7                patterns[word[:i] + "*" + word[i + 1:]].append(word)
+8    
+9        def neighbors(word):
+10            lst = []
+11            for i in range(len(word)):
+12                neighbor = word[:i] + "*" + word[i + 1:]
+13                if neighbor in patterns:
+14                    lst.extend(patterns[neighbor])
+15            return set(lst)
 16        
 17        queue = deque([beginWord])
 18        seen = set()
-19        counter = 1
-20        
-21        while queue:
-22            for _ in range(len(queue)):
-23                word = queue.popleft()
-24
-25                if word == endWord:
-26                    return counter
-27
-28                for neighbor in neighbors(word):
-29                    for option in patterns[neighbor]:
-30                        if option not in seen:
-31                            seen.add(option)
-32                            queue.append(option)
-33            
-34            counter += 1
-35
-36        return 0
-37        
+19        counter = 0
+20        while queue:
+21            for _ in range(len(queue)):
+22                word = queue.popleft()
+23                if word == endWord:
+24                    return counter + 1
+25                for neighbor in neighbors(word):
+26                    if neighbor not in seen:
+27                        seen.add(neighbor)
+28                        queue.append(neighbor)
+29            counter += 1
+30        return 0
