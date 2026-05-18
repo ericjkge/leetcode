3a8b0337581b2d924e76d0512d96b4a08669a25e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-18 16:37:09 -0400

    File updated at 5/18/2026, 4:37:08 PM

--- 269-AlienDictionary/269-AlienDictionary.py
+++ 269-AlienDictionary/269-AlienDictionary.py
@@ -1,4 +1,4 @@
-# Last updated: 4/27/2026, 11:37:28 AM
+# Last updated: 5/18/2026, 4:37:08 PM
 1class Solution:
 2    def alienOrder(self, words: List[str]) -> str:
 3        chars = set([c for word in words for c in word])
@@ -8,23 +8,24 @@
 7        for i in range(len(words) - 1):
 8            if len(words[i]) > len(words[i + 1]) and words[i].startswith(words[i + 1]):
 9                return ""
-10            for j in range(min(len(words[i]), len(words[i + 1]))):
-11                if words[i][j] != words[i + 1][j]:
-12                    graph[words[i][j]].append(words[i + 1][j])
-13                    indegrees[words[i + 1][j]] += 1
-14                    break
-15
-16        queue = deque([c for c in indegrees if indegrees[c] == 0])
-17        order = []
-18
-19        while queue:
-20            node = queue.popleft()
-21            order.append(node)
-22            for neighbor in graph[node]:
-23                indegrees[neighbor] -= 1
-24                if indegrees[neighbor] == 0:
-25                    queue.append(neighbor)
-26
-27        if len(order) == len(chars):
-28            return "".join(order)
-29        return ""
+10
+11            for j in range(min(len(words[i]), len(words[i + 1]))):
+12                if words[i][j] != words[i + 1][j]:
+13                    graph[words[i][j]].append(words[i + 1][j])
+14                    indegrees[words[i + 1][j]] += 1
+15                    break
+16
+17        queue = deque([c for c in indegrees if indegrees[c] == 0])
+18        order = []
+19
+20        while queue:
+21            node = queue.popleft()
+22            order.append(node)
+23            for neighbor in graph[node]:
+24                indegrees[neighbor] -= 1
+25                if indegrees[neighbor] == 0:
+26                    queue.append(neighbor)
+27        
+28        if len(order) == len(chars):
+29            return "".join(order)
+30        return ""
