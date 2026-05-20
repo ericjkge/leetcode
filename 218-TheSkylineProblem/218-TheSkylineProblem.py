f662337286995f91bafbda26ef858dfd01d53dfe
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-20 11:01:49 -0400

    File updated at 5/20/2026, 11:01:49 AM

--- 218-TheSkylineProblem/218-TheSkylineProblem.py
+++ 218-TheSkylineProblem/218-TheSkylineProblem.py
@@ -1,4 +1,4 @@
-# Last updated: 5/20/2026, 10:59:04 AM
+# Last updated: 5/20/2026, 11:01:49 AM
 1class Solution:
 2    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
 3        events = []
@@ -10,20 +10,20 @@
 9        events.sort()
 10
 11        heap = [(0, float("inf"))]
-12        prev = 0
-13        res = []
-14        
+12        res = []
+13        prev = 0
+14
 15        for x, h, r in events:
 16            if h < 0:
 17                heapq.heappush(heap, (h, r))
-18            while heap[0][1] <= x:
-19                heapq.heappop(heap)
-20            
-21            curr = -heap[0][0]
-22            if curr != prev:
-23                res.append([x, curr])
-24                prev = curr
-25
-26        return res
-27
-28        
+18            
+19            while heap[0][1] <= x:
+20                heapq.heappop(heap)
+21            
+22            curr = -heap[0][0]
+23            if curr != prev:
+24                res.append([x, curr])
+25                prev = curr
+26        
+27        return res
+28
