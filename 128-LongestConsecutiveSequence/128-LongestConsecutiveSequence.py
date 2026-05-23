4612b6f0ced1fcbaac396cc27a2498c109a237c3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-23 19:25:48 -0400

    File updated at 5/23/2026, 7:25:48 PM

--- 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
+++ 128-LongestConsecutiveSequence/128-LongestConsecutiveSequence.py
@@ -1,18 +1,23 @@
-# Last updated: 3/20/2026, 4:20:43 PM
+# Last updated: 5/23/2026, 7:25:48 PM
 1class Solution:
-2    def longestConsecutive(self, nums: List[int]) -> int:
-3        nset = set(nums)
-4        longest = 0
-5
-6        for num in nset:
-7            if num - 1 in nset:
-8                continue
-9            
-10            streak = 1
-11            while num + 1 in nset:
-12                num += 1
-13                streak += 1
-14            
-15            longest = max(longest, streak)
-16
-17        return longest
+2    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+3        """
+4        Do not return anything, modify nums1 in-place instead.
+5        """
+6        p1 = m - 1
+7        p2 = n - 1
+8        p3 = m + n - 1
+9
+10        while p1 >= 0 and p2 >= 0:
+11            if nums1[p1] > nums2[p2]:
+12                nums1[p3] = nums1[p1]
+13                p1 -= 1
+14            else:
+15                nums1[p3] = nums2[p2]
+16                p2 -= 1
+17            p3 -= 1
+18
+19        while p2 >= 0:
+20            nums1[p3] = nums2[p2]
+21            p2 -= 1
+22            p3 -= 1
