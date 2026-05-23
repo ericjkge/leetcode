f72ccd99e917a5840ecc7372171bba3980470953
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-23 19:10:24 -0400

    File updated at 5/23/2026, 7:10:23 PM

--- 875-KokoEatingBananas/875-KokoEatingBananas.py
+++ 875-KokoEatingBananas/875-KokoEatingBananas.py
@@ -1,20 +1,17 @@
-# Last updated: 5/7/2026, 1:04:31 PM
+# Last updated: 5/23/2026, 7:10:23 PM
 1class Solution:
-2    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-3        def canEat(speed):
-4            time = 0
-5            for pile in piles:
-6                time += (pile + speed - 1) // speed
-7            return time <= h
-8        
-9        left, right = 1, max(piles)
-10        while left + 1 < right:
-11            mid = (left + right) // 2
-12            if canEat(mid):
-13                right = mid
-14            else:
-15                left = mid
-16        
-17        if canEat(left):
-18            return left
-19        return right
+2    def longestConsecutive(self, nums: List[int]) -> int:
+3        nset = set(nums)
+4        longest = 0
+5
+6        for num in nset:
+7            if num - 1 in nset:
+8                continue
+9
+10            streak = 1
+11            while num + 1 in nset:
+12                num = num + 1
+13                streak += 1
+14            longest = max(longest, streak)
+15
+16        return longest
