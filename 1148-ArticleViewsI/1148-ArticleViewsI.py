460dece510829d2b38bb056df96a15765e0f8cea
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-26 10:56:48 -0400

    File updated at 5/26/2026, 7:56:48 AM

--- 1148-ArticleViewsI/1148-ArticleViewsI.py
+++ 1148-ArticleViewsI/1148-ArticleViewsI.py
@@ -1,13 +1,19 @@
-# Last updated: 5/26/2026, 7:53:14 AM
+# Last updated: 5/26/2026, 7:56:48 AM
 1class Solution:
 2    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-3        seta = set()
-4        setb = set()
-5        res = [0] * len(A)
-6
-7        for i in range(len(A)):
-8            seta.add(A[i])
-9            setb.add(B[i])
-10            res[i] = len(seta & setb)
-11        
-12        return res
+3        n = len(A)
+4        res = [0] * n
+5        count = defaultdict(int)
+6        common = 0
+7
+8        for i in range(n):
+9            count[A[i]] += 1
+10            if count[A[i]] == 2:
+11                common += 1
+12            count[B[i]] += 1
+13            if count[B[i]] == 2:
+14                common += 1
+15            res[i] = common
+16        
+17        return res
+18
