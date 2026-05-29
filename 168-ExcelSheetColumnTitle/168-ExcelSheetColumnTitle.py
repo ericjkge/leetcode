e13534850f4759a95354d9acb7be2e894bee54d5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-28 23:42:06 -0400

    File updated at 5/28/2026, 8:42:06 PM

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.py
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.py
@@ -1,32 +1,12 @@
-# Last updated: 5/28/2026, 8:23:00 PM
-1# Definition for singly-linked list.
-2# class ListNode:
-3#     def __init__(self, val=0, next=None):
-4#         self.val = val
-5#         self.next = next
-6class Solution:
-7    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-8        if not head:
-9            return head
-10            
-11        n = 0
-12        curr = head
-13        while curr:
-14            curr = curr.next
-15            n += 1
-16
-17        dummy = ListNode(next = head)
-18        slow = fast = head
-19        
-20        for _ in range(k % n):
-21            fast = fast.next
-22        
-23        while fast.next:
-24            slow = slow.next
-25            fast = fast.next
-26        
-27        fast.next = dummy.next
-28        dummy.next = slow.next
-29        slow.next = None
-30
-31        return dummy.next
+# Last updated: 5/28/2026, 8:42:06 PM
+1class Solution:
+2    def thirdMax(self, nums: List[int]) -> int:
+3        nums_set = set(nums)
+4        nums = list(nums_set)
+5        nums.sort(reverse=True)
+6
+7        if len(nums) < 3:
+8            return max(nums)
+9        
+10        return nums[2]
+11
