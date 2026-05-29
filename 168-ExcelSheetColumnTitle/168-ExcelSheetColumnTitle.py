42591a3bb21962aba561e000f53f77477ae660f9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-05-28 23:46:05 -0400

    File updated at 5/28/2026, 8:46:05 PM

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.py
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.py
@@ -1,12 +1,21 @@
-# Last updated: 5/28/2026, 8:42:06 PM
+# Last updated: 5/28/2026, 8:46:05 PM
 1class Solution:
 2    def thirdMax(self, nums: List[int]) -> int:
-3        nums_set = set(nums)
-4        nums = list(nums_set)
-5        nums.sort(reverse=True)
-6
-7        if len(nums) < 3:
-8            return max(nums)
-9        
-10        return nums[2]
-11
+3        first = second = third = float("-inf")
+4
+5        for num in nums:
+6            if num == first or num == second or num == third:
+7                continue
+8
+9            if num > first:
+10                third = second
+11                second = first
+12                first = num
+13            elif num > second:
+14                third = second
+15                second = num
+16            elif num > third:
+17                third = num
+18            
+19        return third if third != float("-inf") else first
+20
