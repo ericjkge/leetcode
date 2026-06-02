7b8024cf205c09a33a9cd7831c04ffa60a2e558a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-01 22:50:10 -0700

    File updated at 6/1/2026, 10:50:09 PM

--- 23-MergekSortedLists/23-MergekSortedLists.py
+++ 23-MergekSortedLists/23-MergekSortedLists.py
@@ -1,4 +1,4 @@
-# Last updated: 3/1/2026, 12:42:53 PM
+# Last updated: 6/1/2026, 10:50:09 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -8,29 +8,28 @@
 7    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
 8        if not lists:
 9            return None
-10
-11        def merge(p1, p2):
-12            dummy = curr = ListNode()
-13
-14            while p1 and p2:
-15                if p1.val < p2.val:
-16                    curr.next = p1
-17                    p1 = p1.next
+10            
+11        def merge(l1, l2):
+12            res = []
+13            dummy = curr = ListNode()
+14            while l1 and l2:
+15                if l1.val <= l2.val:
+16                    curr.next = l1
+17                    l1 = l1.next
 18                else:
-19                    curr.next = p2
-20                    p2 = p2.next
+19                    curr.next = l2
+20                    l2 = l2.next
 21                curr = curr.next
-22
-23            curr.next = p1 if p1 else p2
-24
-25            return dummy.next
-26
-27        while len(lists) > 1:
-28            merged = []
-29            for i in range(0, len(lists), 2):
-30                p1 = lists[i]
-31                p2 = lists[i + 1] if i + 1 < len(lists) else None
-32                merged.append(merge(p1, p2))
-33            lists = merged
-34
-35        return lists[0]
+22            curr.next = l1 if l1 else l2
+23            return dummy.next
+24        
+25        merged = []
+26        while len(lists) > 1:
+27            for i in range(0, len(lists), 2):
+28                l1 = lists[i]
+29                l2 = lists[i + 1] if i + 1 < len(lists) else None
+30                merged.append(merge(l1, l2))
+31            lists = merged
+32            merged = []
+33        
+34        return lists[0]
