ddc8e79645c832db31111829029dd4473cf6acad
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-03 21:38:58 -0700

    File updated at 6/3/2026, 9:38:58 PM

--- 3751-TotalWavinessofNumbersinRangeI/3751-TotalWavinessofNumbersinRangeI.py
+++ 3751-TotalWavinessofNumbersinRangeI/3751-TotalWavinessofNumbersinRangeI.py
@@ -1,11 +1,11 @@
-# Last updated: 6/3/2026, 9:38:49 PM
+# Last updated: 6/3/2026, 9:38:58 PM
 1class Solution:
 2    def totalWaviness(self, num1: int, num2: int) -> int:
 3        count = 0
 4
 5        for num in range(num1, num2 + 1):
-6            if num < 100:
-7                continue
+6            # if num < 100:
+7            #     continue
 8
 9            s = str(num)
 10            for i in range(1, len(s) - 1):
