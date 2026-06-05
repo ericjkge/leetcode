52d4945a084aedfd96605e3610a0889145ee1a17
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-04 21:35:01 -0700

    File updated at 6/4/2026, 9:35:01 PM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,4 +1,4 @@
-# Last updated: 2/26/2026, 9:00:47 AM
+# Last updated: 6/4/2026, 9:35:01 PM
 1class Solution(object):
 2    def twoSum(self, nums, target):
 3        """
@@ -11,5 +11,8 @@
 10        for i, num in enumerate(nums):
 11            complement = target - num
 12            if complement in mapping:
-13                return [mapping[complement], i]
-14            mapping[num] = i
+13                return (mapping[complement], i)
+14            
+15            mapping[num] = i
+16        
+17        return None
