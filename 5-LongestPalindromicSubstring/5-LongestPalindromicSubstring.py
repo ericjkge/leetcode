08341ae49598411323b6e23ca90302b7da7b87cb
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-06 00:27:12 -0700

    File updated at 6/6/2026, 12:27:12 AM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,24 +1,22 @@
-# Last updated: 2/26/2026, 9:46:40 AM
+# Last updated: 6/6/2026, 12:27:12 AM
 1class Solution:
 2    def longestPalindrome(self, s: str) -> str:
 3        def expand(i, j):
-4            while i >= 0 and j < len(s) and s[i] == s[j]:
+4            while i > 0 and j < len(s) - 1 and s[i] == s[j]:
 5                i -= 1
 6                j += 1
-7            return j - i - 1
-8    
-9        ans = ""
-10        for i in range(len(s)):
-11            even = expand(i, i + 1)
-12            if even > len(ans):
-13                half = even // 2
-14                ans = s[i - half + 1:i + half + 1]
-15            odd = expand(i, i)
-16            if odd > len(ans):
-17                half = odd // 2
-18                ans = s[i - half:i + half + 1]
-19        
-20        return ans
-21
-22
-23
+7            
+8            if j < len(s) and s[i] == s[j]:
+9                return i, j
+10            return i + 1, j - 1
+11        
+12        longest = ""
+13        for i in range(len(s)):
+14            ei, ej = expand(i, i + 1)
+15            if ej - ei + 1 > len(longest):
+16                longest = s[ei:ej + 1]
+17            oi, oj = expand(i, i)
+18            if oj - oi + 1 > len(longest):
+19                longest = s[oi:oj + 1]
+20            
+21        return longest
