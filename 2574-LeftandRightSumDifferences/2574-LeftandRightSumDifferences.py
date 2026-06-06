6a8cdc3c9d0f1babda2945b8498495eb1d719fd9
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-06 00:17:29 -0700

    File updated at 6/6/2026, 12:17:29 AM

--- 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.py
+++ 2574-LeftandRightSumDifferences/2574-LeftandRightSumDifferences.py
@@ -1,17 +1,13 @@
-# Last updated: 6/6/2026, 12:10:05 AM
+# Last updated: 6/6/2026, 12:17:29 AM
 1class Solution:
 2    def leftRightDifference(self, nums: List[int]) -> List[int]:
-3        leftSum = []
-4        leftPrefix = 0
-5        rightSum = [0] * len(nums)
-6        rightPrefix = 0
-7
-8        for i in range(len(nums)):
-9            leftSum.append(leftPrefix)
-10            leftPrefix += nums[i]
-11        
-12        for j in range(len(nums) - 1, -1, -1):
-13            rightSum[j] = rightPrefix
-14            rightPrefix += nums[j]
-15
-16        return [abs(leftSum[i] - rightSum[i]) for i in range(len(nums))]
+3        total = sum(nums)
+4        left = 0
+5        res = []
+6
+7        for num in nums:
+8            right = total - left - num
+9            res.append(abs(left - right))
+10            left += num
+11
+12        return res
