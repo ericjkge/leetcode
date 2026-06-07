e605e97f2f99998e11ed7b427ef4a6dd7d5e3898
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-07 11:03:47 -0700

    File updated at 6/7/2026, 11:03:47 AM

--- 261-GraphValidTree/261-GraphValidTree.py
+++ 261-GraphValidTree/261-GraphValidTree.py
@@ -1,21 +1,18 @@
-# Last updated: 4/14/2026, 9:38:45 AM
+# Last updated: 6/7/2026, 11:03:47 AM
 1class Solution:
 2    def validTree(self, n: int, edges: List[List[int]]) -> bool:
-3        if len(edges) != n - 1:
-4            return False
-5        
-6        graph = defaultdict(list)
-7        for a, b in edges:
-8            graph[a].append(b)
-9            graph[b].append(a)
-10
-11        seen = set()
-12        def dfs(node):
-13            seen.add(node)
-14            for neighbor in graph[node]:
-15                if neighbor not in seen:
-16                    dfs(neighbor)
-17        
-18        dfs(0)
-19        return len(seen) == n
-20
+3        seen = set()
+4        graph = defaultdict(list)
+5
+6        for a, b in edges:
+7            graph[a].append(b)
+8            graph[b].append(a)
+9
+10        def dfs(node):
+11            seen.add(node)
+12            for neighbor in graph[node]:
+13                if neighbor not in seen:
+14                    dfs(neighbor)
+15
+16        dfs(0)
+17        return len(edges) == n - 1 and len(seen) == n
