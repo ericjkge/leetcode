33297d27f1d53c776d6ee2e6b8c5b297b704e7cb
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-09 20:28:37 -0700

    File updated at 6/9/2026, 8:28:37 PM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -1,4 +1,4 @@
-# Last updated: 3/23/2026, 2:15:03 PM
+# Last updated: 6/9/2026, 8:28:37 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, x):
@@ -10,10 +10,10 @@
 9        slow = fast = head
 10
 11        while fast and fast.next:
-12            slow = slow.next
-13            fast = fast.next.next
+12            fast = fast.next.next
+13            slow = slow.next
 14
-15            if fast == slow:
+15            if slow == fast:
 16                return True
-17        
+17            
 18        return False
