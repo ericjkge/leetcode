db1c9d01e46f0ed0aed94342f871bb2a86114526
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-11 22:18:01 -0700

    File updated at 6/11/2026, 10:18:00 PM

--- 143-ReorderList/143-ReorderList.py
+++ 143-ReorderList/143-ReorderList.py
@@ -1,4 +1,4 @@
-# Last updated: 3/24/2026, 10:39:17 AM
+# Last updated: 6/11/2026, 10:18:00 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -11,23 +11,26 @@
 10        """
 11        slow = fast = head
 12
-13        while fast and fast.next:
+13        while fast.next and fast.next.next:
 14            slow = slow.next
 15            fast = fast.next.next
 16
 17        prev = None
-18        while slow:
-19            nxt = slow.next
-20            slow.next = prev
-21            prev = slow
-22            slow = nxt
-23        
-24        p1 = head
-25        p2 = prev
-26        while p2.next != None:
-27            n1 = p1.next
-28            p1.next = p2
-29            p1 = n1
-30            n2 = p2.next
-31            p2.next = p1
-32            p2 = n2
+18        curr = slow.next
+19        slow.next = None
+20
+21        while curr:
+22            nxt = curr.next
+23            curr.next = prev
+24            prev = curr
+25            curr = nxt
+26        
+27        p1 = head
+28        p2 = prev
+29        while p2 is not None:
+30            n1 = p1.next
+31            p1.next = p2
+32            n2 = p2.next
+33            p2.next = n1
+34            p1 = n1
+35            p2 = n2
