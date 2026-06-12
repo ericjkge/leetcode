e42985598b8299ea334d23cec91a4c96ae0bda33
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-11 21:41:17 -0700

    File updated at 6/11/2026, 9:41:17 PM

--- 15-3Sum/15-3Sum.py
+++ 15-3Sum/15-3Sum.py
@@ -1,14 +1,14 @@
-# Last updated: 2/27/2026, 9:14:24 AM
+# Last updated: 6/11/2026, 9:41:17 PM
 1class Solution:
-2    def threeSum(self, nums: list[int]) -> list[list[int]]:
-3        nums.sort()
-4        print(nums)
-5        triplets = []
-6
-7        for i, num in enumerate(nums):
-8            if i > 0 and num == nums[i - 1]:
-9                continue
-10            target = -num
+2    def threeSum(self, nums: List[int]) -> List[List[int]]:
+3        ans = []
+4        nums.sort()
+5
+6        for i in range(len(nums)):
+7            if i > 0 and nums[i] == nums[i - 1]:
+8                continue
+9
+10            target = -nums[i]
 11            left, right = i + 1, len(nums) - 1
 12            while left < right:
 13                if nums[left] + nums[right] > target:
@@ -16,9 +16,10 @@
 15                elif nums[left] + nums[right] < target:
 16                    left += 1
 17                else:
-18                    triplets.append([num, nums[left], nums[right]])
+18                    ans.append([nums[i], nums[left], nums[right]])
 19                    left += 1
 20                    while left < right and nums[left] == nums[left - 1]:
 21                        left += 1
 22
-23        return triplets
+23
+24        return ans
