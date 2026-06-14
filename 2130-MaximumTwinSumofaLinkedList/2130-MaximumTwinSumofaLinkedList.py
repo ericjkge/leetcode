fc6ba269e0b2eb517e2d52c34e1160686aed3578
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-13 21:51:54 -0700

    File updated at 6/13/2026, 9:51:54 PM

--- 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
+++ 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 6/13/2026, 9:43:51 PM
+# Last updated: 6/13/2026, 9:51:54 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -6,17 +6,25 @@
 5#         self.next = next
 6class Solution:
 7    def pairSum(self, head: Optional[ListNode]) -> int:
-8        p = head
-9        vals = []
-10        while p:
-11            vals.append(p.val)
-12            p = p.next
-13
-14        mx = 0
-15        left, right = 0, len(vals) - 1
-16        while left < right:
-17            mx = max(mx, vals[left] + vals[right])
-18            left += 1
-19            right -= 1
-20        
-21        return mx
+8        slow = fast = head
+9
+10        while fast and fast.next:
+11            slow = slow.next
+12            fast = fast.next.next
+13        
+14        prev = None
+15        while slow:
+16            nxt = slow.next
+17            slow.next = prev
+18            prev = slow
+19            slow = nxt
+20
+21        p1, p2 = head, prev
+22        mx = 0
+23
+24        while p2:
+25            mx = max(mx, p1.val + p2.val)
+26            p1 = p1.next
+27            p2 = p2.next
+28
+29        return mx
