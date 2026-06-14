00535cf88731659c3fb55411aede41a40ffdf79e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-13 21:43:51 -0700

    File updated at 6/13/2026, 9:43:51 PM

--- 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
+++ 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
@@ -1,32 +1,22 @@
-# Last updated: 7/5/2025, 8:57:16 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def pairSum(self, head: Optional[ListNode]) -> int:
-        max_sum = 0
-
-        # 1. Find mid
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        # 2. Reverse second half
-        prev = None
-        while slow:
-            nxt = slow.next
-            slow.next = prev
-            prev = slow
-            slow = nxt
-        
-        # 3. Scan twin sums
-        node = head
-        while prev:
-            max_sum = max(max_sum, prev.val + node.val)
-            prev = prev.next
-            node = node.next
-        
-        return max_sum
+# Last updated: 6/13/2026, 9:43:51 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def pairSum(self, head: Optional[ListNode]) -> int:
+8        p = head
+9        vals = []
+10        while p:
+11            vals.append(p.val)
+12            p = p.next
+13
+14        mx = 0
+15        left, right = 0, len(vals) - 1
+16        while left < right:
+17            mx = max(mx, vals[left] + vals[right])
+18            left += 1
+19            right -= 1
+20        
+21        return mx
