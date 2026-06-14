64c1bf1a0f1a7760bbac062833cd74688fabc555
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-14 10:37:12 -0700

    File updated at 6/14/2026, 10:37:11 AM

--- 49-GroupAnagrams/49-GroupAnagrams.py
+++ 49-GroupAnagrams/49-GroupAnagrams.py
@@ -1,9 +1,11 @@
-# Last updated: 3/3/2026, 8:55:41 AM
+# Last updated: 6/14/2026, 10:37:11 AM
 1class Solution:
 2    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 3        anagrams = defaultdict(list)
 4
 5        for s in strs:
-6            anagrams["".join(sorted(s))].append(s)
-7        
-8        return [v for v in anagrams.values()]
+6            chars = sorted(list(s))
+7            letters = "".join(chars)
+8            anagrams[letters].append(s)
+9        
+10        return [l for l in anagrams.values()]
