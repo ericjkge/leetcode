04f9d1650702eed15062f1e21731981d9daca2ab
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-15 20:50:43 -0700

    File updated at 6/15/2026, 8:50:43 PM

--- 269-AlienDictionary/269-AlienDictionary.py
+++ 269-AlienDictionary/269-AlienDictionary.py
@@ -1,4 +1,4 @@
-# Last updated: 5/19/2026, 9:22:39 AM
+# Last updated: 6/15/2026, 8:50:43 PM
 1class Solution:
 2    def alienOrder(self, words: List[str]) -> str:
 3        chars = set([c for word in words for c in word])
@@ -8,22 +8,23 @@
 7        for i in range(len(words) - 1):
 8            if len(words[i]) > len(words[i + 1]) and words[i].startswith(words[i + 1]):
 9                return ""
-10            for j in range(min(len(words[i]), len(words[i + 1]))):
-11                if words[i][j] != words[i + 1][j]:
-12                    graph[words[i][j]].append(words[i + 1][j])
-13                    indegrees[words[i + 1][j]] += 1
-14                    break
-15
-16        queue = deque([c for c in chars if indegrees[c] == 0])
-17        order = []
-18        while queue:
-19            node = queue.popleft()
-20            order.append(node)
+10
+11            for j in range(min(len(words[i]), len(words[i + 1]))):
+12                if words[i][j] != words[i + 1][j]:
+13                    graph[words[i][j]].append(words[i + 1][j])
+14                    indegrees[words[i + 1][j]] += 1
+15                    break
+16            
+17        queue = deque([char for char in indegrees if indegrees[char] == 0])
+18        order = [char for char in indegrees if indegrees[char] == 0]
+19        while queue:
+20            node = queue.popleft()
 21            for neighbor in graph[node]:
 22                indegrees[neighbor] -= 1
 23                if indegrees[neighbor] == 0:
-24                    queue.append(neighbor)
-25        
-26        if len(order) != len(chars):
-27            return ""
-28        return "".join(order)
+24                    order.append(neighbor)
+25                    queue.append(neighbor)
+26        
+27        if len(order) == len(indegrees):
+28            return "".join(order)
+29        return ""
