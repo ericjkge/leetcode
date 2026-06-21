52e320611c2761d4f7ee294ee521db187e2d711c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-21 15:23:27 -0700

    File updated at 6/21/2026, 3:23:27 PM

--- 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.py
+++ 1833-MaximumIceCreamBars/1833-MaximumIceCreamBars.py
@@ -1,9 +1,12 @@
-# Last updated: 7/12/2025, 3:37:22 PM
-class Solution:
-    def maxIceCream(self, costs: List[int], coins: int) -> int:
-        costs.sort()
-        for i, c in enumerate(costs):
-            if coins < c:
-                return i
-            coins -= c
-        return len(costs)
+# Last updated: 6/21/2026, 3:23:27 PM
+1class Solution:
+2    def maxIceCream(self, costs: List[int], coins: int) -> int:
+3        count = 0
+4        for cost in sorted(costs):
+5            if cost > coins:
+6                break
+7
+8            count += 1
+9            coins -= cost
+10        
+11        return count
