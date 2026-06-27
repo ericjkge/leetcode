bfefbfe498578bb6368b7dd5a2ccf89f22d541ea
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-27 12:00:07 -0700

    File updated at 6/27/2026, 12:00:07 PM

--- 59-SpiralMatrixII/59-SpiralMatrixII.py
+++ 59-SpiralMatrixII/59-SpiralMatrixII.py
@@ -1,4 +1,4 @@
-# Last updated: 6/27/2026, 12:00:01 PM
+# Last updated: 6/27/2026, 12:00:07 PM
 1class Solution:
 2    def generateMatrix(self, n: int) -> List[List[int]]:
 3        res = [[0] * n for _ in range(n)]
@@ -9,29 +9,25 @@
 8        while left <= right and top <= bottom:
 9            for col in range(left, right + 1):
 10                res[top][col] = i
-11                print(res)
-12                i += 1
-13            top += 1
-14            
-15            for row in range(top, bottom + 1):
-16                res[row][right] = i
-17                print(res)
-18                i += 1
-19            right -= 1
-20
-21            if left > right or top > bottom:
-22                break
-23
-24            for col in range(right, left - 1, -1):
-25                res[bottom][col] = i
-26                print(res)
-27                i += 1
-28            bottom -= 1
-29            
-30            for row in range(bottom, top - 1, -1):
-31                res[row][left] = i
-32                print(res)
-33                i += 1
-34            left += 1
-35                
-36        return res
+11                i += 1
+12            top += 1
+13            
+14            for row in range(top, bottom + 1):
+15                res[row][right] = i
+16                i += 1
+17            right -= 1
+18
+19            if left > right or top > bottom:
+20                break
+21
+22            for col in range(right, left - 1, -1):
+23                res[bottom][col] = i
+24                i += 1
+25            bottom -= 1
+26            
+27            for row in range(bottom, top - 1, -1):
+28                res[row][left] = i
+29                i += 1
+30            left += 1
+31                
+32        return res
