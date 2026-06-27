7c92b6ad22bbbbb4d69b08930d1cb91f628a3792
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-27 12:05:45 -0700

    File updated at 6/27/2026, 12:05:45 PM

--- 48-RotateImage/48-RotateImage.py
+++ 48-RotateImage/48-RotateImage.py
@@ -1,21 +1,16 @@
-# Last updated: 3/3/2026, 8:49:14 AM
+# Last updated: 6/27/2026, 12:05:45 PM
 1class Solution:
 2    def rotate(self, matrix: List[List[int]]) -> None:
 3        """
 4        Do not return anything, modify matrix in-place instead.
 5        """
-6        rows, cols = len(matrix), len(matrix[0])
+6        n = len(matrix)
 7
-8        for r in range(rows):
-9            for c in range(cols):
-10                if r < c:
-11                    matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
-12
-13        for row in matrix:
-14            row.reverse()
-15        
-16        return matrix
-17
-18                    # 147
-19                    # 258
-20                    # 369
+8        for r in range(n):
+9            for c in range(r, n):
+10                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+11        
+12        for row in matrix:
+13            row.reverse()
+14
+15        return matrix
