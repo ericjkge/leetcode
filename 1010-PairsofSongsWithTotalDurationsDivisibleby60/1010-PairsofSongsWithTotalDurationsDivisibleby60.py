e6906c9f318f5717bed47f349eb114fa04e9ecf4
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-27 16:17:39 -0700

    Create 1010-PairsofSongsWithTotalDurationsDivisibleby60

---
+++ 1010-PairsofSongsWithTotalDurationsDivisibleby60/1010-PairsofSongsWithTotalDurationsDivisibleby60.py
@@ -0,0 +1,13 @@
+# Last updated: 6/27/2026, 4:17:38 PM
+1class Solution:
+2    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+3        mapping = defaultdict(int)
+4        count = 0
+5
+6        for i in range(len(time)):
+7            complement = (60 - time[i] % 60) % 60
+8            if complement in mapping:
+9                count += mapping[complement]
+10            mapping[time[i] % 60] += 1
+11
+12        return count
