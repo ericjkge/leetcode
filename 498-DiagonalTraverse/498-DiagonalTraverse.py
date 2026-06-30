26372f08f47b5927ea18d2f37aef8704402933de
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-06-29 20:15:29 -0700

    File updated at 6/29/2026, 8:15:28 PM

--- 498-DiagonalTraverse/498-DiagonalTraverse.py
+++ 498-DiagonalTraverse/498-DiagonalTraverse.py
@@ -1,22 +1,32 @@
-# Last updated: 6/29/2026, 8:07:04 PM
+# Last updated: 6/29/2026, 8:15:28 PM
 1class Solution:
 2    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-3        rows, cols = len(mat), len(mat[0])
-4        starts = [(i, 0) for i in range(rows)] + [(rows - 1, i) for i in range(1, cols)]
-5        diags = []
-6
-7        for r, c in starts:
-8            i, j = r, c
-9            diag = []
-10            while i >= 0 and j < cols:
-11                diag.append(mat[i][j])
-12                i -= 1
-13                j += 1
-14            diags.append(diag)
-15
-16        for i in range(len(diags)):
-17            if i % 2 == 0:
-18                continue
-19            diags[i].reverse()
-20
-21        return [num for diag in diags for num in diag]
+3        i, j = 0, 0
+4        res = []
+5        rows, cols = len(mat), len(mat[0])
+6        up = True
+7
+8        for _ in range(rows * cols):
+9            res.append(mat[i][j])
+10            if up:
+11                if j == cols - 1:
+12                    i += 1
+13                    up = False
+14                elif i == 0:
+15                    j += 1
+16                    up = False
+17                else:
+18                    i -= 1
+19                    j += 1
+20            else:
+21                if i == rows - 1:
+22                    j += 1
+23                    up = True
+24                elif j == 0:
+25                    i += 1
+26                    up = True
+27                else:
+28                    i += 1
+29                    j -= 1
+30        
+31        return res
