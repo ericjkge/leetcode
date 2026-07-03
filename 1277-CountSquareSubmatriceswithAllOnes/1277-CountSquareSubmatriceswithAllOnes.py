238f0d2dc3d5f44c4092ff2ae7acbcc5420aa6ae
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-02 23:51:58 -0700

    File updated at 7/2/2026, 11:51:57 PM

--- 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.py
+++ 1277-CountSquareSubmatriceswithAllOnes/1277-CountSquareSubmatriceswithAllOnes.py
@@ -1,22 +1,14 @@
-# Last updated: 8/14/2025, 11:37:36 PM
-class Solution:
-    def countSquares(self, matrix: List[List[int]]) -> int:
-        m, n = len(matrix), len(matrix[0])
-        total = 0
-
-        @cache
-        def dp(i, j):
-            if i == m or j == n or matrix[i][j] == 0:
-                return 0
-            
-            right = dp(i, j + 1)
-            down = dp(i + 1, j)
-            diag = dp(i + 1, j + 1)
-
-            return 1 + min(right, down, diag)
-
-        for i in range(m):
-            for j in range(n):
-                total += dp(i, j)
-
-        return total
+# Last updated: 7/2/2026, 11:51:57 PM
+1class Solution:
+2    def countSquares(self, matrix: List[List[int]]) -> int:
+3        m, n = len(matrix), len(matrix[0])
+4
+5        @cache
+6        def dp(i, j):
+7            if i >= m or j >= n or matrix[i][j] == 0:
+8                return 0
+9            
+10            return 1 + min(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1))
+11        
+12        return sum(dp(i, j) for i in range(m) for j in range(n))
+13
