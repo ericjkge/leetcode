7eb9a74db59f0168b314200b268b24499b586512
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-04 14:14:56 -0700

    File updated at 7/4/2026, 2:14:56 PM

--- 532-K-diffPairsinanArray/532-K-diffPairsinanArray.py
+++ 532-K-diffPairsinanArray/532-K-diffPairsinanArray.py
@@ -1,16 +1,12 @@
-# Last updated: 7/4/2026, 2:03:27 PM
+# Last updated: 7/4/2026, 2:14:56 PM
 1class Solution:
 2    def findPairs(self, nums: List[int], k: int) -> int:
 3        freqs = Counter(nums)
 4        count = 0
 5
 6        if k == 0:
-7            for num in freqs:
-8                if freqs[num] > 1:
-9                    count += 1
-10        else:
-11            for num in freqs:
-12                if num + k in freqs:
-13                    count += 1
-14
-15        return count
+7            return sum(1 for num in freqs if freqs[num] > 1)
+8        else:
+9            return sum(1 for num in freqs if num + k in freqs)
+10
+11        return count
