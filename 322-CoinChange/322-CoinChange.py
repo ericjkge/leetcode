fbb005d05edbb93e8335a2f3d6f3fb6d482308eb
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-05 20:39:00 -0700

    File updated at 7/5/2026, 8:39:00 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,15 +1,22 @@
-# Last updated: 7/5/2026, 8:34:00 PM
+# Last updated: 7/5/2026, 8:39:00 PM
 1class Solution:
 2    def coinChange(self, coins: List[int], amount: int) -> int:
-3        @cache
-4        def dp(i, j):
-5            if i >= len(coins) or j > amount:
-6                return float("inf")
-7            
-8            if j == amount:
-9                return 0
-10
-11            return min(1 + dp(i, j + coins[i]), dp(i + 1, j))
-12        
-13        res = dp(0, 0)
-14        return res if res != float("inf") else -1
+3        memo = {}
+4
+5        def dp(i, j):
+6            if (i, j) in memo:
+7                return memo[(i, j)]
+8
+9            if i >= len(coins) or j > amount:
+10                memo[(i, j)] = float("inf")
+11                return memo[(i, j)]
+12            
+13            if j == amount:
+14                memo[(i, j)] = 0
+15                return memo[(i, j)]
+16
+17            memo[(i, j)] = min(1 + dp(i, j + coins[i]), dp(i + 1, j))
+18            return memo[(i, j)]
+19        
+20        res = dp(0, 0)
+21        return res if res != float("inf") else -1
