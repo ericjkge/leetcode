b7dd8e477e8bd441c9f436697e78d8543d82b878
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-05 23:49:16 -0700

    File updated at 7/5/2026, 11:49:15 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,19 +1,19 @@
-# Last updated: 4/1/2026, 10:55:21 PM
+# Last updated: 7/5/2026, 11:49:15 PM
 1class Solution:
 2    def numIslands(self, grid: List[List[str]]) -> int:
-3        rows, cols = len(grid), len(grid[0])
+3        m, n = len(grid), len(grid[0])
 4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 5
 6        def dfs(r, c):
-7            grid[r][c] = "0"
+7            grid[r][c] = "#"
 8            for dr, dc in directions:
 9                nr, nc = r + dr, c + dc
-10                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
+10                if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == "1":
 11                    dfs(nr, nc)
 12        
 13        count = 0
-14        for r in range(rows):
-15            for c in range(cols):
+14        for r in range(m):
+15            for c in range(n):
 16                if grid[r][c] == "1":
 17                    count += 1
 18                    dfs(r, c)
