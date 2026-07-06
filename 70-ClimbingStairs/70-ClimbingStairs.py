57e168897dc2f44c085800dedcea913565004fc1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-05 22:23:57 -0700

    File updated at 7/5/2026, 10:23:57 PM

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,4 +1,4 @@
-# Last updated: 3/6/2026, 11:59:40 AM
+# Last updated: 7/5/2026, 10:23:57 PM
 1class Solution:
 2    def climbStairs(self, n: int) -> int:
 3        @cache
@@ -8,7 +8,7 @@
 7            
 8            if i > n:
 9                return 0
-10
+10            
 11            return dp(i + 1) + dp(i + 2)
 12        
 13        return dp(0)
