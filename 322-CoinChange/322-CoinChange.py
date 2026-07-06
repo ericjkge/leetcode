347236948f33d628381bb406749dcc27dccd2011
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-05 20:34:01 -0700

    File updated at 7/5/2026, 8:34:00 PM

--- 322-CoinChange/322-CoinChange.py
+++ 322-CoinChange/322-CoinChange.py
@@ -1,15 +1,15 @@
-# Last updated: 4/24/2026, 11:39:01 PM
+# Last updated: 7/5/2026, 8:34:00 PM
 1class Solution:
 2    def coinChange(self, coins: List[int], amount: int) -> int:
 3        @cache
 4        def dp(i, j):
 5            if i >= len(coins) or j > amount:
 6                return float("inf")
-7
+7            
 8            if j == amount:
 9                return 0
 10
 11            return min(1 + dp(i, j + coins[i]), dp(i + 1, j))
 12        
-13        ans = dp(0, 0)
-14        return ans if ans != float("inf") else -1
+13        res = dp(0, 0)
+14        return res if res != float("inf") else -1
