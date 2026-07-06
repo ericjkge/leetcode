7cdd16df6ce7cb5a6dc16dd6d4ff5fe2fc959d61
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-06 00:20:55 -0700

    File updated at 7/6/2026, 12:20:54 AM

--- 337-HouseRobberIII/337-HouseRobberIII.py
+++ 337-HouseRobberIII/337-HouseRobberIII.py
@@ -1,23 +1,19 @@
-# Last updated: 8/21/2025, 12:14:56 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def rob(self, root: Optional[TreeNode]) -> int:
-        @cache
-        def dp(node):
-            if not node:
-                return 0, 0
-            
-            left = dp(node.left)
-            right = dp(node.right)
-
-            rob = node.val + left[1] + right[1]
-            skip = max(left) + max(right)
-
-            return rob, skip
-        
-        return max(dp(root))
+# Last updated: 7/6/2026, 12:20:54 AM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def rob(self, root: Optional[TreeNode]) -> int:
+9        @cache
+10        def dp(node):
+11            if node == None:
+12                return 0, 0
+13
+14            left, right = dp(node.left), dp(node.right)
+15
+16            return node.val + left[1] + right[1], max(left) + max(right) # rob, skip
+17        
+18        return max(dp(root))
