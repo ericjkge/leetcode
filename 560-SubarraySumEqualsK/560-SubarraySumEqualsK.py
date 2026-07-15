63daf7568790dafd2e45124acc1cb3080a3e3794
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-14 23:29:12 -0700

    File updated at 7/14/2026, 11:29:12 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.py
@@ -1,14 +1,15 @@
-# Last updated: 6/14/2026, 10:32:20 AM
+# Last updated: 7/14/2026, 11:29:12 PM
 1class Solution:
 2    def subarraySum(self, nums: List[int], k: int) -> int:
 3        prefix = 0
-4        seen = {0:1}
+4        mapping = {0:1}
 5        count = 0
 6
-7        for num in nums:
+7        for i, num in enumerate(nums):
 8            prefix += num
-9            if prefix - k in seen:
-10                count += seen[prefix - k]
-11            seen[prefix] = seen.get(prefix, 0) + 1
-12        
+9            complement = prefix - k
+10            count += mapping.get(complement, 0)
+11            mapping[prefix] = mapping.get(prefix, 0) + 1
+12
 13        return count
+14
