3e050bf0dfb3b89b4f8b9e18d157f6f8382d3c0f
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 00:01:30 -0700

    File updated at 7/15/2026, 12:01:29 AM

--- 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
+++ 239-SlidingWindowMaximum/239-SlidingWindowMaximum.py
@@ -1,20 +1,18 @@
-# Last updated: 6/23/2026, 8:40:57 PM
+# Last updated: 7/15/2026, 12:01:29 AM
 1class Solution:
 2    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-3        res = []
-4        heap = [(-num, i) for i, num in enumerate(nums[:k])]
-5        heapq.heapify(heap)
-6
-7        for i in range(k, len(nums)):
-8            while heap and heap[0][1] <= i - k - 1:
-9                heapq.heappop(heap)
+3        window = []
+4        ans = [0] * (len(nums) - k + 1)
+5
+6        for i, num in enumerate(nums):
+7            if i < k - 1:
+8                heapq.heappush(window, (-num, i))
+9                continue
 10
-11            res.append(-heap[0][0])
-12            heapq.heappush(heap, (-nums[i], i))
-13    
-14        while heap and heap[0][1] <= len(nums) - 1 - k:
-15            heapq.heappop(heap)
-16
-17        res.append(-heap[0][0])
-18        return res
-19
+11            heapq.heappush(window, (-num, i))
+12            while window and window[0][1] <= i - k:
+13                heapq.heappop(window)
+14            
+15            ans[i - k + 1] = -window[0][0]
+16        
+17        return ans
