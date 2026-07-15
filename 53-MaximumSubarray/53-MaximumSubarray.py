e9bbeb8f1767f40a52f3a1626cc183ec8503786c
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-14 22:58:58 -0700

    File updated at 7/14/2026, 10:58:58 PM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,16 +1,14 @@
-# Last updated: 3/4/2026, 2:22:47 PM
+# Last updated: 7/14/2026, 10:58:58 PM
 1class Solution:
 2    def maxSubArray(self, nums: List[int]) -> int:
 3        window = 0
-4        ans = float("-inf")
+4        mx = float("-inf")
 5
-6        for i in range(len(nums)):
-7            window += nums[i]
-8            ans = max(ans, window)
-9
-10            if window < 0:
-11                window = 0
-12            
-13        return ans
-14
-15
+6        for num in nums:
+7            if window < 0:
+8                window = 0
+9            
+10            window += num
+11            mx = max(mx, window)
+12
+13        return mx
