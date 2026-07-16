3966376dcc3d1c8964447c282209f895134b7aa6
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 23:48:43 -0700

    File updated at 7/15/2026, 11:48:43 PM

--- 724-FindPivotIndex/724-FindPivotIndex.py
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -1,13 +1,12 @@
-# Last updated: 7/15/2026, 11:46:06 PM
+# Last updated: 7/15/2026, 11:48:43 PM
 1class Solution:
 2    def pivotIndex(self, nums: List[int]) -> int:
-3        prefix = 0
-4
-5        for i, num in enumerate(nums):
-6            if prefix == sum(nums[i + 1:]):
-7                return i
-8            
+3        total = sum(nums)
+4        prefix = 0
+5
+6        for i, num in enumerate(nums):
+7            if prefix == total - prefix - num:
+8                return i
 9            prefix += num
-10
+10        
 11        return -1
-1214
