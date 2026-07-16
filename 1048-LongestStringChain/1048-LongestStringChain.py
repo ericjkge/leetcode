4295c1bf665d120645ebf972b625b1c39121cc14
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 21:48:44 -0700

    File updated at 7/15/2026, 9:48:44 PM

--- 1048-LongestStringChain/1048-LongestStringChain.py
+++ 1048-LongestStringChain/1048-LongestStringChain.py
@@ -1,16 +1,17 @@
-# Last updated: 8/20/2025, 10:23:58 PM
-class Solution:
-    def longestStrChain(self, words: List[str]) -> int:
-        s = set(words) # O(1) look up
-
-        @cache
-        def dp(w):
-            best = 1
-            for i in range(len(w)):
-                prev = w[:i] + w[i + 1:]
-                if prev in s:
-                    best = max(best, 1 + dp(prev))
-            return best
-        
-        return max(dp(w) for w in s)
-        
+# Last updated: 7/15/2026, 9:48:44 PM
+1class Solution:
+2    def longestStrChain(self, words: List[str]) -> int:
+3        words.sort(key=lambda x: len(x))
+4        seen = {}
+5
+6        @cache
+7        def dp(i):
+8            word = words[i]
+9            longest = 1
+10            for j in range(len(word)):
+11                if word[:j] + word[j + 1:] in seen:
+12                    longest = max(longest, 1 + dp(seen[word[:j] + word[j + 1:]]))
+13            seen[word] = i
+14            return longest
+15        
+16        return max(dp(i) for i in range(len(words)))
