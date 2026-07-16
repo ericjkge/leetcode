b63d05e26d7d1e3544286ccf8eafb788e235d0ee
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 21:05:26 -0700

    File updated at 7/15/2026, 9:05:26 PM

--- 647-PalindromicSubstrings/647-PalindromicSubstrings.py
+++ 647-PalindromicSubstrings/647-PalindromicSubstrings.py
@@ -1,18 +1,10 @@
-# Last updated: 6/8/2026, 8:34:57 PM
+# Last updated: 7/15/2026, 9:05:26 PM
 1class Solution:
 2    def countSubstrings(self, s: str) -> int:
-3        self.counter = 0
-4
-5        def expand(i, j):
-6            while i >= 0 and j < len(s) and s[i] == s[j]:
-7                self.counter += 1
-8                i -= 1
-9                j += 1
-10
-11        for i in range(len(s)):
-12            expand(i, i)
-13            expand(i, i + 1)
-14        
-15        return self.counter
-16            
-17
+3        @cache
+4        def dp(i, j):
+5            if j - i < 1:
+6                return True
+7            return s[i] == s[j] and dp(i + 1, j - 1)
+8        
+9        return sum(dp(i, j) for i in range(len(s)) for j in range(i, len(s)))
