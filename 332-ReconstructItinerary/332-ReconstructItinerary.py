181e3e15cf5fed47c69528ba2008255aa5890de7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 23:35:10 -0700

    File updated at 7/15/2026, 11:35:10 PM

--- 332-ReconstructItinerary/332-ReconstructItinerary.py
+++ 332-ReconstructItinerary/332-ReconstructItinerary.py
@@ -1,16 +1,18 @@
-# Last updated: 5/19/2026, 9:42:46 AM
+# Last updated: 7/15/2026, 11:35:10 PM
 1class Solution:
 2    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-3        graph = defaultdict(list)
-4
-5        for src, dst in tickets:
-6            heapq.heappush(graph[src], dst)
-7        
+3        neighbors = defaultdict(list)
+4        
+5        for u, v in tickets:
+6            heapq.heappush(neighbors[u], v)
+7
 8        order = []
 9        def dfs(node):
-10            while graph[node]:
-11                dfs(heapq.heappop(graph[node]))
+10            while neighbors[node]:
+11                dfs(heapq.heappop(neighbors[node]))
 12            order.append(node)
 13
 14        dfs("JFK")
 15        return order[::-1]
+16        
+17            
