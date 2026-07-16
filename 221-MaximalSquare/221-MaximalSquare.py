f859cbbeeb2d4c67e861b9c54022c4ebcb9578c8
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 20:37:05 -0700

    File updated at 7/15/2026, 8:37:05 PM

--- 221-MaximalSquare/221-MaximalSquare.py
+++ 221-MaximalSquare/221-MaximalSquare.py
@@ -1,13 +1,13 @@
-# Last updated: 7/2/2026, 11:32:54 PM
+# Last updated: 7/15/2026, 8:37:05 PM
 1class Solution:
 2    def maximalSquare(self, matrix: List[List[str]]) -> int:
-3        m, n = len(matrix), len(matrix[0])
-4        @cache
-5        def dp(i, j):
-6            if i >= m or j >= n or matrix[i][j] == "0":
-7                return 0
-8
-9            return 1 + min(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1))
-10        
-11        return max(dp(i, j) for i in range(m) for j in range(n)) ** 2
-12
+3        rows, cols = len(matrix), len(matrix[0])
+4
+5        @cache
+6        def dp(i, j):
+7            if i >= rows or j >= cols or matrix[i][j] == "0":
+8                return 0
+9            
+10            return 1 + min(dp(i + 1, j), dp(i, j + 1), dp(i + 1, j + 1))
+11
+12        return max(dp(r, c) for r in range(rows) for c in range(cols)) ** 2
