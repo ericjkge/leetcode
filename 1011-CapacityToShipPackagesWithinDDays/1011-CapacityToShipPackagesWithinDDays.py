19c43ad6d5a5e498380df659e177dea01384e512
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-15 20:02:53 -0700

    File updated at 7/15/2026, 8:02:53 PM

--- 1011-CapacityToShipPackagesWithinDDays/1011-CapacityToShipPackagesWithinDDays.py
+++ 1011-CapacityToShipPackagesWithinDDays/1011-CapacityToShipPackagesWithinDDays.py
@@ -1,4 +1,4 @@
-# Last updated: 7/15/2026, 8:02:50 PM
+# Last updated: 7/15/2026, 8:02:53 PM
 1class Solution:
 2    def shipWithinDays(self, weights: List[int], days: int) -> int:
 3        def canShip(capacity):
@@ -6,23 +6,22 @@
 5            total = 0
 6            for weight in weights:
 7                if total + weight > capacity:
-8                    print(total)
-9                    total = 0
-10                    count += 1
-11                
-12                total += weight
-13            
-14            count += 1
-15            return count <= days
-16                
-17        left, right = max(weights), sum(weights)
-18        while left + 1 < right:
-19            mid = (left + right) // 2
-20            if canShip(mid):
-21                right = mid
-22            else:
-23                left = mid
-24            
-25        if canShip(left):
-26            return left
-27        return right
+8                    total = 0
+9                    count += 1
+10                
+11                total += weight
+12            
+13            count += 1
+14            return count <= days
+15                
+16        left, right = max(weights), sum(weights)
+17        while left + 1 < right:
+18            mid = (left + right) // 2
+19            if canShip(mid):
+20                right = mid
+21            else:
+22                left = mid
+23            
+24        if canShip(left):
+25            return left
+26        return right
