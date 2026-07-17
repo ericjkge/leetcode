fdd8633963d1bfc8b0c7583e4036ed46ca48909a
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-17 10:31:59 -0700

    File updated at 7/17/2026, 10:31:58 AM

--- 813-LargestSumofAverages/813-LargestSumofAverages.py
+++ 813-LargestSumofAverages/813-LargestSumofAverages.py
@@ -1,22 +1,16 @@
-# Last updated: 8/22/2025, 7:15:40 PM
-class Solution:
-    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
-        n = len(nums)
-        prefix = [0]
-        for i in range(n):
-            prefix.append(prefix[-1] + nums[i])
-        
-        def average(i, j): # i, j inclusive
-            return (prefix[j + 1] - prefix[i]) / (j - i + 1)
-
-        @cache
-        def dp(i, j): # max sum of averages in nums[i:] with j groups
-            if j == 1:
-                return average(i, n - 1)
-
-            best = 0
-            for split in range(i, n - j + 1):
-                best = max(best, average(i, split) + dp(split + 1, j - 1))
-            return best
-                
-        return dp(0, k)
+# Last updated: 7/17/2026, 10:31:58 AM
+1class Solution:
+2    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+3        n = len(nums)
+4
+5        @cache
+6        def dp(i, j):
+7            if j == 1:
+8                return sum(nums[i:n]) / len(nums[i:n])
+9            
+10            mx = 0
+11            for index in range(i, n - j + 1):
+12                mx = max(mx, sum(nums[i:index + 1]) / len(nums[i:index + 1]) + dp(index + 1, j - 1))
+13            return mx
+14
+15        return dp(0, k)
