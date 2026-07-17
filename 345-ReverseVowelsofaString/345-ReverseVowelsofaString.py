613af97c8466d72ae0747a547b5b18e327855eac
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-16 23:06:36 -0700

    File updated at 7/16/2026, 11:06:36 PM

--- 345-ReverseVowelsofaString/345-ReverseVowelsofaString.py
+++ 345-ReverseVowelsofaString/345-ReverseVowelsofaString.py
@@ -1,21 +1,21 @@
-# Last updated: 7/25/2025, 11:46:52 PM
-class Solution:
-    def reverseVowels(self, s: str) -> str:
-        p1, p2 = 0, len(s) - 1
-        chars = [char for char in s]
-        vowels = "aeiouAEIOU"
-        
-        while p1 < p2:
-            if chars[p1] in vowels and chars[p2] in vowels:
-                chars[p1], chars[p2] = chars[p2], chars[p1]
-                p1 += 1
-                p2 -= 1
-            elif chars[p1] in vowels:
-                p2 -= 1
-            elif chars[p2] in vowels:
-                p1 += 1
-            else:
-                p1 += 1
-                p2 -= 1
-        
-        return "".join(chars)
+# Last updated: 7/16/2026, 11:06:36 PM
+1class Solution:
+2    def reverseVowels(self, s: str) -> str:
+3        vowels = "aeiouAEIOU"
+4        left, right = 0, len(s) - 1
+5        s = list(s)
+6
+7        while left < right:
+8            if s[left] in vowels and s[right] in vowels:
+9                s[left], s[right] = s[right], s[left]
+10                left += 1
+11                right -= 1
+12            elif s[left] in vowels:
+13                right -= 1
+14            elif s[right] in vowels:
+15                left += 1
+16            else:
+17                left += 1
+18                right -= 1
+19        
+20        return "".join(s)
