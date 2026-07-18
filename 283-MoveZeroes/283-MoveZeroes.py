d09f0061a0e8d55691aa670126a9534587022194
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-17 20:13:06 -0700

    File updated at 7/17/2026, 8:13:06 PM

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,11 +1,13 @@
-# Last updated: 7/8/2025, 9:34:04 AM
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        tracker = 0
-        for pointer in range(len(nums)):
-            if nums[pointer] != 0:
-                nums[tracker], nums[pointer] = nums[pointer], nums[tracker]
-                tracker += 1
+# Last updated: 7/17/2026, 8:13:06 PM
+1class Solution:
+2    def moveZeroes(self, nums: List[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        p1 = p2 = 0
+7
+8        while p2 < len(nums):
+9            if nums[p2] != 0:
+10                nums[p1], nums[p2] = nums[p2], nums[p1]
+11                p1 += 1
+12            p2 += 1
