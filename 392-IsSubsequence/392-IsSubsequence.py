ee69893e961a64ae53697ef67aba3f3fa9fa2474
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-17 20:18:58 -0700

    File updated at 7/17/2026, 8:18:58 PM

--- 392-IsSubsequence/392-IsSubsequence.py
+++ 392-IsSubsequence/392-IsSubsequence.py
@@ -1,16 +1,17 @@
-# Last updated: 11/14/2025, 10:01:52 AM
-class Solution:
-    def isSubsequence(self, s: str, t: str) -> bool:
-        if not s:
-            return True
-            
-        p1 = p2 = 0
-
-        while p2 < len(t):
-            if s[p1] == t[p2]:
-                p1 += 1
-            if p1 >= len(s):
-                return True
-            p2 += 1
-
-        return False
+# Last updated: 7/17/2026, 8:18:58 PM
+1class Solution:
+2    def isSubsequence(self, s: str, t: str) -> bool:
+3        p1 = p2 = 0
+4
+5        while p1 < len(s):
+6            if p2 == len(t):
+7                return False
+8            
+9            if s[p1] == t[p2]:
+10                p1 += 1
+11                p2 += 1
+12            else:
+13                p2 += 1
+14            
+15        return True
+16
