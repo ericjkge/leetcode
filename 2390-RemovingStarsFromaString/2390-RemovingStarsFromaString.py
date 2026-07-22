dda352c514374da0236f2a0a66629f15430180e5
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-21 19:39:41 -0700

    File updated at 7/21/2026, 7:39:41 PM

--- 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.py
+++ 2390-RemovingStarsFromaString/2390-RemovingStarsFromaString.py
@@ -1,12 +1,12 @@
-# Last updated: 7/6/2025, 1:10:04 AM
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0")) 
-
-class Solution:
-    def removeStars(self, s: str) -> str:
-        l = []
-        for i in s:
-            if i == '*':
-                l.pop()
-            else:
-                l.append(i)
-        return ''.join(l)
+# Last updated: 7/21/2026, 7:39:41 PM
+1class Solution:
+2    def removeStars(self, s: str) -> str:
+3        stack = []
+4
+5        for c in s:
+6            if c != "*":
+7                stack.append(c)
+8            else:
+9                stack.pop()
+10        
+11        return "".join(stack)
