c6f227856584b1583a948185de037e0e2ee70840
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-23 21:20:15 -0700

    File updated at 7/23/2026, 9:20:14 PM

--- 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
+++ 2095-DeletetheMiddleNodeofaLinkedList/2095-DeletetheMiddleNodeofaLinkedList.py
@@ -1,20 +1,22 @@
-# Last updated: 7/5/2025, 1:56:17 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head.next:
-            return None
-        
-        slow = head
-        fast = head.next.next
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-        slow.next = slow.next.next
-
-        return head
-        
+# Last updated: 7/23/2026, 9:20:14 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+8        if not head or not head.next:
+9            return None
+10
+11        slow = fast = head
+12        prev = None
+13
+14        while fast and fast.next:
+15            fast = fast.next.next
+16            prev = slow
+17            slow = slow.next
+18        
+19        prev.next = prev.next.next
+20
+21        return head
