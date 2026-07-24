8521c6eb48a3227adb344069cbcae220631020fd
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-23 21:36:44 -0700

    File updated at 7/23/2026, 9:36:44 PM

--- 328-OddEvenLinkedList/328-OddEvenLinkedList.py
+++ 328-OddEvenLinkedList/328-OddEvenLinkedList.py
@@ -1,23 +1,26 @@
-# Last updated: 8/28/2025, 12:03:30 PM
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-
-        odd = head
-        first_even = even = head.next
-
-        while even and even.next:
-            odd.next = even.next
-            odd = odd.next
-            even.next = odd.next
-            even = even.next
-        
-        odd.next = first_even
-
-        return head
+# Last updated: 7/23/2026, 9:36:44 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+8        even_dummy = even = ListNode() 
+9        odd_dummy = odd = ListNode
+10        cur = head
+11        flag = -1
+12
+13        while cur:
+14            if flag == 1:
+15                even.next = cur
+16                even = even.next
+17            else:
+18                odd.next = cur
+19                odd = odd.next
+20            cur = cur.next
+21            flag *= -1
+22        
+23        even.next = None
+24        odd.next = even_dummy.next
+25        return odd_dummy.next
