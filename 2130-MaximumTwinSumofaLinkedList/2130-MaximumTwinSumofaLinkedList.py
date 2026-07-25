71da936bbe9b3ba537f13101c9e3534ee180c25e
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-24 22:16:07 -0700

    File updated at 7/24/2026, 10:16:07 PM

--- 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
+++ 2130-MaximumTwinSumofaLinkedList/2130-MaximumTwinSumofaLinkedList.py
@@ -1,4 +1,4 @@
-# Last updated: 6/13/2026, 9:51:54 PM
+# Last updated: 7/24/2026, 10:16:07 PM
 1# Definition for singly-linked list.
 2# class ListNode:
 3#     def __init__(self, val=0, next=None):
@@ -11,20 +11,22 @@
 10        while fast and fast.next:
 11            slow = slow.next
 12            fast = fast.next.next
-13        
+13
 14        prev = None
-15        while slow:
-16            nxt = slow.next
-17            slow.next = prev
-18            prev = slow
-19            slow = nxt
-20
-21        p1, p2 = head, prev
-22        mx = 0
-23
-24        while p2:
-25            mx = max(mx, p1.val + p2.val)
-26            p1 = p1.next
-27            p2 = p2.next
-28
-29        return mx
+15        cur = slow
+16
+17        while cur:
+18            nxt = cur.next
+19            cur.next = prev
+20            prev = cur
+21            cur = nxt
+22        
+23        p1, p2 = head, prev
+24        mx = 0
+25
+26        while p2:
+27            mx = max(mx, p1.val + p2.val)
+28            p1 = p1.next
+29            p2 = p2.next
+30        
+31        return mx
