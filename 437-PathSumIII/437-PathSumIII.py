da8c571cae4dd795f0ec0fd905b90bf95d91c9ad
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-26 15:08:28 -0700

    File updated at 7/26/2026, 3:08:28 PM

--- 437-PathSumIII/437-PathSumIII.py
+++ 437-PathSumIII/437-PathSumIII.py
@@ -1,28 +1,22 @@
-# Last updated: 7/8/2025, 1:29:09 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        def dfs(node, total):
-            nonlocal counter
-            if not node:
-                return
-
-            total += node.val
-            counter += prefix[total - targetSum]
-            prefix[total] += 1
-                        
-            dfs(node.left, total)
-            dfs(node.right, total)
-
-            prefix[total] -= 1
-        
-        counter = 0
-        prefix = defaultdict(int)
-        prefix[0] = 1 # this is to account for when total == targetSum right?
-        dfs(root, 0)
-        return counter
+# Last updated: 7/26/2026, 3:08:28 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+9        if not root:
+10            return 0
+11            
+12        def dfs(node, total):
+13            if not node:
+14                return 0
+15            
+16            if total + node.val == targetSum:
+17                return 1 + dfs(node.left, total + node.val) + dfs(node.right, total + node.val)
+18            
+19            return dfs(node.left, total + node.val) + dfs(node.right, total + node.val)
+20
+21        return dfs(root, 0) + self.pathSum(root.left, targetSum) + self.pathSum(root.right, targetSum)
