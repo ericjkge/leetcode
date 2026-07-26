fab3e2b5b3210527b4bcae04472c7542a70bcba1
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-26 15:35:06 -0700

    File updated at 7/26/2026, 3:35:06 PM

--- 1372-LongestZigZagPathinaBinaryTree/1372-LongestZigZagPathinaBinaryTree.py
+++ 1372-LongestZigZagPathinaBinaryTree/1372-LongestZigZagPathinaBinaryTree.py
@@ -1,25 +1,25 @@
-# Last updated: 7/8/2025, 2:49:36 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
-
-        def dfs(node, goLeft, steps):
-            if not node:
-                return
-            
-            self.ans = max(self.ans, steps)
-            if goLeft:
-                dfs(node.left, False, steps + 1)
-                dfs(node.right, True, 1)
-            else:
-                dfs(node.left, False, 1)
-                dfs(node.right, True, steps + 1)
-            
-        dfs(root, True, 0)
-        return self.ans
+# Last updated: 7/26/2026, 3:35:06 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+9        self.best = 0
+10
+11        def dfs(node):
+12            if not node:
+13                return -1, -1
+14
+15            ll, lr = dfs(node.left)
+16            rl, rr = dfs(node.right)
+17
+18            left, right = 1 + lr, 1 + rl
+19
+20            self.best = max(self.best, left, right)
+21            return left, right
+22
+23        dfs(root)
+24        return self.best
