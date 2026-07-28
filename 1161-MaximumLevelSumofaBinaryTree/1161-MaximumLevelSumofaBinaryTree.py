363e016ff2032a6cab1a50b973713ef048983c20
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-27 20:38:50 -0700

    File updated at 7/27/2026, 8:38:50 PM

--- 1161-MaximumLevelSumofaBinaryTree/1161-MaximumLevelSumofaBinaryTree.py
+++ 1161-MaximumLevelSumofaBinaryTree/1161-MaximumLevelSumofaBinaryTree.py
@@ -1,28 +1,32 @@
-# Last updated: 7/8/2025, 7:02:12 PM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        max_sum, ans, level = -float("inf"), 0, 1
-        queue = deque([root])
-        
-        while queue:
-            level_sum = 0
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                level_sum += node.val
-                
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-                    
-            if max_sum < level_sum:
-                ans = level
-                max_sum = level_sum
-            level += 1
-        return ans
+# Last updated: 7/27/2026, 8:38:50 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution:
+8    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+9        queue = deque([root])
+10        mx = float("-inf")
+11        ans = None
+12
+13        count = 1
+14        while queue:
+15            n = len(queue)
+16            level = 0
+17            for _ in range(n):
+18                node = queue.popleft()
+19                if node.left:
+20                    queue.append(node.left)
+21                if node.right:
+22                    queue.append(node.right)
+23                level += node.val
+24
+25            if level > mx:
+26                mx = level
+27                ans = count
+28
+29            count += 1
+30        
+31        return ans
