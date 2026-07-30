e46e4718a3aebf2130e9d03843c0675e099d7473
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-07-29 21:17:33 -0700

    File updated at 7/29/2026, 9:17:32 PM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,20 +1,27 @@
-# Last updated: 10/19/2025, 9:00:17 AM
-class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        ans = 0
-        seen = set()
-
-        def dfs(i):
-            for j in range(0, len(isConnected)):
-                if isConnected[i][j]:
-                    if j not in seen:
-                        seen.add(i)
-                        dfs(j)
-        
-        for i in range(len(isConnected)):
-            if i not in seen:
-                seen.add(i)
-                dfs(i)
-                ans += 1
-
-        return ans
+# Last updated: 7/29/2026, 9:17:32 PM
+1class Solution:
+2    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+3        n = len(isConnected)
+4        graph = defaultdict(list)
+5
+6        for r in range(n):
+7            for c in range(n):
+8                if isConnected[r][c] == 1:
+9                    graph[r].append(c)
+10                    graph[c].append(r)
+11        
+12        def dfs(node):
+13            seen.add(node)
+14            for neighbor in graph[node]:
+15                if neighbor not in seen:
+16                    dfs(neighbor)
+17
+18        seen = set()
+19        count = 0
+20        for i in range(n):
+21            if i not in seen:
+22                dfs(i)
+23                count += 1
+24        
+25        return count
+26
