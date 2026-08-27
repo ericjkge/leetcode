8c64df9be737cdb03e8dbf8b0e654c911c93936b
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-08-27 15:52:47 +0800

    File updated at 8/27/2026, 3:52:46 PM

--- 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.py
+++ 452-MinimumNumberofArrowstoBurstBalloons/452-MinimumNumberofArrowstoBurstBalloons.py
@@ -1,16 +1,15 @@
-# Last updated: 8/27/2026, 3:52:39 PM
+# Last updated: 8/27/2026, 3:52:46 PM
 1class Solution:
 2    def findMinArrowShots(self, points: List[List[int]]) -> int:
 3        prev = None
 4        count = 0
 5        points.sort()
-6        print(points)
-7
-8        for start, end in points:
-9            if prev is None or start > prev:
-10                count += 1
-11                prev = end
-12            else:
-13                prev = min(prev, end)
-14
-15        return count
+6
+7        for start, end in points:
+8            if prev is None or start > prev:
+9                count += 1
+10                prev = end
+11            else:
+12                prev = min(prev, end)
+13
+14        return count
