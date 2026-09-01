18b3c54a8050ce6f13fb83619d9294f789a5ec02
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-09-01 17:09:20 +0300

    File updated at 9/1/2026, 10:09:19 AM

--- 253-MeetingRoomsII/253-MeetingRoomsII.py
+++ 253-MeetingRoomsII/253-MeetingRoomsII.py
@@ -1,18 +1,15 @@
-# Last updated: 4/9/2026, 9:28:27 AM
+# Last updated: 9/1/2026, 10:09:19 AM
 1class Solution:
 2    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
 3        intervals.sort()
-4        rooms = 0
+4        heap = []
 5        ans = 0
-6        heap = []
-7
-8        for start, end in intervals:
-9            if not heap or start < heap[0]:
-10                rooms += 1
-11                heapq.heappush(heap, end)
-12                ans = max(ans, rooms)
-13            else:
-14                heapq.heappop(heap)
-15                heapq.heappush(heap, end)
-16
-17        return ans
+6
+7        for start, end in intervals:
+8            while heap and heap[0] <= start:
+9                heapq.heappop(heap)
+10            
+11            heapq.heappush(heap, end)
+12            ans = max(ans, len(heap))
+13
+14        return ans
