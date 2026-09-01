ab467495eed4a6ea7b0e429021f28bbb0e7078d7
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-09-01 18:18:25 +0300

    File updated at 9/1/2026, 11:18:25 AM

--- 112-PathSum/112-PathSum.py
+++ 112-PathSum/112-PathSum.py
@@ -1,4 +1,4 @@
-# Last updated: 8/9/2026, 11:53:17 AM
+# Last updated: 9/1/2026, 11:18:25 AM
 1# Definition for a binary tree node.
 2# class TreeNode:
 3#     def __init__(self, val=0, left=None, right=None):
@@ -11,18 +11,12 @@
 10            return False
 11
 12        def dfs(node, total):
-13            if not node.left and not node.right:
-14                if total + node.val == targetSum:
-15                    return True
-16                return False
-17            
-18            total += node.val
-19            if node.left and dfs(node.left, total):
-20                return True
-21            
-22            if node.right and dfs(node.right, total):
-23                return True
-24
-25            return False
-26        
-27        return dfs(root, 0)
+13            if not node:
+14                return False
+15                
+16            total += node.val
+17            if not node.left and not node.right:
+18                return total == targetSum
+19            return dfs(node.left, total) or dfs(node.right, total)
+20        
+21        return dfs(root, 0)
