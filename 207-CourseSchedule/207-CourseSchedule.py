8e20c866ac34d569efd64562fae750cbc665c7ce
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-09-01 15:10:29 +0300

    File updated at 9/1/2026, 8:10:29 AM

--- 207-CourseSchedule/207-CourseSchedule.py
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -1,22 +1,22 @@
-# Last updated: 8/9/2026, 12:05:15 AM
+# Last updated: 9/1/2026, 8:10:29 AM
 1class Solution:
 2    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-3        indegrees = [0] * numCourses
-4        graph = defaultdict(list)
+3        graph = defaultdict(list)
+4        indegrees = [0] * numCourses
 5
 6        for a, b in prerequisites:
 7            graph[b].append(a)
 8            indegrees[a] += 1
-9        
+9
 10        queue = deque([i for i in range(numCourses) if indegrees[i] == 0])
-11        seen = set()
+11        seen = len(queue)
 12        while queue:
-13            node = queue.popleft()
-14            seen.add(node)
-15            for neighbor in graph[node]:
-16                indegrees[neighbor] -= 1
-17                if indegrees[neighbor] == 0:
+13            course = queue.popleft()
+14            for neighbor in graph[course]:
+15                indegrees[neighbor] -= 1
+16                if indegrees[neighbor] == 0:
+17                    seen += 1
 18                    queue.append(neighbor)
 19        
-20        return len(seen) == numCourses
-21
+20        return seen == numCourses
+21        
