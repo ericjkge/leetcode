630711e05f0acf15b27975ccff68106b1ea5bed2
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-09-01 18:24:20 +0300

    File updated at 9/1/2026, 11:24:20 AM

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.py
@@ -1,18 +1,18 @@
-# Last updated: 8/9/2026, 2:46:08 PM
+# Last updated: 9/1/2026, 11:24:20 AM
 1class Solution:
 2    def minWindow(self, s: str, t: str) -> str:
-3        ans = None
-4        left = right = 0
-5        s_freqs = Counter()
-6        t_freqs = Counter(t)
-7
-8        while right < len(s):
-9            s_freqs[s[right]] += 1
-10            while all(s_freqs[c] >= t_freqs[c] for c in t_freqs):
-11                if ans is None or right - left + 1 < len(ans):
-12                    ans = s[left:right + 1]
-13                s_freqs[s[left]] -= 1
-14                left += 1
+3        left = right = 0
+4        ans = None
+5        freqs1, freqs2 = Counter(), Counter(t)
+6
+7        while right < len(s):
+8            freqs1[s[right]] += 1
+9            while all(freqs1[c] >= freqs2[c] for c in freqs2):
+10                if ans is None or right - left + 1 < len(ans):
+11                    ans = s[left:right + 1]
+12                freqs1[s[left]] -= 1
+13                left += 1
+14            
 15            right += 1
-16        
+16
 17        return ans if ans is not None else ""
