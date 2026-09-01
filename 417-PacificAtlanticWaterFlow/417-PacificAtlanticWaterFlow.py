e9673fc5fc6892fdf6d8e5b9c2a94ac4f6c410a3
Eric Ge <85514000+ericjkge@users.noreply.github.com>
2026-09-01 13:15:58 +0300

    File updated at 9/1/2026, 6:15:58 AM

--- 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
+++ 417-PacificAtlanticWaterFlow/417-PacificAtlanticWaterFlow.py
@@ -1,26 +1,23 @@
-# Last updated: 8/8/2026, 4:52:16 PM
+# Last updated: 9/1/2026, 6:15:58 AM
 1class Solution:
 2    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-3        rows, cols = len(heights), len(heights[0])
-4        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-5        q1 = deque([(r, 0) for r in range(rows)] + [(0, c) for c in range(1, cols)])
-6        q2 = deque([(rows - 1, c) for c in range(cols)] + [(r, cols - 1) for r in range(rows - 1)])
-7        pacific, atlantic = set(), set()
-8
-9        while q1:
-10            r, c = q1.popleft()
-11            pacific.add((r, c))
-12            for dr, dc in directions:
-13                nr, nc = r + dr, c + dc
-14                if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in pacific:
-15                    q1.append((nr, nc))
-16        
-17        while q2:
-18            r, c = q2.popleft()
-19            atlantic.add((r, c))
-20            for dr, dc in directions:
-21                nr, nc = r + dr, c + dc
-22                if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in atlantic:
-23                    q2.append((nr, nc))
-24
-25        return list(pacific & atlantic)
+3        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+4        rows, cols = len(heights), len(heights[0])
+5        pacific, atlantic = set(), set()
+6
+7        def dfs(r, c, s):
+8            s.add((r, c))
+9            for dr, dc in directions:
+10                nr, nc = r + dr, c + dc
+11                if 0 <= nr < rows and 0 <= nc < cols and heights[nr][nc] >= heights[r][c] and (nr, nc) not in s:
+12                    dfs(nr, nc, s)
+13
+14        for r in range(rows):
+15            dfs(r, 0, pacific)
+16            dfs(r, cols - 1, atlantic)
+17        
+18        for c in range(cols):
+19            dfs(0, c, pacific)
+20            dfs(rows - 1, c, atlantic)
+21
+22        return list(pacific & atlantic)
